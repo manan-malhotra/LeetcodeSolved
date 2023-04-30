ade9f65c0ee1cf4ae09359ba9a4e38459bda59a1
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-04-30 17:57:45 +0530

    Time: 2 ms (49.71%) | Memory: 51.3 MB (13.40%) - LeetSync

--- product-of-array-except-self/product-of-array-except-self.java
+++ product-of-array-except-self/product-of-array-except-self.java
@@ -1,36 +1,22 @@
-class Solution { 
-
-    private int productIfNumIsZero(int[] nums, int k) {
-        int res = 1;
-        boolean flag = false;
-        for(int i=0;i<nums.length;i++) {
-            if(i!=k) {
-                flag = true;
-                res *= nums[i];
-            }
-        }
-        return flag ? res : 0;
-    } 
-    
+class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int res = 1;
-        for(int i=0;i<n;i++) {
-            res *= nums[i];
+        int pre[] = new int[n];
+        int suff[] = new int[n];
+        pre[0] = 1;
+        suff[n - 1] = 1;
+        
+        for(int i = 1; i < n; i++) {
+            pre[i] = pre[i - 1] * nums[i - 1];
         }
-
+        for(int i = n - 2; i >= 0; i--) {
+            suff[i] = suff[i + 1] * nums[i + 1];
+        }
+        
         int ans[] = new int[n];
-
-        for(int i=0;i<n;i++) {
-            int num = nums[i];
-            if(num != 0) {
-                ans[i] = res / num;
-            } else {
-                ans[i] = productIfNumIsZero(nums, i);
-            }
+        for(int i = 0; i < n; i++) {
+            ans[i] = pre[i] * suff[i];
         }
-
         return ans;
-
     }
 }
