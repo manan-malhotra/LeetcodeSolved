0613bdda8b41be5cf1766675a77af58a6f897aa4
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-05-05 22:39:08 +0530

    Time: 1 ms (99.99%) | Memory: 50.8 MB (62.75%) - LeetSync

---
+++ first-missing-positive/first-missing-positive.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int firstMissingPositive(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] > 0 && arr[i] <= arr.length && arr[i] != arr[correct]) {
+                swap(arr, i , correct);
+            } else {
+                i++;
+            }
+        }
+
+        // search for first missing number
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index + 1) {
+                return index + 1;
+            }
+        }
+
+        // case 2
+        return arr.length + 1;
+    }
+    
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
