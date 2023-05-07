f5804b9a1f1f6e17459546b22a0c43ae0d5db22b
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-05-07 18:26:31 +0530

    Time: 19 ms (18.72%) | Memory: 43.4 MB (13.45%) - LeetSync

--- valid-anagram/valid-anagram.java
+++ valid-anagram/valid-anagram.java
@@ -1,9 +1,24 @@
 public class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] alphabet = new int[26];
-        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
-        for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
-        for (int i : alphabet) if (i != 0) return false;
-        return true;
+        if (s.length() != t.length()) return false; // If both string are not of the same length
+        HashMap<Character, Integer> map = new HashMap<>(); // Create a new hashmap
+        for(int i=0;i<s.length();i++){ // Iterate through s string
+            char ch = s.charAt(i); // get value of ith index of string s
+            map.put(ch,map.getOrDefault(ch,0)+1); // if val already exists it will return that or else it will return 0
+            // Add 1 to that value and put it again in the map
+        }
+        for(int i=0;i<t.length();i++){ // Iterate through t string
+            char ch = t.charAt(i); // get value of ith index of string t
+            if(map.get(ch)!=null){ // if the value at i exists
+                if(map.get(ch)==1){
+                    map.remove(ch); // if value to key is 1, remove it from the map
+                }else{
+                    map.put(ch,map.get(ch)-1); // else get the value and decrement
+                }
+            }else{ // else it is not an anagram
+                return false;
+            }
+        }
+        return map.isEmpty(); // if map is empty then return true
     }
 }
