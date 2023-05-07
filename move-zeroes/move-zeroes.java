2b2a1459f67e8d80e2ff8c08ce4ab09d124fb92a
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-05-07 21:21:14 +0530

    Time: 1 ms (99.98%) | Memory: 44.1 MB (45.17%) - LeetSync

--- move-zeroes/move-zeroes.java
+++ move-zeroes/move-zeroes.java
@@ -1,29 +1,16 @@
 class Solution {
-    // public void moveZeroes(int[] nums) {
-    //     int lastIndexUsed = 0;
-    //     int currNonZero = 0;
-    //     for(int i=0;i<nums.length;i++){
-    //         if(nums[i]!=0){
-    //             nums[currNonZero++]= nums[i];
-    //             lastIndexUsed = currNonZero;
-    //         }
-    //     }
-    //     for(int j=lastIndexUsed;j<nums.length;j++){
-    //         nums[j]=0;
-    //     }
-        
-    // }
     public void moveZeroes(int[] nums) {
-        int snowBallSize = 0; 
-        for (int i=0;i<nums.length;i++){
-	        if (nums[i]==0){
-                snowBallSize++; 
-            }
-            else if (snowBallSize > 0) {
-	            int t = nums[i];
-	            nums[i]=0;
-	            nums[i-snowBallSize]=t;
+        int lastIndexUsed = 0;
+        int currNonZero = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[currNonZero++]= nums[i];
+                lastIndexUsed = currNonZero;
             }
         }
+        for(int j=lastIndexUsed;j<nums.length;j++){
+            nums[j]=0;
+        }
+        
     }
 }
