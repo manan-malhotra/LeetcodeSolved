69eb51e6515898d9c11d1aa8f6fa121e8a9c3741
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-05-10 09:20:12 +0530

    Time: 459 ms (5.41%) | Memory: 59.2 MB (53.35%) - LeetSync

--- longest-consecutive-sequence/longest-consecutive-sequence.java
+++ longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,20 +1,26 @@
 class Solution {
-    public int longestConsecutive(int[] nums) {
-        if(nums == null || nums.length == 0) return 0;
-        Set<Integer> set = new HashSet<>();
-        for(int i : nums) set.add(i);
-        int ans = 0;
-        for(int num : nums) {
-            int left = num - 1;
-            int right = num + 1;
-            while(set.remove(left)) left--;
-            while(set.remove(right)) right++;
-            System.out.println(right+"right");
-            System.out.println(left+"left");
-            System.out.println(set.size()+"size");
-            ans = Math.max(ans,right - left - 1);
-            if(set.isEmpty()) return ans;//save time if there are items in nums, but no item in hashset.
+    public static int longestConsecutive(int[] nums) {
+        Set < Integer > hashSet = new HashSet < Integer > ();
+        for (int num: nums) {
+            hashSet.add(num);
         }
-        return ans;
+
+        int longestStreak = 0;
+
+        for (int num: nums) {
+            if (!hashSet.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (hashSet.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
     }
 }
