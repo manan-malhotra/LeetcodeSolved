84fa0fa5f2c41f3800c0e7828ebb16ce55134f3d
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-05-14 16:50:27 +0530

    Time: 2 ms (99.71%) | Memory: 42.9 MB (53.13%) - LeetSync

--- valid-anagram/valid-anagram.java
+++ valid-anagram/valid-anagram.java
@@ -1,16 +1,9 @@
 public class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false; // If both string are not of the same length
-
-        int[] store = new int[26]; // Create a bucket of 26 char; a=0, z=25; Only works if strings are just small letters and no numbers
-
-        for (int i = 0; i < s.length(); i++) { // Iterate through the strings;
-            store[s.charAt(i) - 'a']++; // Add 1 at ASCII value from s in the bucket store
-            store[t.charAt(i) - 'a']--; // Sub 1 at ASCII value from t in the bucket store
-        }
-
-        for (int n : store) if (n != 0) return false; // If value of any char is not equal to 0, it is not a valid anagram
-
-        return true; // else return true;
+       var count = new int[26];
+        for (char c : s.toCharArray()) count[c - 'a']++;
+        for (char c : t.toCharArray()) count[c - 'a']--;
+        for (int i : count) if (i != 0) return false;
+        return true;
     }
 }
