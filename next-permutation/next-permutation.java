7eba4fbe3bd5b18c5dd56640483b1683e188ba29
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-05-20 09:20:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (92.17%) - LeetSync

--- next-permutation/next-permutation.java
+++ next-permutation/next-permutation.java
@@ -1,47 +1,48 @@
-public class Solution {
-/*0*/ public void nextPermutation(int[] nums) {
+class Solution {
+    public void nextPermutation(int[] nums) {
         // pivot is the element just before the non-increasing (weakly decreasing) suffix
-/*2*/   int pivot = indexOfLastPeak(nums) - 1;
+        int pivot = findPivot(nums)-1;
         // paritions nums into [prefix pivot suffix]
-        if (pivot != -1) {
-            int nextPrefix = lastIndexOfGreater(nums, nums[pivot]); // in the worst case it's suffix[0]
+        if(pivot!=-1){
+            int nextGreaterElement = findNext(nums,pivot);
             // next prefix must exist because pivot < suffix[0], otherwise pivot would be part of suffix
-/*4*/       swap(nums, pivot, nextPrefix); // this minimizes the change in prefix
+            swap(nums,pivot,nextGreaterElement);
+            // this minimizes the change in prefix
         }
-/*5*/   reverseSuffix(nums, pivot + 1); // reverses the whole list if there was no pivot
-/*6*/ }
-    
-    /**
-     * Find the last element which is a peak.
-     * In case there are multiple equal peaks, return the first of those.
-     * @return first index of last peak
-     */
-/*1*/ int indexOfLastPeak(int[] nums) {
-        for (int i = nums.length - 1; 0 < i; --i) {
-            if (nums[i - 1] < nums[i]) return i;
-        }
-        return 0;
+        reverseSuffix(nums,pivot+1);
+        // reverses the whole list if there was no pivot
     }
-
-    /** @return last index where the {@code num > threshold} or -1 if not found */
-/*3*/ int lastIndexOfGreater(int[] nums, int threshold) {
-        for (int i = nums.length - 1; 0 <= i; --i) {
-            if (threshold < nums[i]) return i;
-        }
-        return -1;
-    }
-
-    /** Reverse numbers starting from an index till the end. */
-    void reverseSuffix(int[] nums, int start) {
-        int end = nums.length - 1;
-        while (start < end) {
+    /* Reverse numbers starting from an index till the end. */
+    private static void reverseSuffix(int[] nums, int start) {
+        int end = nums.length-1;
+        while(start<end){
             swap(nums, start++, end--);
         }
     }
-    
-    void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[j];
+        nums[j]=nums[i];
+        nums[i]=temp;
+    }
+
+    private static int findNext(int[] nums, int pivot) {
+        int threshold = nums[pivot];
+        for(int i=nums.length-1;i>0;i--){
+            if(nums[i]>threshold) return i;
+        }
+        return 0;
+    }
+    /* Find the last element which is a peak.
+    * In case there are multiple equal peaks, return the first of those.
+    * @return first index of last peak
+    */
+    private static int findPivot(int[] nums) {
+        for(int i=nums.length-1;i>0;i--){
+            if(nums[i-1]<nums[i]){
+                return i;
+            }
+        }
+        return 0;
     }
 }
