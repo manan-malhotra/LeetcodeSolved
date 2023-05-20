5e0c89bd79960b8ceb1229f628fc0d139c4b4adf
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-05-20 23:03:24 +0530

    Time: 1 ms (100.00%) | Memory: 51.8 MB (5.88%) - LeetSync

--- product-of-array-except-self/product-of-array-except-self.java
+++ product-of-array-except-self/product-of-array-except-self.java
@@ -1,22 +1,20 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int pre[] = new int[n];
-        int suff[] = new int[n];
-        pre[0] = 1;
-        suff[n - 1] = 1;
-        
-        for(int i = 1; i < n; i++) {
-            pre[i] = pre[i - 1] * nums[i - 1];
-        }
-        for(int i = n - 2; i >= 0; i--) {
-            suff[i] = suff[i + 1] * nums[i + 1];
+       int[] numsProduct = new int[nums.length]; // Creating an array to store products.
+        int preProduct = 1, postProduct = 1; // Initializing a prefix, and postfix product.
+
+        // Find product of each array i, but exclude i.
+            // By calculating the left and right products seperately we are able to calculate in O(N) time.
+        for(int i = 0; i < nums.length; i++){ // Left Index
+            numsProduct[i] = preProduct;
+            preProduct *= nums[i];
         }
         
-        int ans[] = new int[n];
-        for(int i = 0; i < n; i++) {
-            ans[i] = pre[i] * suff[i];
+        for(int i = nums.length - 1; i >= 0; i--){ // Right Index
+            numsProduct[i] *= postProduct;
+            postProduct *= nums[i];
         }
-        return ans;
+
+        return numsProduct;
     }
 }
