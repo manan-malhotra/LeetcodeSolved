cb37fec9122cc15a59d8e93e3a6539a1e25c5bd1
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-05-25 17:00:33 +0530

    Time: 37 ms (86.80%) | Memory: 51.4 MB (5.20%) - LeetSync

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -4,7 +4,7 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         if(nums[0]>0) return ans;
         for(int i=0;i<nums.length-2;i++){
-            if(i==0||(i>0 && nums[i]!=nums[i-1])) {
+            if(i==0|| nums[i]!=nums[i-1]) {
                 int a = i+1;
                 int b = nums.length-1;
                 
