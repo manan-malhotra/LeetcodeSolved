c3cd531a63186971daedc6c6c0a46d7964114222
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-05-25 07:44:06 +0530

    Time: 2 ms (97.22%) | Memory: 47.5 MB (5.21%) - LeetSync

--- majority-element-ii/majority-element-ii.java
+++ majority-element-ii/majority-element-ii.java
@@ -1,23 +1,49 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n = nums.length;
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        for(int i=0;i<n;i++){
-            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
-        }
-        
-        List<Integer> ans = new ArrayList<>();
-         Set<Integer> hs = new HashSet<Integer>();
-        for(int i=0;i<n;i++){
-            if(hm.get(nums[i])*3>n) hs.add(nums[i]);
-        }
-        for(int i=0;i<n;i++){
-            if(hs.remove(nums[i])){
-                ans.add(nums[i]);
-                
+        int count1=0;
+        int count2=0;
+        int ele1=-1;
+        int ele2=-1;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==ele1){
+                count1++;
+            }else if(nums[i]==ele2){
+                count2++;
+            }            
+            else if(count1==0){
+                ele1=nums[i];
+                count1++;
+            }else if(count2==0){
+                ele2=nums[i];
+                count2++;
+            }else{
+                count1--;
+                count2--;
             }
         }
-        return ans;
 
+        count1=0;
+        count2=0;
+        ArrayList<Integer> arr=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==ele1){
+                count1++;
+            }else if(nums[i]==ele2){
+                count2++;
+            }
+        }
+        // System.out.println(ele1);
+        // System.out.println(ele2);
+        // System.out.println(count1);
+        // System.out.println(count2);
+
+        if(count1>nums.length/3){
+            arr.add(ele1);
+        }if(count2>nums.length/3){
+            arr.add(ele2);
+        }
+
+        return arr;
     }
 }
