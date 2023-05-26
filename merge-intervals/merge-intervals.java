b46a80065735ef209b92b6f7589df6b003f77a77
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-05-26 09:34:03 +0530

    Time: 11 ms (23.36%) | Memory: 46.7 MB (82.80%) - LeetSync

--- merge-intervals/merge-intervals.java
+++ merge-intervals/merge-intervals.java
@@ -1,23 +1,30 @@
 class Solution {
-	public int[][] merge(int[][] intervals) {
-		if (intervals.length <= 1)
-			return intervals;
-
-		// Sort by ascending starting point
-		Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
-
-		List<int[]> result = new ArrayList<>();
-		int[] newInterval = intervals[0];
-		result.add(newInterval);
-		for (int[] interval : intervals) {
-			if (interval[0] <= newInterval[1]) // Overlapping intervals, move the end if needed
-				newInterval[1] = Math.max(newInterval[1], interval[1]);
-			else {                             // Disjoint intervals, add the new interval to the list
-				newInterval = interval;
-				result.add(newInterval);
-			}
-		}
-
-		return result.toArray(new int[result.size()][]);
-	}
+    public int[][] merge(int[][] intervals) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
+        for(int i=0;i<intervals.length;i++){
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            boolean isOverlapping = true;
+            while(isOverlapping){
+                if(i<intervals.length-1 && end>=intervals[i+1][0]){
+                    i++;
+                    start = Math.min(start,intervals[i][0]);
+                    end = Math.max(end,intervals[i][1]);
+                }else{
+                       isOverlapping=false; 
+                }
+            }
+            List<Integer> arr = new ArrayList<>();
+            arr.add(start);
+            arr.add(end);
+            ans.add(arr);
+        }
+        int[][] res = new int[ans.size()][2];
+        for(int j=0;j<ans.size();j++){
+            res[j][0] = (int)ans.get(j).get(0);
+            res[j][1] = (int)ans.get(j).get(1);
+        }
+        return res;
+    }
 }
