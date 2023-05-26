7ec356ece97d536a18308ed97192a112a8a6a28b
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-05-26 10:02:42 +0530

    Time: 8 ms (96.76%) | Memory: 45.9 MB (93.02%) - LeetSync

--- merge-intervals/merge-intervals.java
+++ merge-intervals/merge-intervals.java
@@ -1,30 +1,18 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        List<List<Integer>> ans = new ArrayList<>();
+        List<int[]> result = new ArrayList<>();
         Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
-        for(int i=0;i<intervals.length;i++){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            boolean isOverlapping = true;
-            while(isOverlapping){
-                if(i<intervals.length-1 && end>=intervals[i+1][0]){
-                    i++;
-                    start = Math.min(start,intervals[i][0]);
-                    end = Math.max(end,intervals[i][1]);
-                }else{
-                       isOverlapping=false; 
-                }
+        int[] newArr = new int[2];
+        newArr = intervals[0];
+        result.add(newArr);
+        for(int[] interval : intervals){
+            if(interval[0]<=newArr[1]){
+                newArr[1]=Math.max(newArr[1],interval[1]);
+            }else{
+                newArr = interval;
+                result.add(newArr);
             }
-            List<Integer> arr = new ArrayList<>();
-            arr.add(start);
-            arr.add(end);
-            ans.add(arr);
         }
-        int[][] res = new int[ans.size()][2];
-        for(int j=0;j<ans.size();j++){
-            res[j][0] = (int)ans.get(j).get(0);
-            res[j][1] = (int)ans.get(j).get(1);
-        }
-        return res;
+        return result.toArray(new int[result.size()][]);
     }
 }
