99d17fda2ca871d0e9ecf9601c81e5fd23e4db50
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-06-15 23:26:53 +0530

    Time: 45 ms (59.94%) | Memory: 50.2 MB (69.48%) - LeetSync

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -3,22 +3,23 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
         if(nums[0]>0) return ans;
-        for(int i=0;i<nums.length-2;i++){
-            if(i==0|| nums[i]!=nums[i-1]) {
-                int a = i+1;
-                int b = nums.length-1;
-                
+        for(int i=0; i < nums.length-2;i++){
+            int a = i+1;
+            int b = nums.length-1;
+            if(i==0 || nums[i]!=nums[i-1]){
                 while(a<b){
                     int sum = nums[i]+nums[a]+nums[b];
                     if(sum==0){
-                        ans.add(Arrays.asList(nums[i], nums[a], nums[b]));
+                        ans.add(Arrays.asList(nums[i],nums[a],nums[b]));
                         while(a<b && nums[a]==nums[a+1]) a++;
                         while(a<b && nums[b]==nums[b-1]) b--;
                         a++;
                         b--;
-                    }else if(sum>0) b--;
-                    else a++;
+                    }else if(sum>0){
+                        b--;
+                    }else a++;
                 }
+
             }
         }
         return ans;
