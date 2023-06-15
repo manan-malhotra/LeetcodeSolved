64cec860658bc0b338e77ab83ccc15fc393f0d32
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-06-15 23:07:17 +0530

    Time: 2 ms (97.84%) | Memory: 46.9 MB (31.89%) - LeetSync

--- majority-element-ii/majority-element-ii.java
+++ majority-element-ii/majority-element-ii.java
@@ -1,30 +1,34 @@
 class Solution {
     public List<Integer> majorityElement(int[] arr) {
-         int count1 = 0;
+        int ele1 = 0;
+        int ele2 = 0;
+        int count1 = 0;
         int count2 = 0;
-        int ele1 = -1;
-        int ele2 = -1;
-        for(int i: arr){
-            if(ele1==i){
+        for(int i=0;i<arr.length;i++){
+            if(ele1==arr[i]){
                 count1++;
-            }else if(ele2 == i){
+            }else if(ele2 == arr[i]){
                 count2++;
-            }else if(count1==0){
-                ele1 = i;
-                count1++;
-            }else if(count2==0){
-                ele2=i;
-                count2++;
+            }else if(count1 == 0){
+                ele1 = arr[i];
+                count1=1;
+            }else if(count2 == 0){
+                ele2 = arr[i];
+                count2=1;
             }else{
                 count1--;
                 count2--;
             }
         }
-        count1=0;
-        count2=0;
-        for(int i:arr){
-            if(i==ele1) count1++;
-            else if(i==ele2) count2++;
+        count1 = 0;
+        count2 = 0;
+        for(int i: arr){
+            if(i==ele1){
+                count1++;
+            }
+            else if(i==ele2){
+                count2++;
+            }
         }
         List<Integer> ans = new ArrayList<>();
         if(count1>arr.length/3) ans.add(ele1);
