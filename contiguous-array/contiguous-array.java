210e4645e2e887281f00a64a0b743299f5d7588a
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-06-15 22:39:57 +0530

    Time: 28 ms (60.21%) | Memory: 54.6 MB (33.05%) - LeetSync

--- contiguous-array/contiguous-array.java
+++ contiguous-array/contiguous-array.java
@@ -4,19 +4,19 @@ class Solution {
         int sum = 0;
         int maxLen = 0;
         for(int i=0;i<arr.length;i++){
-            int add = 1;
-            if(arr[i]==0) add = -1;
-            sum+=add;
+            if(arr[i]==0){
+                sum-=1;
+            }else{
+                sum+=1;
+            }
             if(sum==0){
-                maxLen= Math.max(maxLen, i+1);
+                maxLen = Math.max(maxLen,i+1);
             }
             if(hm.containsKey(sum)){
-                int len = i-hm.get(sum);
-                if(len>maxLen){
-                    maxLen=len;
-                }
+                int len = i- hm.get(sum);
+                maxLen = Math.max(maxLen,len);
             }else{
-                hm.put(sum, i);
+                hm.put(sum,i);
             }
         }
         return maxLen;
