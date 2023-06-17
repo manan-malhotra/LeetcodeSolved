175884f33a151f8b3eac8799c1145edbcfe7d30f
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-06-17 22:07:22 +0530

    Time: 21 ms (12.34%) | Memory: 48.1 MB (9.29%) - LeetSync

--- top-k-frequent-elements/top-k-frequent-elements.java
+++ top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,31 +1,27 @@
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int n : nums) {
-            map.put(n, map.getOrDefault(n, 0) + 1);
+    public int[] topKFrequent(int[] num, int k) {
+        Map<Integer,Integer> map = new HashMap<>();
+        List<Integer>[] bucket = new ArrayList[num.length+1];
+        for(int n: num){
+            map.put(n, map.getOrDefault(n, 0)+1);
         }
-
-        List<Integer>[] bucket = new ArrayList[nums.length + 1];
-
-        for (int key : map.keySet()) {
+        for(int key : map.keySet()){
             int freq = map.get(key);
-            if (bucket[freq] == null) {
+            if(bucket[freq]==null){
                 bucket[freq] = new ArrayList<>();
             }
             bucket[freq].add(key);
         }
-
         int[] res = new int[k];
         int index = 0;
-        for (int i = bucket.length - 1; i >= 0; i--) {
-            if (bucket[i] != null) {
-                for (int value : bucket[i]) {
-                    res[index++] = value;
-                    if (index == k) {
-                        return res;
-                    }
+        for(int i=bucket.length-1;i>=0 && index<k;i--){
+            if(bucket[i]!=null){
+                for(int value : bucket[i]){
+                    res[index++]=value;
+                    System.out.println(value + "val");
                 }
             }
+
         }
         return res;
     }
