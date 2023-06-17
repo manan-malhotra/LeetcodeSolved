1670b751a20148326a5e19f8ca2928e9f95a7506
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-06-17 22:28:14 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (89.35%) - LeetSync

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,40 +1,34 @@
 class Solution {
-   public static int binarySearch(int[] nums, int target, boolean findStartIndex){
-        int start=0;
-        int end=nums.length-1;
-        int ans=-1;
-        while(start<=end){
-            int mid=start + (end-start)/2;
-
-            if(nums[mid]<target){
-                start=mid+1;
-            }
-            else if(nums[mid]>target){
-                end=mid-1;
-            }
-            else{
-                //Possible Answer
-                ans=mid;
-                if(findStartIndex){
-                    end=mid-1;
-                }
-                else{
-                    start=mid+1;
-                }
-            }
-        }
-
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1,-1};
+        int len = nums.length;
+        if (len==0) return ans;
+        int first = binarySearch(nums,target,true);
+        if(first==-1) return ans;
+        int second = binarySearch(nums,target,false);
+        ans[0]= first;
+        ans[1]= second;
         return ans;
     }
-
-    public int[] searchRange(int[] nums, int target) {
-        int[] ans={-1, -1};
-
-        ans[0]=binarySearch(nums, target, true);
-        if(ans[0]!=-1){
-            ans[1]=binarySearch(nums, target, false);
+    public int binarySearch(int[] arr, int target,boolean first){
+        int start = 0;
+        int end = arr.length-1;
+        int ans = -1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]<target){
+                start = mid+1;
+            }else if(arr[mid]>target){
+                end = mid -1;
+            }else{
+                ans = mid;
+                if(first){
+                    end = mid -1;
+                }else{
+                    start = mid+1;
+                }
+            }
         }
-
-        return ans;  
+        return ans;
     }
 }
