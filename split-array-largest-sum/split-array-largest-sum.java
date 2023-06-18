5c8982cea4ab242d412b943753f6d857ca561201
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-06-18 18:13:52 +0530

    Time: 1 ms (60.13%) | Memory: 39.9 MB (93.93%) - LeetSync

---
+++ split-array-largest-sum/split-array-largest-sum.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int start = findMax(nums);
+        int end = findSum(nums);
+        while(start<end){
+            int mid = start + (end-start)/2; 
+            int max = Integer.MIN_VALUE;
+            int sum = 0;
+            int piece = 1;
+            
+            for(int i: nums){
+                sum += i;
+                if(sum>mid){
+                    sum = i;
+                    piece++;
+                }
+            }
+           if(piece>k){
+                start = mid + 1;
+            }else{
+                end = mid ;
+            }
+            
+        }
+        return end;
+    }
+    public int findMax(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for(int i : nums){
+            if(i>max){
+                max = i;
+            }
+        }
+        return max;
+    }
+    public int findSum(int[] nums) {
+        int sum = 0;
+        for(int i: nums){
+            sum+=i;
+        }
+        return sum;
+    }
+}
