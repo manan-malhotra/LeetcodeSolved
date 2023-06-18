085ef5b0ec62754439c0520cc2437b6e5549e0e6
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-06-18 20:33:31 +0530

    Time: 3 ms (7.27%) | Memory: 43.3 MB (18.11%) - LeetSync

--- search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,40 +1,50 @@
 class Solution {
-   public boolean search(int[] nums, int target) {
-    int start  = 0, end = nums.length - 1;
-    
-    //check each num so we will check start == end
-    //We always get a sorted part and a half part
-    //we can check sorted part to decide where to go next
-    while(start <= end){
-        int mid = start + (end - start)/2;
-        if(nums[mid] == target) return true;
-        
-        //if left part is sorted
-        if(nums[start] < nums[mid]){
-            if(target < nums[start] || target > nums[mid]){
-                //target is in rotated part
-                start = mid + 1;
-            }else{
-                end = mid - 1;
-            }
-        }else if(nums[start] > nums[mid]){
-            //right part is rotated
-            
-            //target is in rotated part
-            if(target < nums[mid] || target > nums[end]){
-                end = mid -1;
-            }else{
-                start = mid + 1;
-            }
-        }else{
-            //duplicates, we know nums[mid] != target, so nums[start] != target
-            //based on current information, we can only move left pointer to skip one cell
-            //thus in the worest case, we would have target: 2, and array like 11111111, then
-            //the running time would be O(n)
-            start ++;
+    public boolean search(int[] nums, int target) {
+        int peak = findPeak(nums);
+        System.out.println(peak);
+        int found = binarySearch(nums,target,0,peak);
+        if(found == -1) {
+            found = binarySearch(nums,target,peak+1,nums.length-1);
         }
+        return found!=-1;
     }
-    
-    return false;
-}
+    private int binarySearch(int[] arr, int target,int start,int end) {
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            int ans = arr[mid];
+            if(ans==target){
+                return mid;
+            }else if(ans>target){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+    private int findPeak(int[] nums) {
+          int start = 0;
+        int end = nums.length-1;
+        while(start<end){
+            int mid = start + (end - start)/2;
+            if(nums[mid]>nums[mid+1]){
+                return mid;
+            }else if(mid!=start && nums[mid]<nums[mid-1]){
+                return mid-1;
+            }
+            if(nums[start]==nums[mid] && nums[mid]==nums[end]){
+                if(nums[start]>nums[start+1]) return start;
+                start++;
+                if(nums[end-1]>nums[end]) return end-1;
+                end--;
+            }
+            else if (nums[start]>nums[mid]){
+                end = mid-1;
+
+            }else{
+                start = mid + 1;
+            }
+        }
+        return end;
+    }
 }
