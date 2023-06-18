2bf234672ee7aba23aa031264e7c14801d871d8d
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-06-18 22:20:04 +0530

    Time: 3 ms (7.27%) | Memory: 43.5 MB (11.24%) - LeetSync

--- search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -22,28 +22,34 @@ class Solution {
         }
         return -1;
     }
-    private int findPeak(int[] nums) {
+    private int findPeak(int[] arr) {
           int start = 0;
-        int end = nums.length-1;
-        while(start<end){
+        int end = arr.length-1;
+        while(start<=end){
             int mid = start + (end - start)/2;
-            if(nums[mid]>nums[mid+1]){
+            if(mid<end && arr[mid]>arr[mid+1]){
                 return mid;
-            }else if(mid!=start && nums[mid]<nums[mid-1]){
+            }
+            if(mid>start && arr[mid]<arr[mid-1]){
                 return mid-1;
             }
-            if(nums[start]==nums[mid] && nums[mid]==nums[end]){
-                if(nums[start]>nums[start+1]) return start;
+            if(arr[start]==arr[mid] && arr[mid] == arr[end]){
+                if(start<end && arr[start]>arr[start+1]){
+                    return start;
+                }
                 start++;
-                if(nums[end-1]>nums[end]) return end-1;
+                if(end>start && arr[end]<arr[end-1]){
+                    return end-1;
+                }
                 end--;
-            }
-            else if (nums[start]>nums[mid]){
-                end = mid-1;
-
+            }else if(arr[start]<arr[mid]){
+                start=mid+1;
+            }else if(arr[start]==arr[mid] && arr[mid]>arr[end]){
+                start = mid+1;
             }else{
-                start = mid + 1;
+                end = mid-1;
             }
+
         }
         return end;
     }
