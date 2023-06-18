d245976fd0768cc1169a98d2a90f579dfb35e38e
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-06-18 22:47:50 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (33.74%) - LeetSync

--- search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,56 +1,31 @@
 class Solution {
-    public boolean search(int[] nums, int target) {
-        int peak = findPeak(nums);
-        System.out.println(peak);
-        int found = binarySearch(nums,target,0,peak);
-        if(found == -1) {
-            found = binarySearch(nums,target,peak+1,nums.length-1);
-        }
-        return found!=-1;
-    }
-    private int binarySearch(int[] arr, int target,int start,int end) {
+    public boolean search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
         while(start<=end){
             int mid = start + (end-start)/2;
-            int ans = arr[mid];
-            if(ans==target){
-                return mid;
-            }else if(ans>target){
-                end = mid-1;
-            }else{
-                start = mid+1;
-            }
-        }
-        return -1;
-    }
-    private int findPeak(int[] arr) {
-          int start = 0;
-        int end = arr.length-1;
-        while(start<=end){
-            int mid = start + (end - start)/2;
-            if(mid<end && arr[mid]>arr[mid+1]){
-                return mid;
-            }
-            if(mid>start && arr[mid]<arr[mid-1]){
-                return mid-1;
-            }
-            if(arr[start]==arr[mid] && arr[mid] == arr[end]){
-                if(start<end && arr[start]>arr[start+1]){
-                    return start;
-                }
+            if(arr[mid]==target) return true;
+            if(arr[mid]==arr[start] && arr[mid]==arr[end]){
                 start++;
-                if(end>start && arr[end]<arr[end-1]){
-                    return end-1;
-                }
                 end--;
-            }else if(arr[start]<arr[mid]){
-                start=mid+1;
-            }else if(arr[start]==arr[mid] && arr[mid]>arr[end]){
-                start = mid+1;
-            }else{
-                end = mid-1;
+                continue;
             }
-
+            if(arr[start]<=arr[mid]){
+                // LHS is sorted
+                if(target>=arr[start] && target<arr[mid]){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
+            }else{
+                //RHS is sorted
+                if(target>arr[mid] && target<=arr[end]){
+                    start = mid+1;
+                }else{
+                    end = mid-1;
+                }
+            }
         }
-        return end;
+        return false;
     }
 }
