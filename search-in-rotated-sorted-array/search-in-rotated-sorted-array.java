ea7c85c1357fa7366fc012c99d46f01de8753e7b
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-06-18 22:46:34 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (61.21%) - LeetSync

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,43 +1,24 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        if(nums[0]<=nums[nums.length-1]){
-            return binarySearch(nums,target,0,nums.length-1);
-        }
-        int peak = findPeak(nums);
-        if(target>=nums[0]){
-            return binarySearch(nums,target,0,peak);
-        }else{
-            return binarySearch(nums,target,peak+1,nums.length-1);
-        }
-    }
-    private int findPeak(int[] nums) {
+    public int search(int[] arr, int target) {
         int start = 0;
-        int end = nums.length-1;
-        while(start<=end){
-            int mid = start + (end - start)/2;
-            if(nums[mid]>nums[mid+1]){
-                return mid;
-            }else if(nums[mid]<nums[mid-1]){
-                return mid-1;
-            }else if (nums[start]>nums[mid]){
-                end = mid-1;
-
-            }else{
-                start = mid + 1;
-            }
-         }
-        return -1;
-    }
-    private static int binarySearch(int[] arr, int target,int start,int end) {
+        int end = arr.length - 1;
         while(start<=end){
             int mid = start + (end-start)/2;
-            int ans = arr[mid];
-            if(ans==target){
-                return mid;
-            }else if(ans>target){
-                end = mid-1;
+            if(arr[mid]==target) return mid;
+            if(arr[start]<=arr[mid]){
+                // LHS is sorted
+                if(target>=arr[start] && target<arr[mid]){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
             }else{
-                start = mid+1;
+                //RHS is sorted
+                if(target>arr[mid] && target<=arr[end]){
+                    start = mid+1;
+                }else{
+                    end = mid-1;
+                }
             }
         }
         return -1;
