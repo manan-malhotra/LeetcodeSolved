6ad118513347cf851d330f4d4933730fb0570c1a
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-06-18 11:47:49 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (98.35%) - LeetSync

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,36 +1,18 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int end = nums.length;
-
-        if(nums[0]<=nums[end-1]){
-            return binarySearch(nums,target,0,end-1);
+        if(nums[0]<=nums[nums.length-1]){
+            return binarySearch(nums,target,0,nums.length-1);
         }
-       int peak = findPeak(nums);
-       int first = binarySearch(nums,target,0,peak);
-       if(first!=-1){
-           return first;
-       }
-        return binarySearch(nums,target,peak+1,nums.length-1);
-       
-       
-    }
-    public int binarySearch(int[] nums, int target, int start, int end){
-        while(start<=end){
-            int mid = start + (end - start)/2;
-            if(target==nums[mid]){
-                return mid;
-            }else if(target>nums[mid]){
-                start=mid+1;
-            }else{
-                end = mid-1;
-            }
+        int peak = findPeak(nums);
+        if(target>=nums[0]){
+            return binarySearch(nums,target,0,peak);
+        }else{
+            return binarySearch(nums,target,peak+1,nums.length-1);
         }
-
-        return -1;
     }
-    public int findPeak(int[] nums){
-         int start = 0;
-        int end = nums.length - 1;
+    private int findPeak(int[] nums) {
+        int start = 0;
+        int end = nums.length-1;
         while(start<=end){
             int mid = start + (end - start)/2;
             if(nums[mid]>nums[mid+1]){
@@ -44,6 +26,20 @@ class Solution {
                 start = mid + 1;
             }
          }
+        return -1;
+    }
+    private static int binarySearch(int[] arr, int target,int start,int end) {
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            int ans = arr[mid];
+            if(ans==target){
+                return mid;
+            }else if(ans>target){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
         return -1;
     }
 }
