066110e561e59fcb25af006386c74338212196de
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-06-22 23:14:32 +0530

    Time: 2 ms (100.00%) | Memory: 44.7 MB (45.23%) - LeetSync

--- median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -9,8 +9,7 @@ class Solution {
                 return (nums2[m/2]+nums2[(m-1)/2])/2.0;
             }
             return nums2[m/2];
-        }
-        System.out.println("nums1 "+nums1[0]);
+        }        
         int start = 0;
         int end = n;
         while(start<=end){
@@ -26,10 +25,6 @@ class Solution {
             if(mid2!=0) l2 = nums2[mid2-1];
             if(mid1!=n) r1 = nums1[mid1];
             if(mid2!=m) r2 = nums2[mid2];
-            System.out.println("l1 "+l1);
-            System.out.println("l2 "+l2);
-            System.out.println("r1 "+r1);
-            System.out.println("r2 "+r2);
             if(l1<=r2 && l2<=r1){
                 //ans
                 if((n+m)%2==0){
