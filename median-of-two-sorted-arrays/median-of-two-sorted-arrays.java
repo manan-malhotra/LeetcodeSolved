aded6139ea9f1af3b74d753bc175974856350ee0
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-06-22 23:14:12 +0530

    Time: 44 ms (5.02%) | Memory: 44.7 MB (45.23%) - LeetSync

--- median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,29 +1,49 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        int n = n1 + n2;
-        int[] new_arr = new int[n];
-
-        int i=0, j=0, k=0;
-
-        while (i<=n1 && j<=n2) {
-            if (i == n1) {
-                while(j<n2) new_arr[k++] = nums2[j++];
-                break;
-            } else if (j == n2) {
-                while (i<n1) new_arr[k++] = nums1[i++];
-                break;
+        int n = nums1.length;
+        int m = nums2.length;
+        if(m==0 && n==0) return 0.0;
+        if(n>m) return findMedianSortedArrays(nums2,nums1);
+        if(n==0) {
+            if(m%2==0) {
+                return (nums2[m/2]+nums2[(m-1)/2])/2.0;
             }
-
-            if (nums1[i] < nums2[j]) {
-                new_arr[k++] = nums1[i++];
-            } else {
-                new_arr[k++] = nums2[j++];
-            }
+            return nums2[m/2];
         }
+        System.out.println("nums1 "+nums1[0]);
+        int start = 0;
+        int end = n;
+        while(start<=end){
+            int mid1 = start + (end-start)/2;
+            int mid2= (n+m+1)/2 - mid1;
 
-        if (n%2==0) return (float)(new_arr[n/2-1] + new_arr[n/2])/2;
-        else return new_arr[n/2];
+            int l1 = Integer.MIN_VALUE;
+            int l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE;
+            int r2 = Integer.MAX_VALUE;
+
+            if(mid1!=0) l1 = nums1[mid1-1];
+            if(mid2!=0) l2 = nums2[mid2-1];
+            if(mid1!=n) r1 = nums1[mid1];
+            if(mid2!=m) r2 = nums2[mid2];
+            System.out.println("l1 "+l1);
+            System.out.println("l2 "+l2);
+            System.out.println("r1 "+r1);
+            System.out.println("r2 "+r2);
+            if(l1<=r2 && l2<=r1){
+                //ans
+                if((n+m)%2==0){
+                    return (Math.max(l1,l2)+Math.min(r1,r2))/2.0;
+                }
+                return (Math.max(l1,l2));
+            }else if(l1>r2){
+                end = mid1-1;
+            }else{
+                start = mid1+1;
+            }
+
+
+        }
+        return 0.0;
     }
 }
