8652977f3286d7b84d4eb5ea718b538416220a79
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-06-23 22:57:59 +0530

    Time: 2 ms (100.00%) | Memory: 44.4 MB (56.15%) - LeetSync

--- median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,5 +1,5 @@
 class Solution {
-    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+      public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n = nums1.length;
         int m= nums2.length;
         if(n==0 && m==0) return 0.0;
@@ -8,17 +8,16 @@ class Solution {
             if(m%2==0){
                 return (nums2[m/2]+nums2[(m-1)/2])/2.0;
             }
-            System.out.println("test");
             return nums2[m/2];
         }
         
         int start = 0;
         int end = n;
-        int total = n+m+1;
+        int total = n+m;
 
         while(start<=end){
             int mid1 = start + (end-start)/2;
-            int mid2 = (total/2) - mid1;
+            int mid2 = (total+1)/2 - mid1;
             int left1 = Integer.MIN_VALUE;
             int left2 = Integer.MIN_VALUE;
             int right1 = Integer.MAX_VALUE;
@@ -29,7 +28,7 @@ class Solution {
             if(mid2!=m) right2 = nums2[mid2];
 
             if(left1<=right2 && left2<=right1){
-                if((total-1)%2==0){
+                if(total%2==0){
                     return (Math.max(left1, left2)+Math.min(right1, right2))/2.0;
                 }
                 return Math.max(left1, left2);
