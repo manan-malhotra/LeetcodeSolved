89998e253a025fdb1a10a71e75fcbeaa7e62369e
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-06-23 22:56:18 +0530

    Time: 2 ms (100.00%) | Memory: 45.3 MB (5.18%) - LeetSync

--- median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,42 +1,43 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n = nums1.length;
-        int m = nums2.length;
-        if(m==0 && n==0) return 0.0;
-        if(n>m) return findMedianSortedArrays(nums2,nums1);
-        if(n==0) {
-            if(m%2==0) {
+        int m= nums2.length;
+        if(n==0 && m==0) return 0.0;
+        if(n>m) return findMedianSortedArrays(nums2, nums1);
+        if(n==0){
+            if(m%2==0){
                 return (nums2[m/2]+nums2[(m-1)/2])/2.0;
             }
+            System.out.println("test");
             return nums2[m/2];
-        }        
+        }
+        
         int start = 0;
         int end = n;
+        int total = n+m+1;
+
         while(start<=end){
             int mid1 = start + (end-start)/2;
-            int mid2= (n+m+1)/2 - mid1;
+            int mid2 = (total/2) - mid1;
+            int left1 = Integer.MIN_VALUE;
+            int left2 = Integer.MIN_VALUE;
+            int right1 = Integer.MAX_VALUE;
+            int right2 = Integer.MAX_VALUE;
+            if(mid1!=0) left1 = nums1[mid1-1];
+            if(mid2!=0) left2 = nums2[mid2-1];
+            if(mid1!=n) right1 = nums1[mid1];
+            if(mid2!=m) right2 = nums2[mid2];
 
-            int l1 = Integer.MIN_VALUE;
-            int l2 = Integer.MIN_VALUE;
-            int r1 = Integer.MAX_VALUE;
-            int r2 = Integer.MAX_VALUE;
-
-            if(mid1!=0) l1 = nums1[mid1-1];
-            if(mid2!=0) l2 = nums2[mid2-1];
-            if(mid1!=n) r1 = nums1[mid1];
-            if(mid2!=m) r2 = nums2[mid2];
-            if(l1<=r2 && l2<=r1){
-                //ans
-                if((n+m)%2==0){
-                    return (Math.max(l1,l2)+Math.min(r1,r2))/2.0;
+            if(left1<=right2 && left2<=right1){
+                if((total-1)%2==0){
+                    return (Math.max(left1, left2)+Math.min(right1, right2))/2.0;
                 }
-                return (Math.max(l1,l2));
-            }else if(l1>r2){
+                return Math.max(left1, left2);
+            }else if(left1>right2){
                 end = mid1-1;
             }else{
                 start = mid1+1;
             }
-
 
         }
         return 0.0;
