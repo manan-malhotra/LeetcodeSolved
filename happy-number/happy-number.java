7cb99c78acbcb4695171e51dcce2f7303976b29d
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-06-26 22:13:26 +0530

    Time: 10 ms (5.25%) | Memory: 39.7 MB (72.17%) - LeetSync

--- happy-number/happy-number.java
+++ happy-number/happy-number.java
@@ -1,22 +1,24 @@
 class Solution {
     public boolean isHappy(int n) {
-        int s = n,f = n; // slow , fast
-        
+        int slow = n;
+        int fast = n;
         do{
-            s = compute(s); // slow computes only once
-            f = compute(compute(f)); // fast computes 2 times
-            
-            if(s == 1)return true; // if we found 1 then happy indeed !!!
-        }while(s != f); // else at some point they will meet in the cycle
+            slow = findSquare(slow);
+            fast = findSquare(findSquare(fast));
+            System.out.println(slow);
+            if(slow==1){
+                return true;
+            }
+        }while(slow!=fast);
+        return false;
         
-        return false; // it's a cycle , not happy at all !!!        
     }
-    public int compute(int n){
-        int s = 0;
-        while(n != 0){
-            s += (n%10)*(n%10);
-            n = n/10;
+    public int findSquare(int n){
+        int square = 0;
+        while(n!=0){
+            square += (n%10)*(n%10);
+            n=n/10;
         }
-        return s;
+        return square;
     }
 }
