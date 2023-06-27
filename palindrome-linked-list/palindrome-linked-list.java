2021a568dbefa044fb2e1b44140d05de87fa949b
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-06-27 23:12:51 +0530

    Time: 3 ms (99.66%) | Memory: 56.6 MB (78.74%) - LeetSync

--- palindrome-linked-list/palindrome-linked-list.java
+++ palindrome-linked-list/palindrome-linked-list.java
@@ -1,43 +1,26 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode mid = findMid(head);
-        mid = reverseList(mid);
-        while(head!=null && mid!=null){
-            if(head.val!=mid.val) return false;
-            head=head.next;
-            mid=mid.next;
+        ListNode slow = head, fast = head, prev, temp;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev = slow;
+        slow = slow.next;
+        prev.next = null;
+        while (slow != null) {
+            temp = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = temp;
+        }
+        fast = head;
+        slow = prev;
+        while (slow != null) {
+            if (fast.val != slow.val) return false;
+            fast = fast.next;
+            slow = slow.next;
         }
         return true;
-        
-    }
-    public ListNode findMid(ListNode head){
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        return slow;
-    }
-    public ListNode reverseList(ListNode head) {
-        ListNode p1 = head;
-        ListNode p2 = null;
-        while(p1!=null){
-            p1 = head.next;
-            head.next = p2;
-            p2 = head;
-            head = p1;
-        }
-        return p2;
     }
 }
