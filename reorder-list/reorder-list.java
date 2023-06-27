2280d63977e22a8bd1093a62de512b7878727c90
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-06-27 23:44:05 +0530

    Time: 116 ms (5.08%) | Memory: 46.7 MB (6.41%) - LeetSync

---
+++ reorder-list/reorder-list.java
@@ -0,0 +1,74 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        ListNode mid = middleNode(head);
+
+        ListNode hs = reverseList(mid);
+        display(head);
+        display(hs);
+        ListNode hf = head;
+
+        // rearrange
+        while (hf != null && hs != null) {
+            ListNode temp = hf.next;
+            hf.next = hs;
+            hf = temp;
+
+            temp = hs.next;
+            hs.next = hf;
+            hs = temp;
+        }
+        // next of tail to null
+        if (hf != null) {
+            hf.next = null;
+        }
+    }
+    public void display(ListNode head){
+        ListNode temp = head;
+        while(temp!=null){
+            System.out.print(temp.val + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    public ListNode middleNode(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode present = head;
+        ListNode next = present.next;
+
+        while (present != null) {
+            present.next = prev;
+            prev = present;
+            present = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+        return prev;
+    }
+}
