499dd96f4d54b5841ea0c2ad70cb167fbf476a3e
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-06-28 22:58:54 +0530

    Time: 12 ms (6.11%) | Memory: 43.9 MB (9.69%) - LeetSync

---
+++ reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        int size = findSize(head);
+        int totalDiv = size/k;
+        int startIndex = 1;
+        int endIndex = k;
+        while(totalDiv!=0){
+            System.out.println(head.val+" "+startIndex+" "+endIndex);
+            head = reverseBetween(head,startIndex,endIndex);
+            startIndex=endIndex+1;
+            endIndex += k; 
+            totalDiv--;
+        }
+        return head;
+    }
+    public int findSize(ListNode head){
+        int size = 0;
+        ListNode temp = head;
+        while(temp!=null){
+            size++;
+            temp=temp.next;
+        }
+        return size;
+    }
+    public ListNode reverseBetween(ListNode head, int left, int right){
+        if(left==right) return head;
+        ListNode current = head;
+        ListNode prev = null;
+        for(int i=0;current!=null && i<left-1;i++){
+            prev = current;
+            current = current.next;
+        }
+        ListNode last = prev;
+        ListNode newEnd = current;
+        ListNode next = current.next;
+        for(int i=0; current!=null && i< right-left+1;i++){
+            current.next=prev;
+            prev=current;
+            current=next;
+            if(next!=null) next=next.next;
+        }
+        if(last!=null){
+            last.next=prev;
+        }else{
+            head = prev;
+        }
+        newEnd.next=current;
+        return head;
+    }
+}
