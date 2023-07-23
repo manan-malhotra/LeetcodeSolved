26f56bea33aeab60112c7206be2795aba17f4bbf
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-07-23 17:39:43 +0530

    Time: 1 ms (91.25%) | Memory: 39.9 MB (28.60%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,7 +1,13 @@
+/**
+ * The Solution class contains a method isHappy that determines if a given number is a happy number.
+ */
+
 class Solution {
     public boolean isHappy(int n) {
         int slow = n;
         int fast = n;
+       // The code snippet is implementing the Floyd's cycle-finding algorithm to determine if a given
+       // number is a happy number.
         do{
             slow = findSquare(slow);
             fast = findSquare(findSquare(fast));
@@ -13,6 +19,7 @@ class Solution {
         
     }
     public int findSquare(int n){
+        // The code snippet is calculating the sum of the squares of the digits of a given number.
         int square = 0;
         while(n!=0){
             int temp = n%10;
