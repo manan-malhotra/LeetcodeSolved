da4f75a43723de6e50560f9ff3b0b1e43289cfb5
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-07-25 20:51:37 +0530

    Time: 2 ms (91.70%) | Memory: 40.5 MB (68.66%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+        for(char i:s.toCharArray()){
+            if(i=='(' || i=='[' || i=='{'){
+                stack.push(i);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char check = stack.pop();
+                if(check=='(' && i!=')') return false;
+                if(check=='[' && i!=']') return false;
+                if(check=='{' && i!='}') return false;
+            }
+        }
+            if(stack.isEmpty()) return true;
+            return false;
+    }   
+}
