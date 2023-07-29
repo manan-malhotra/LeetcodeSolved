fcb208af77226d063f887ff8a717843932adf689
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-07-29 13:44:00 +0530

    Time: 0 ms (100.00%) | Memory: 39.9 MB (99.99%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,15 +1,12 @@
 class Solution {
     public double myPow(double x, int n) {
 
-        return power(x,n);
-    }
-    public double power(double x,int n){
         if(n<0) return 1/x * myPow(1/x, -(n+1));
     if(n==0) return 1;
     if(n==2) return x*x;
     if(n%2==0) return myPow( myPow(x, n/2), 2);
     else return x*myPow( myPow(x, n/2), 2);
 
-
     }
+    
 }
