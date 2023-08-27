ac8d240b3290ec49c998a58586f147dbfe2f7fd1
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-08-27 16:34:03 +0530

    Time: 3 ms (99.52%) | Memory: 45.3 MB (21.03%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,26 +1,16 @@
 class Solution {
-        public List<Integer> findClosestElements(int[] arr, int k, int c) {
-            int n = arr.length;
-            if(c>=arr[n-1]){
-                return adde(arr,n-k,n);
-            }
-            else if(c<=arr[0]){
-                return adde(arr,0,k);
-            }else{
-            int l = 0;
-            int r = n-1;
-            while(r-l!=k-1){
-                int leftVal = Math.abs(arr[l]-c);
-                int rightVal = Math.abs(arr[r]-c);
-                if(leftVal>rightVal){
-                    l++;
-                }else{
-                    r--;
-                }
-            }
-            return adde(arr,l,r+1);
+            public List<Integer> findClosestElements(int[] A, int k, int x) {
+        int left = 0, right = A.length - k;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (x - A[mid] > A[mid + k] - x)
+                left = mid + 1;
+            else
+                right = mid;
         }
-        }
+        return adde(A,left,left+k);
+        // Arrays.stream(A, left, left + k).boxed().collect(Collectors.toList());
+    }
         public List<Integer> adde(int[] arr, int l, int r){
             List<Integer> a = new ArrayList<Integer>();
             for(int i=l;i<r;i++) a.add(arr[i]);
