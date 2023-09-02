4f5050a0feb91aae2093d776dc067420dca2b10b
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-09-02 15:51:42 +0530

    Time: 3 ms (41.29%) | Memory: 42.9 MB (91.37%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -28,16 +28,17 @@ class Solution {
         q.offer(root);
         while(!q.isEmpty()){
             int levelSize = q.size();
-            for(int i=0;i<levelSize-1;i++){
+            for(int i=0;i<levelSize;i++){
                 Node curr = q.poll();
                 if(curr.left!=null) q.offer(curr.left);
                 if(curr.right!=null) q.offer(curr.right);
                 curr.next=q.peek();
+                if(i==levelSize-1) curr.next=null;
             }
-            Node curr = q.poll();
-            if(curr.left!=null) q.offer(curr.left);
-            if(curr.right!=null) q.offer(curr.right);
-            curr.next=null;
+            // Node curr = q.poll();
+            // if(curr.left!=null) q.offer(curr.left);
+            // if(curr.right!=null) q.offer(curr.right);
+            // curr.next=null;
         }
         return root;
     }
