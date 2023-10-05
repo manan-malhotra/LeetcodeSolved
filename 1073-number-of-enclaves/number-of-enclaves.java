384bfda597354d9c8bc1ee2cd35a3a126676fefb
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-10-05 22:27:38 +0530

    Time: 6 ms (98.46%) | Memory: 55.2 MB (42.89%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -5,18 +5,18 @@ class Solution {
         int m = grid[0].length;
         for(int i=0;i<n;i++){
             if(grid[i][0]==1){
-                bfs(grid,i,0,n,m);
+                dfs(grid,i,0,n,m);
             }
             if(grid[i][m-1]==1){
-                bfs(grid,i,m-1,n,m);
+                dfs(grid,i,m-1,n,m);
             }
         }
         for(int j=0;j<m;j++){
             if(grid[0][j]==1){
-                bfs(grid,0,j,n,m);
+                dfs(grid,0,j,n,m);
             }
             if(grid[n-1][j]==1){
-                bfs(grid,n-1,j,n,m);
+                dfs(grid,n-1,j,n,m);
             }
         }
         for(int i=0;i<n;i++){
@@ -26,13 +26,13 @@ class Solution {
         }
         return count;
     }
-    public void bfs(int[][] grid, int i,int j, int n, int m){
+    public void dfs(int[][] grid, int i,int j, int n, int m){
         if(i<0 || j<0 || i>=n || j>=m || grid[i][j]!=1) return;
         grid[i][j]=0;
-        bfs(grid,i+1,j,n,m);
-        bfs(grid,i-1,j,n,m);
-        bfs(grid,i,j+1,n,m);
-        bfs(grid,i,j-1,n,m);
+        dfs(grid,i+1,j,n,m);
+        dfs(grid,i-1,j,n,m);
+        dfs(grid,i,j+1,n,m);
+        dfs(grid,i,j-1,n,m);
 
     }
 }
