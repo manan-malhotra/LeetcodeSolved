ce30c6a8e1285f281eae5a1da47a3843a6d1ac8d
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-11-25 02:57:56 +0530

    Time: 180 ms (30.91%) | Memory: 86 MB (7.27%) - LeetSync

--- 2171-second-minimum-time-to-reach-destination/second-minimum-time-to-reach-destination.java
+++ 2171-second-minimum-time-to-reach-destination/second-minimum-time-to-reach-destination.java
@@ -1,30 +1,38 @@
 class Solution {
-    public int secondMinimum(int n, int[][] edges, int time, int change) {
-        Map<Integer, List<Integer>> g = new HashMap();
-        for(int[] e : edges) {
-            int u  = e[0], v = e[1];
-            g.computeIfAbsent(u, x -> new ArrayList()).add(v);
-            g.computeIfAbsent(v, x -> new ArrayList()).add(u);
-        }
-        PriorityQueue<int[]> q = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-        q.offer(new int[]{1, 0});
-        int[] uniqueVisit = new int[n+1]; // uniqueVisit limit to 2 <==> relax twice at most
-        int[] dis = new int[n+1];
-        Arrays.fill(dis, -1);
-        while(!q.isEmpty()) {
-            int size = q.size();
-            int[] cur = q.poll();
-            int node = cur[0], t = cur[1];  // arriving time
-            if(dis[node] == t || uniqueVisit[node] >= 2) continue; // skip if it's same value or has been relaxed twice already
-            uniqueVisit[node]++; 
-            dis[node] = t;
-            if(node == n && uniqueVisit[node] == 2) return dis[node];
-            // generate leaving time (waiting the green light)
-            if((t / change) % 2 != 0) t = (t/change + 1) * change;
-            for(int nei : g.getOrDefault(node, new ArrayList<>())) {
-                q.offer(new int[]{nei, t + time});
-            }
-        }
-        return -1;
+    int stepsTime(int steps, int time, int change) {
+    int res = 0;
+    while (--steps > 0) {
+        res += time;
+        if ((res / change) % 2 == 1)
+            res = (res / change + 1) * change;
     }
+    return res + time;
+}    
+public int secondMinimum(int n, int[][] edges, int time, int change) {
+    List<List<Integer>> al = new ArrayList<List<Integer>>();
+    int[] minSteps = new int[n + 1];
+    Arrays.fill(minSteps, 10001);
+    for (int i = 0; i <= n; ++i)
+        al.add(new ArrayList<Integer>());
+    for (var e : edges) {
+        al.get(e[0]).add(e[1]);
+        al.get(e[1]).add(e[0]);
+    }
+    int step = -1;
+    List<Integer> q = new ArrayList<>(List.of(1));
+    while(!q.isEmpty() && ++step <= minSteps[n] + 1) {
+        List<Integer> q1 = new ArrayList<>();
+        for (int i : q) {
+            if (step == minSteps[i] || step > minSteps[i] + 1)
+                continue;
+            minSteps[i] = Math.min(minSteps[i], step);
+            if (i == n && step > minSteps[n])
+                return stepsTime(step, time, change);
+            for (int j : al.get(i))
+                q1.add(j);
+        }
+        q = q1;
+    }
+    return stepsTime(minSteps[n] + 2, time, change);
+}
 }
