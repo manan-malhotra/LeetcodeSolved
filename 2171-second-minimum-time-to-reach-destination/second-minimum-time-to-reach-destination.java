3ccb6189712ee6bdf269c3195fc39215f95d514e
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-11-25 04:07:04 +0530

    Time: 47 ms (92.73%) | Memory: 56.6 MB (18.18%) - LeetSync

--- 2171-second-minimum-time-to-reach-destination/second-minimum-time-to-reach-destination.java
+++ 2171-second-minimum-time-to-reach-destination/second-minimum-time-to-reach-destination.java
@@ -1,61 +1,86 @@
+/*
+    If it was needed to find the minimum time to reach vertex n from vertex 1, 
+	we would have done a simple BFS and stored the minimum time to reach each vertex. 
+	Here we need to find the second minimum time, so basically, we need to reach each vertex 
+	at most twice and store the minimum and second minimum time to reach every vertex. 
+	Note: second minimum time should be strictly greater than the minimum time. 
+	We can apply the same BFS with modification that we reach every vertex for the second time 
+	and the second arriving time should be strictly greater than the first arriving time.
+*/
 class Solution {
-    public int secondMinimum(int n, int[][] edges, int time, int change) {
-		// build the graph
-        Map<Integer, Set<Integer>> graph = new HashMap<>();
-        for (int i = 1; i <= n; i++) graph.put(i, new HashSet<>());
-        for (int[] e : edges) {
-            graph.get(e[0]).add(e[1]);
-            graph.get(e[1]).add(e[0]);
-        }
-		// keep two min times for each node
-        int[][] visited = new int[n+1][2]; //{min, secMin}
-        for (int[] v : visited) {
-            Arrays.fill(v, Integer.MAX_VALUE);
+    public int secondMinimum(int n, int[][] edges, int time, int change) {    
+        
+        // creating the graph
+        List<Integer> list[]=new ArrayList[n+1];
+        for(int i=0;i<=n;i++)
+            list[i]=new ArrayList<>();
+        for(int []e:edges){
+            list[e[0]].add(e[1]);
+            list[e[1]].add(e[0]);
         }
         
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[1], b[1]));
-        pq.offer(new int[]{1, 0}); // {curNode, curTime}
+        // for each vertex we store two arriving times.
+        long [][]dist=new long [n+1][2];
+        for(long []I:dist)
+            Arrays.fill(I,Long.MAX_VALUE);
+                
+        Queue<Duo> q=new LinkedList<>();
         
-        int min = Integer.MAX_VALUE, secMin = min;
+        // we start from vertex 1.
+        q.offer(new Duo(1,0l));
+        dist[1][0]=0; 
         
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int curNode = cur[0], curTime = cur[1];
-
-			// update the global value when reach target
-            if (curNode == n) {
-                if (curTime < min) {
-                    secMin = min;
-                    min = curTime;
-                } else if (curTime != min && curTime < secMin) {
-                    secMin = curTime;
+        Duo p;
+        long val,wait;
+        while(q.size()>0){           
+            p=q.poll();
+            
+            /*
+                Calculation for the arriving time to the adjacent vertex:
+                We have reached the current vertex p.idx at time p.val.
+                Now, we need to find out the current state of the traffic light.
+                Number of changes in traffic light=(p.val/change).
+                If number of changes is odd, starting from green, 
+				the current state will be red and vice versa.
+                If the current state is red, we need to wait for it to become green(another change) to leave the vertex.
+                waiting time=change-(p.val%change);
+                If the current state is green, we can instantly leave the vertex, 
+                waiting time=0.
+            */
+            wait=0l;
+            if((p.val/change)%2==1){
+                wait=change-(p.val%change);
+            }                
+            val=p.val+wait+(long)time; // time to reach adjacent node from the current node.
+            
+            for(int i:list[p.idx]){
+                
+                if(dist[i][0]>val){
+                    // arriving for the first time to this node.
+                    dist[i][0]=val;
+                    q.offer(new Duo(i,val));
                 }
-            }
-            
-			// calculate if we need to wait for the green light, if change is 5, then:
-			// 0~4 green
-			// 5~9 red
-			// 10 ~14 green
-			// ...
-            if ((curTime / change) % 2 ==1) {
-                curTime += change - (curTime % change);
-            }
-            
-            int nextTime = curTime + time;
-            
-            for (int nxt : graph.get(curNode)) {
-                int nxtMin = visited[nxt][0], nxtSecMin = visited[nxt][1];
-                if (nextTime < nxtMin) { // update min
-                    visited[nxt][1] = nxtMin;
-                    visited[nxt][0] = nextTime;
-                    pq.offer(new int[]{nxt, nextTime});
-                } else if (nextTime != nxtMin && nextTime < nxtSecMin) { // update secMin
-                    visited[nxt][1] = nextTime;
-                    pq.offer(new int[]{nxt, nextTime});
-                }
+                
+                else if(val>dist[i][0]&&dist[i][1]>val){
+                    // arriving for the second time to this node with second arriving time strictly greater than the first arriving time.
+                    
+                    if(i==n) // returning the answer
+                        return (int)val;
+                    
+                    dist[i][1]=val;
+                    q.offer(new Duo(i,val));
+                }  
+                
             }
         }
-  
-        return secMin;
+        return -1;
+    }
+}
+class Duo{
+    int idx;
+    long val;
+    Duo(int i, long v){
+        idx=i;
+        val=v;
     }
 }
