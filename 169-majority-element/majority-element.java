80555b20f9124203582364c47db3f13ca02b1c43
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-02-05 12:25:16 +0530

    Time: 1 ms (99.81%) | Memory: 50.6 MB (34.51%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        // Moore voting
+        int res = 0, count = 0;
+        for(int num:nums){
+            if(count==0){
+                res=num;
+            }
+            if(num!=res){
+                count--;
+            }else{
+                count++;
+            }
+        }
+        return res;
+    }
+}
