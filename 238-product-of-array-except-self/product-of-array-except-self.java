95078878f2ccfa10288ab8cc50bfbe7dae31960c
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-05-15 12:48:35 +0530

    Time: 2 ms (83.87%) | Memory: 55.4 MB (27.88%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -6,11 +6,11 @@ class Solution {
         for(int i=0;i<n;i++){
             ans[i]=pre;
             pre*=nums[i];
-        }
-        int post = 1;
+        } 
+        pre = 1;
         for(int i=n-1;i>=0;i--){
-            ans[i]*=post;
-            post*=nums[i];
+            ans[i]*=pre;
+            pre*=nums[i];
         }
         return ans;
     }
