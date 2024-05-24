e4119e6414f0b63853a5835876d790b170651011
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-05-24 19:39:16 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (35.74%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,42 +1,23 @@
 class Solution {
-    public int trap(int[] A) {
-        int n = A.length;
-        int[] GFR = new int[n];
-        int[] GFL = new int[n];
-        Stack<Integer> st = new Stack<>();
-        for(int i=n-1;i>=0;i--){
-            while(!st.isEmpty() && A[st.peek()]<A[i]){
-                st.pop();
-            }
-            if(st.isEmpty()){
-                GFR[i] = -1;
-                st.push(i);
-            }else{
-                GFR[i] = st.peek();
-            }
-        }
-        while(!st.isEmpty()){
-            st.pop();
-        }
-        for(int i=0;i<n;i++){
-            while(!st.isEmpty() && A[st.peek()]<A[i]){
-                st.pop();
-            }
-            if(st.isEmpty()){
-                GFL[i] = -1;
-                st.push(i);
-            }else{
-                GFL[i] = st.peek();
+    public int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = height[left];
+        int rightMax = height[right];
+        int water = 0;
+
+        while (left < right) {
+            if (leftMax < rightMax) {
+                left++;
+                leftMax = Math.max(leftMax, height[left]);
+                water += leftMax - height[left];
+            } else {
+                right--;
+                rightMax = Math.max(rightMax, height[right]);
+                water += rightMax - height[right];
             }
         }
-        int sum = 0;
-        for(int i=0;i<n;i++){
-            if(GFR[i]!=-1 && GFL[i]!=-1){
-                int min = Math.min(A[GFR[i]],A[GFL[i]]);
-                sum+=min-A[i];
-                
-            }
-        }
-        return sum;
+
+        return water;        
     }
 }
