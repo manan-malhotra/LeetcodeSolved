a7eb9f33e249941b798e3a41da4cfb155cbad9f2
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-05-25 13:56:42 +0530

    Time: 31 ms (78.27%) | Memory: 56.8 MB (22.79%) - LeetSync

--- 931-maximum-frequency-stack/maximum-frequency-stack.java
+++ 931-maximum-frequency-stack/maximum-frequency-stack.java
@@ -1,43 +1,36 @@
 class FreqStack {
-    private final Map<Integer, Stack<Integer>> stacks;
-    private final Map<Integer, Integer> frequencies;
-    private int maxFrequency;
-
+    Map<Integer,Integer> cnt;//frequency count for every digit
+    int max;//max frequency present for any num(we don't know which)
+    
+    //used to store frequency-wise stacks of inputs.eg.
+    //([2,1,2,5,5,5])(1 freq-[2,1,5],2 freq-[2,5],3 freq-[5])
+    Map<Integer,ArrayList<Integer>> stack;//stack assigned with num of frequencies
     public FreqStack() {
-        this.stacks = new HashMap<>();
-        this.frequencies = new HashMap<>();
-        this.maxFrequency = 0;
+        //initialise our members
+        cnt=new HashMap<>();
+        stack=new HashMap<>();
+        max=0;
     }
     
-    public void push(final int val) {
-        final int frequency = frequencies.getOrDefault(val, 0) + 1;
-
-        this.maxFrequency = Math.max(frequency, this.maxFrequency);
-
-        frequencies.put(val, frequency);
-
-        this.stacks.putIfAbsent(frequency, new Stack<>());
-        this.stacks.get(frequency).add(val);
+    public void push(int val) {
+        //increment val count
+        int valCnt=cnt.getOrDefault(val,0)+1;
+        cnt.put(val,valCnt);
+        //if current cal count is more than max
+        if(valCnt>max){
+            max=valCnt;
+            stack.put(max,new ArrayList<>());//add new stack for this new max count
+        }
+        stack.get(valCnt).add(val);//add this num to its stacks
     }
     
     public int pop() {
-        final Stack<Integer> stack = this.stacks.get(this.maxFrequency);
-        final int val = stack.pop();
-
-        if(stack.isEmpty())
-            this.stacks.remove(this.maxFrequency--);
-
-        final int frequency = this.frequencies.get(val);
-
-        this.frequencies.put(val, frequency - 1);
-
-        return val;
+        //remove last element of max freq
+        int res=stack.get(max).remove(stack.get(max).size()-1);
+        cnt.put(res,cnt.get(res)-1);//reduce freq of number
+        if(stack.get(max).size()==0){//if now this freq have no num left
+            max--;//reduce the freq
+        }
+        return res;
     }
 }
-
-/**
- * Your FreqStack object will be instantiated and called as such:
- * FreqStack obj = new FreqStack();
- * obj.push(val);
- * int param_2 = obj.pop();
- */
