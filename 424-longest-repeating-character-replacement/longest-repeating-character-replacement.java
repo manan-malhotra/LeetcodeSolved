fb465a8c43a8b2b4e4ade477c9e5c7244e5046b5
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-05-28 20:31:05 +0530

    Time: 28 ms (26.89%) | Memory: 44.4 MB (25.44%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,17 +1,41 @@
+// Unique
+
 class Solution {
     public int characterReplacement(String s, int k) {
-        int i=0, j=0,ans=0,current=0;
-        HashMap<Character,Integer> map = new HashMap<>();
-        while(j!=s.length()){
-            map.put(s.charAt(j),map.getOrDefault(s.charAt(j),0)+1);
-            current = Math.max(current,map.get(s.charAt(j)));
-            if(j-i+1-current>k){
-                map.put(s.charAt(i),map.get(s.charAt(i))-1);
-                i++;
+        int left = 0, right = 0, max = 0;
+        int maxCount = 0;
+        int n = s.length();
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        while (right < n) {
+            // Perform operation on right
+            char rightChar = s.charAt(right);
+            map.put(rightChar, map.getOrDefault(rightChar, 0) + 1);
+            maxCount = Math.max(maxCount, map.get(rightChar));
+
+            // If the window size minus the count of the most frequent character is greater than k
+            // Reduce the window from the left
+            // maxCount is the maximum of same character, the condition dicdated we can change anything to the required character 
+            //so the remaining characters == length-maxCount. Here "current_length" is right-left+1. hope it is clear now
+            if (right - left + 1 - maxCount > k) {
+                char leftChar = s.charAt(left);
+                map.put(leftChar, map.get(leftChar) - 1);
+
+                /* this part is not required because we dont have to do 
+                anything with size() of map reference fruits into basket
+                
+                if(map.get(leftChar)==0) map.remove(leftChar);
+
+                */
+                
+                left++;
             }
-            ans = Math.max(ans,j-i+1);
-            j++;
+
+            // Compute the max length
+            max = Math.max(max, right - left + 1);
+            // Increment right to calculate for the next value
+            right++;
         }
-        return ans;
+        return max;
     }
 }
