7f37127f059b3676cc77f789d6fb14485ebed137
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-02 15:23:27 +0530

    Time: 2 ms (44.29%) | Memory: 45.1 MB (5.03%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,21 +1,32 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-   List<List<Integer>> list = new ArrayList<>();
-   // Arrays.sort(nums); // not necessary
-   backtrack(list, new ArrayList<>(), nums);
-   return list;
-}
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums.length == 1) {
+            List<Integer> singleList = new ArrayList<>();
+            singleList.add(nums[0]);
+            res.add(singleList);
+            return res;
+        }
 
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
-   if(tempList.size() == nums.length){
-      list.add(new ArrayList<>(tempList));
-   } else{
-      for(int i = 0; i < nums.length; i++){ 
-         if(tempList.contains(nums[i])) continue; // element already exists, skip
-         tempList.add(nums[i]);
-         backtrack(list, tempList, nums);
-         tempList.remove(tempList.size() - 1);
-      }
-   }
-} 
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            int[] remainingNums = new int[nums.length - 1];
+            int index = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (j != i) {
+                    remainingNums[index] = nums[j];
+                    index++;
+                }
+            }
+            
+            List<List<Integer>> perms = permute(remainingNums);
+            for (List<Integer> p : perms) {
+                p.add(n);
+            }
+            
+            res.addAll(perms);
+        }
+        
+        return res;        
+    }
 }
