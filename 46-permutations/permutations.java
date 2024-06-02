42b891852cc48721dae6ed66d4ff9f6829767637
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-02 15:23:06 +0530

    Time: 2 ms (44.29%) | Memory: 44.2 MB (72.46%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,31 +1,21 @@
 class Solution {
-    public void swap(List<Integer> sl, int i, int j) {
-        int temp = sl.get(i);
-        sl.set(i, sl.get(j));
-        sl.set(j, temp);
-    }
-
-    public void permutations(List<List<Integer>> ml, List<Integer> sl, int i) {
-        if (i == sl.size()) {
-            ml.add(new ArrayList<>(sl));
-            return;
-        }
-
-        for (int j = i; j < sl.size(); j++) {
-            swap(sl, i, j);
-            permutations(ml, sl, i + 1);
-            swap(sl, i, j);
-        }
-
-    }
-
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ml = new ArrayList<>();
-        List<Integer> sl = new ArrayList<>();
-        for (int el : nums) {
-            sl.add(el);
-        }
-        permutations(ml, sl, 0);
-        return ml;
-    }
+   List<List<Integer>> list = new ArrayList<>();
+   // Arrays.sort(nums); // not necessary
+   backtrack(list, new ArrayList<>(), nums);
+   return list;
+}
+
+private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
+   if(tempList.size() == nums.length){
+      list.add(new ArrayList<>(tempList));
+   } else{
+      for(int i = 0; i < nums.length; i++){ 
+         if(tempList.contains(nums[i])) continue; // element already exists, skip
+         tempList.add(nums[i]);
+         backtrack(list, tempList, nums);
+         tempList.remove(tempList.size() - 1);
+      }
+   }
+} 
 }
