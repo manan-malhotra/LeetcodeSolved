e7daffc985e99140f16b776880d8bcc6788483e5
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-07 13:57:05 +0530

    Time: 2 ms (85.25%) | Memory: 44.3 MB (73.62%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] arr, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        rec(0,target,list,new ArrayList<>(),arr);
+        return list;
+    }
+
+    public void rec(int i, int target, List<List<Integer>> list, List<Integer> temp, int[] arr){
+        if(target==0){
+            list.add(new ArrayList<>(temp));
+            return;
+        }
+        if(i==arr.length) return;
+        if(target<0) return;
+        temp.add(arr[i]);
+        rec(i,target-arr[i],list,temp,arr);
+        temp.remove(temp.size()-1);
+        rec(i+1,target,list,temp,arr);
+    }
+}
