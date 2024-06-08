4e03e85ca263866e5ab1ec6b269d280f5fd8c318
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-08 14:01:04 +0530

    Time: 5 ms (87.24%) | Memory: 40.9 MB (59.18%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,44 +1,79 @@
 class Solution {
-    public void solveSudoku(char[][] board) {
-        solve(board);
-    }
 
-    public boolean solve (char[][] board){
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-
-                if(board[i][j]=='.'){
-                    for(char digit='1';digit<='9';digit++){
-                        if(isValid(board,i,j,digit)){
-                            board[i][j]=digit;
-                            if(solve(board)==true){
-                                return true;
-                            }
-                            else{
-                                board[i][j]='.';
-                            }
-                        }
-                    }
+    public boolean isSafe(char[][] board ,int val , int row, int col)
+        {
+            for(int i=0;i<board.length;i++)
+            {
+                if(board[row][i] == (char)(val+'0'))
+                {
                     return false;
                 }
             }
+
+            for(int j = 0;j<board.length;j++)
+            {
+                if(board[j][col] == (char)(val+'0'))
+                    return false;
+            }
+
+            int sr = row/3 * 3;
+            int sc = col/3 * 3;
+
+            for(int i = sr ;i<sr+3 ;i++)
+            {
+             for(int j = sc ; j<sc+3;j++)
+             {
+                 if(board[i][j] == (char)(val+'0'))
+                 {
+                     return false;
+                 }
+             }
+            }
+return true;
         }
-        return true;
+
+    public boolean helper(char[][] board, int row, int col)
+    {
+        if(row == board.length)
+            return true;
+        int nrow = 0 , ncol = 0 ; 
+        if(col == board.length-1)
+        {
+            nrow = row+1;
+            ncol = 0;
+        }
+        else
+        {
+            nrow = row;
+            ncol = col+1;
+        }
+
+        if(board[row][col] != '.')
+        {
+            if(helper(board,nrow,ncol))
+                return true;
+        }
+        else
+        {
+            for(int i=1;i<=9;i++)
+            {
+                if(isSafe(board,i,row,col))
+                {
+                    board[row][col] = (char)(i+'0');
+                    if(helper(board,nrow,ncol))
+                        return true;
+                    else
+                    {
+                        board[row][col] = '.';
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 
-    public boolean isValid(char[][] board,int row,int col,char digit){
-        for(int i=0;i<9;i++){
-            if(board[i][col]==digit){
-                return false;
-            }
-            if(board[row][i]==digit){
-                return false;
-            }
-            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==digit){
-                return false;
-            }
-            
-        }
-        return true;
+    public void solveSudoku(char[][] board) {
+        helper(board,0,0);
     }
 }
