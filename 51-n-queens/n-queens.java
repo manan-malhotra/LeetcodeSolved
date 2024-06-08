e39aad143cf2fa7a8a9641164ac0247ffa06927b
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-08 12:56:54 +0530

    Time: 5 ms (41.82%) | Memory: 45.2 MB (5.30%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,48 +1,47 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans = new ArrayList<>();
-        List<String> temp = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            StringBuilder sb = new StringBuilder("");
-            for(int j=0;j<n;j++){
-                sb.append(".");
-            }
-            temp.add(sb.toString());
-        }
-        solve(ans,temp,0);
-        return ans;
+        List<List<String>> res = new ArrayList<>();
+        backtrack(res, new ArrayList<>(), 0, n);
+        return res;
     }
-    public void solve(List<List<String>> ans, List<String> temp, int i){
-        if(i==temp.size()){
-            ans.add(new ArrayList<>(temp));
-            return;
-        }
-        for(int j=0;j<temp.size();j++){
-            if(isValid(temp,i,j)){
-                StringBuilder newRow=new StringBuilder(temp.get(i));
-                newRow.setCharAt(j,'Q');
-                temp.set(i,newRow.toString());
-                solve(ans,temp,i+1);
-                newRow.setCharAt(j,'.');
-                temp.set(i,newRow.toString());
-            }
-        }
-    }
-    public boolean isValid(List<String> temp, int row,int col){
-        for(int i=row;i>=0;i--){
-            if(temp.get(i).charAt(col)=='Q'){
-                return false;
-            }
-        }
-        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-            if (temp.get(i).charAt(j) == 'Q')
-                return false;
-        }
 
-        for (int i = row, j = col; i >= 0 && j < temp.size(); i--, j++) {
-            if (temp.get(i).charAt(j) == 'Q')
+    private void backtrack(List<List<String>> res, List<Integer> pos, int row, int n) {
+        if (row == n) {
+            res.add(formString(pos));
+        }
+        for (int col = 0; col < n; ++col) {
+            if (isValid(pos, row, col)) {
+                pos.add(col);
+                backtrack(res, pos, row + 1, n);
+                pos.remove(pos.size() - 1);
+            }
+        }
+    }
+
+    private boolean isValid(List<Integer> pos, int row, int col) {
+        for (int rowTaken = 0; rowTaken < pos.size(); ++rowTaken) {
+            int colTaken = pos.get(rowTaken);
+            if (col == colTaken || Math.abs(row - rowTaken) == Math.abs(col - colTaken)) {
                 return false;
+            }
         }
         return true;
+    }
+    
+    private List<String> formString(List<Integer> pos) {
+        List<String> res = new ArrayList<>();
+        int n = pos.size();
+        for (int col : pos) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < n; ++j) {
+                if (j == col) {
+                    sb.append("Q");
+                } else {
+                    sb.append(".");
+                }
+            }
+            res.add(sb.toString());
+        }
+        return res;
     }
 }
