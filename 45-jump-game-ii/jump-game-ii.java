8634f23443ba517fe603a103fb793934a54f5e33
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-09 05:05:54 +0530

    Time: 12 ms (29.62%) | Memory: 45.2 MB (33.02%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,19 +1,17 @@
 class Solution {
     public int jump(int[] nums) {
-        int jumps = 0; // Number of jumps taken
-        int curMaxReach = 0; // Farthest reachable index with current number of jumps
-        int nextMaxReach = 0; // Farthest reachable index with one more jump
-        
-        for (int i = 0; i < nums.length - 1; ++i) {
-            nextMaxReach = Math.max(nextMaxReach, i + nums[i]);
-
-            // If we finish the current jump range, move on to the next one
-            if (i == curMaxReach) {
-                jumps++;
-                curMaxReach = nextMaxReach;
+        int jumps = 0;
+        int l = 0;
+        int r = 0;
+        int max = 0;
+        while(r<nums.length-1){
+            for(int i=0;i<=r;i++){
+                max=Math.max(max,i+nums[i]);
             }
+            l=r+1;
+            r=max;
+            jumps++;
         }
-        
         return jumps;
     }
 }
