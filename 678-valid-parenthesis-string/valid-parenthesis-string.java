9ddc194aefed61a792a5d243da506f3a31e6f734
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-09 06:14:21 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (69.49%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,5 +1,15 @@
 public class Solution {
     public boolean checkValidString(String s) {
+/*
+ Initialize leftMin and leftMax to 0.
+ Iterate through each character in the string s.
+ If the character is '(', increment both leftMin and leftMax by 1.
+ If the character is ')', decrement both leftMin and leftMax by 1.
+ If the character is '*', decrement leftMin by 1 and increment leftMax by 1.
+ If leftMax becomes negative at any point, return False since it means there are more closing parentheses than opening ones.
+If leftMin becomes negative, reset it to 0 since we can't have negative open parentheses count.
+After iterating through the string, check if leftMin is 0. If it is, return True; otherwise, return False.
+*/
         int leftMin = 0, leftMax = 0;
 
         for (char c : s.toCharArray()) {
