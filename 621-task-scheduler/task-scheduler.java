31d5b03739aa197382bad6b5328c3e16d85bd9d6
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-10 12:01:33 +0530

    Time: 32 ms (25.56%) | Memory: 45.2 MB (85.87%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : tasks) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        pq.addAll(map.values());
+        
+        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+        int time = 0;
+
+        while (!pq.isEmpty() || !q.isEmpty()) {
+            time++;
+
+            if (!pq.isEmpty()) {
+                int cnt = pq.poll() - 1;
+
+                if (cnt != 0) {
+                    q.offer(new Pair(cnt, time + n));
+                }
+            }
+
+            if (!q.isEmpty() && q.peek().getValue() == time) {
+                pq.offer(q.poll().getKey());
+            }
+        }
+
+        return time;
+    }
+}
