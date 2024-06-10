092b211ac8733daf7119d1c85139b4b12f0e8946
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-11 01:43:09 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (68.64%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,37 +1,22 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+    int i=0;
+    int p=0;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        for(int i=0;i<inorder.length;i++){
-            hm.put(inorder[i],i);
+       return bdt(preorder,inorder,Integer.MIN_VALUE);
+    }
+
+    public TreeNode bdt(int[] preorder, int[] inorder, int stop){
+        if(p>=preorder.length) return null;
+
+        if(inorder[i]==stop){
+            i++;
+            return null;
         }
-        return build(preorder,0,preorder.length-1,inorder,0,inorder.length-1,hm);
+
+        TreeNode node = new TreeNode(preorder[p]);
+        node.left = bdt(preorder,inorder,preorder[p++]);
+        node.right = bdt(preorder,inorder,stop);
+        return node;
     }
-
-    public TreeNode build(int[] preorder, int ps, int pe,
-    int[] inorder, int is,int ie,HashMap<Integer,Integer> hm){
-        if(ps>pe || is>pe) return null;
-        TreeNode root = new TreeNode(preorder[ps]);
-        int index = hm.get(preorder[ps]);
-        root.left = build(preorder,ps+1,ps+(index-is),inorder,is,index-1,hm);
-        root.right = build(preorder,ps+(index-is)+1,pe,inorder,index+1,ie,hm);
-        return root;
-    }
-
-
 
 }
