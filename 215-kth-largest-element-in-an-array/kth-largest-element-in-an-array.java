5a530a801a4bdd8d25d3c5773a194053cc671fb6
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-10 11:58:08 +0530

    Time: 58 ms (54.08%) | Memory: 56.7 MB (94.63%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,23 +1,12 @@
 class Solution {
-    public int findKthLargest(int[] A, int k) {
-        if(k==50000 ) return 1;
-        k = A.length - k; // convert to index of k largest
-        int l = 0, r = A.length - 1;
-        while (l <= r) {
-            int i = l; // partition [l,r] by A[l]: [l,i]<A[l], [i+1,j)>=A[l]
-            for (int j = l + 1; j <= r; j++)
-                if (A[j] < A[l]) swap(A, j, ++i);
-            swap(A, l, i);
-
-            if (k < i) r = i - 1;
-            else if (k > i) l = i + 1;
-            else return A[i];
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((x,y)->y-x);
+        for(int i:nums){
+            pq.offer(i);
         }
-        return -1; // k is invalid
-    }
-    public void swap(int[] a, int i , int j){
-        int temp = a[i];
-        a[i]=a[j];
-        a[j]=temp;
+        for(int i=1;i<k;i++){
+            pq.poll();
+        }
+        return pq.peek();
     }
 }
