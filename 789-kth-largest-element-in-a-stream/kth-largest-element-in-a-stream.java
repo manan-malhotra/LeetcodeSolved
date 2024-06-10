62f49880ae8f88c2eaf312646b91c773a7cfc288
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-10 11:44:07 +0530

    Time: 13 ms (85.52%) | Memory: 46.6 MB (91.41%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
@@ -1,22 +1,24 @@
-
 class KthLargest {
-    private PriorityQueue<Integer> minHeap;
-    private int k;
 
+    PriorityQueue<Integer> pq;
+    int k;
     public KthLargest(int k, int[] nums) {
         this.k = k;
-        minHeap = new PriorityQueue<>(k);
-
-        // Add all elements from nums to the heap
-        for (int num : nums) {
+        pq = new PriorityQueue<>();
+        for(int num: nums){
             add(num);
         }
     }
-
+    
     public int add(int val) {
-        // Add the new value to the heap
-        minHeap.offer(val);
-        if (minHeap.size()>k)minHeap.poll();     
-        return minHeap.peek();
+        pq.add(val);
+        while(pq.size()>k) pq.poll();
+        return pq.peek();
     }
 }
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
