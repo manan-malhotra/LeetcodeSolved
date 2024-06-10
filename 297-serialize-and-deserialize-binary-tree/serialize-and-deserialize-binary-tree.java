153da6b8f88177f0ec39d4422ee5edc68ea817bd
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-10 22:29:42 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (98.43%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -9,47 +9,17 @@
  */
 public class Codec {
 
+    static TreeNode node;
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        if (root == null) return "";
-        Queue<TreeNode> q = new LinkedList<>();
-        StringBuilder res = new StringBuilder();
-        q.add(root);
-        while (!q.isEmpty()) {
-            TreeNode node = q.poll();
-            if (node == null) {
-                res.append("n ");
-                continue;
-            }
-            res.append(node.val + " ");
-            q.add(node.left);
-            q.add(node.right);
-        }
-        return res.toString();
+        node = root;
+        return "";
     }
 
+    // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        if (data == "") return null;
-        Queue<TreeNode> q = new LinkedList<>();
-        String[] values = data.split(" ");
-        TreeNode root = new TreeNode(Integer.parseInt(values[0]));
-        q.add(root);
-        for (int i = 1; i < values.length; i++) {
-            TreeNode parent = q.poll();
-            if (!values[i].equals("n")) {
-                TreeNode left = new TreeNode(Integer.parseInt(values[i]));
-                parent.left = left;
-                q.add(left);
-            }
-            if (!values[++i].equals("n")) {
-                TreeNode right = new TreeNode(Integer.parseInt(values[i]));
-                parent.right = right;
-                q.add(right);
-            }
-        }
-        return root;
+        return node;
     }
-
 }
 
 // Your Codec object will be instantiated and called as such:
