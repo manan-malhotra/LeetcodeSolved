8b69b010ce8c9a145495e075f8b810dfdc83505b
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-11 23:07:00 +0530

    Time: 15 ms (24.20%) | Memory: 42.4 MB (65.70%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -9,61 +9,59 @@
  */
 class Solution {
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        List<Integer> ans = new ArrayList<Integer>();
-        if(root==null) return ans;
-        HashMap<TreeNode,TreeNode> parent = new HashMap<>();
-        HashMap<TreeNode,Boolean> visited = new HashMap<>();
+        HashMap<TreeNode,TreeNode> parentMap = new HashMap<>();
+        // contains <Node,ParentOfNode> in map
+        createParentMap(root,parentMap);
+        HashSet<TreeNode> visited = new HashSet<>();
         Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                TreeNode node = q.poll();
-                if(node.left!=null){
-                    q.offer(node.left);
-                    parent.put(node.left,node);
-                }
-                if(node.right!=null){
-                    q.offer(node.right);
-                    parent.put(node.right,node);
-                }
-            }
-        }
-        int distance = 0;
+        visited.add(target);
         q.offer(target);
-        visited.put(target,true);
-        while(!q.isEmpty()){
-            if(distance==k) break;
-            distance++;
+        int distance = 0;
+        while(!q.isEmpty() && distance<k){
             int size = q.size();
+            distance++;
             for(int i=0;i<size;i++){
                 TreeNode node = q.poll();
-                if(parent.containsKey(node)){
-                    TreeNode par = parent.get(node);
-                    if(!visited.containsKey(par)){
-                    q.offer(par);
-                    visited.put(par,true);
-                    }
+                TreeNode parent = parentMap.getOrDefault(node,null);
+                if(parent!=null && !visited.contains(parent)){
+                    visited.add(parent);
+                    q.offer(parent);
                 }
-                if(node.left!=null){
-                    if(!visited.containsKey(node.left)){
-                        q.offer(node.left);
-                        visited.put(node.left,true);
-                    }
+                TreeNode left = node.left;
+                TreeNode right = node.right;
+                if(left!=null && !visited.contains(left)){
+                    visited.add(left);
+                    q.offer(left);
                 }
-                if(node.right!=null){
-                    if(!visited.containsKey(node.right)){
-                        q.offer(node.right);
-                        visited.put(node.right,true);
-                    }
+                if(right!=null && !visited.contains(right)){
+                    visited.add(right);
+                    q.offer(right);
                 }
-
             }
-            
+            if(distance==k){
+                break;
+            }
         }
+        List<Integer> ans = new ArrayList<>();
         while(!q.isEmpty()){
             ans.add(q.poll().val);
         }
         return ans;
+        
+    }
+    public void createParentMap(TreeNode root,HashMap<TreeNode,TreeNode> parentMap){
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            TreeNode parent = q.poll();
+            if(parent.left!=null){
+                parentMap.put(parent.left,parent);
+                q.offer(parent.left);
+            }
+            if(parent.right!=null){
+                parentMap.put(parent.right,parent);
+                q.offer(parent.right);
+            }
+        }
     }
 }
