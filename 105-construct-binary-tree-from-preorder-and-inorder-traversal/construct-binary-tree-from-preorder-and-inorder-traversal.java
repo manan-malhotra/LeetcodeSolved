91a78a9f009103e5768b14e7d1086d7f177432a9
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-12 17:22:14 +0530

    Time: 2 ms (70.96%) | Memory: 43.9 MB (79.35%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,22 +1,23 @@
 class Solution {
-    int i=0;
-    int p=0;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-       return bdt(preorder,inorder,Integer.MIN_VALUE);
-    }
-
-    public TreeNode bdt(int[] preorder, int[] inorder, int stop){
-        if(p>=preorder.length) return null;
-
-        if(inorder[i]==stop){
-            i++;
-            return null;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i=0;i<preorder.length;i++){
+            hm.put(inorder[i],i);
         }
-
-        TreeNode node = new TreeNode(preorder[p]);
-        node.left = bdt(preorder,inorder,preorder[p++]);
-        node.right = bdt(preorder,inorder,stop);
+        int[] index = {0};
+        int l = 0;
+        int r = inorder.length-1;
+        return helper(preorder,inorder,l,r,hm,index);
+    }
+    public TreeNode helper(int[] preorder, int[] inorder, int l, int r, HashMap<Integer,Integer> hm,int[] index){
+        if(l>r) return null;
+        int current = preorder[index[0]];
+        index[0]++;
+        TreeNode node = new TreeNode(current);
+        if(l==r) return node;
+        int inOrderIndex = hm.get(current);
+        node.left=helper(preorder,inorder,l,inOrderIndex-1,hm,index);
+        node.right=helper(preorder,inorder,inOrderIndex+1,r,hm,index);
         return node;
     }
-
 }
