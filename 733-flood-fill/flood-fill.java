0cd6932270b0a9978c2ffe3dd3127c9701cdc109
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-13 05:03:46 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (7.13%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,18 +1,22 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int startColor = image[sr][sc];
-        if(startColor == color) return image;
+        if (startColor == color)
+            return image;
         int n = image.length;
         int m = image[0].length;
-        dfs(image,sr,sc,n,m,startColor,color);
+        dfs(image, sr, sc, n, m, startColor, color);
         return image;
     }
+
     public void dfs(int[][] image, int sr, int sc, int n, int m, int startColor, int color){
         if(sr<0 || sc<0 || sr>=n || sc>=m || image[sr][sc]!=startColor) return;
         image[sr][sc] = color;
-        dfs(image,sr+1,sc,n,m,startColor,color);
-        dfs(image,sr-1,sc,n,m,startColor,color);
-        dfs(image,sr,sc+1,n,m,startColor,color);
-        dfs(image,sr,sc-1,n,m,startColor,color);
+        int[][] traverse ={
+        {0,1},{0,-1},{1,0},{-1,0}
+        };
+        for(int i=0;i<4;i++){
+            dfs(image,sr+traverse[i][0],sc+traverse[i][1],n,m,startColor,color);
+        }
     }
 }
