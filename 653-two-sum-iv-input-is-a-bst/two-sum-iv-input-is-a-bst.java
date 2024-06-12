e2111153adc590f78166a1a77c8c8dc47ec51bc2
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-12 21:04:52 +0530

    Time: 23 ms (5.48%) | Memory: 45 MB (48.00%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -1,16 +1,13 @@
 class Solution {
+    List<Integer> l=new ArrayList<>();
     public boolean findTarget(TreeNode root, int k) {
-        return help(root, k, root);
+        return help(root,k);
     }
-    private boolean help(TreeNode root, int k, TreeNode root2) {
-        if(root == null) return false;
-        if(find(root2, k - root.val, root))    return true;
-        return help(root.left, k, root2) || help(root.right, k, root2); 
-    }
-    private boolean find(TreeNode root, int target, TreeNode org) {
-        if(root == null) return false;
-        if(root.val == target && root != org) return true;
-        if(root.val < target)    return find(root.right, target, org);
-        return find(root.left, target, org);
+    public boolean help(TreeNode root,int k )
+    {
+        if(root==null)return false;
+        if(l.contains(k-root.val))return true;
+        l.add(root.val);
+        return help(root.left,k)||help(root.right,k);
     }
 }
