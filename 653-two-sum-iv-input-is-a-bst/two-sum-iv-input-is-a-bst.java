5a6ef03954a34dcdc4c25f37fa5a0739936c0588
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-12 20:55:41 +0530

    Time: 1 ms (100.00%) | Memory: 45.3 MB (17.80%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -1,30 +1,16 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
-        HashSet<Integer> hm = new HashSet<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()){
-            TreeNode node = q.poll();
-            if(hm.contains(node.val)) return true;
-            hm.add(k-node.val);
-            if(node.left!=null) q.offer(node.left);
-            if(node.right!=null) q.offer(node.right);
-        }
-        return false;
+        return help(root, k, root);
+    }
+    private boolean help(TreeNode root, int k, TreeNode root2) {
+        if(root == null) return false;
+        if(find(root2, k - root.val, root))    return true;
+        return help(root.left, k, root2) || help(root.right, k, root2); 
+    }
+    private boolean find(TreeNode root, int target, TreeNode org) {
+        if(root == null) return false;
+        if(root.val == target && root != org) return true;
+        if(root.val < target)    return find(root.right, target, org);
+        return find(root.left, target, org);
     }
 }
