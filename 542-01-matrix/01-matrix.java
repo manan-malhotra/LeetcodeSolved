6a894bc7d7fdfa31464827c725dc032a6a491b5b
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-13 06:06:29 +0530

    Time: 1531 ms (5.02%) | Memory: 49.6 MB (5.42%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,31 +1,40 @@
 class Solution {
-    public int[][] updateMatrix(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        
-        Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    queue.offer(new int[] {i, j});
-                }
-                else {
-                    matrix[i][j] = Integer.MAX_VALUE;
+    public int[][] updateMatrix(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        Queue<Pair<Integer,Integer>> queue = new LinkedList<>();
+        int ones = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==1) ones++;
+                else queue.offer(new Pair(i,j));
+            }
+        }
+        if(ones==0) return mat;
+        int[][] ans = new int[n][m];
+        int distance = 0;
+        int[] row = {0,0,1,-1};
+        int[] col = {1,-1,0,0};
+        while(!queue.isEmpty() && ones!=0){
+            distance++;
+            int size = queue.size();
+            for(int i=0;i<size;i++){
+                Pair<Integer,Integer> pair = queue.poll();
+                int x = pair.getKey();
+                int y = pair.getValue();
+                for(int j=0;j<4;j++){
+                    int newRow = x+row[j];
+                    int newCol = y+col[j];
+                    System.out.println(newRow+" "+newCol);
+                    if(newRow<0 || newRow>=n || newCol<0 || newCol>=m || mat[newRow][newCol]==0) continue;
+                    System.out.println("changed");
+                    mat[newRow][newCol]=0;
+                    queue.offer(new Pair(newRow,newCol));
+                    ans[newRow][newCol] = distance;
+                    ones--;
                 }
             }
         }
-        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-         while (!queue.isEmpty()) {
-            int[] cell = queue.poll();
-            for (int[] d : dirs) {
-                int r = cell[0] + d[0];
-                int c = cell[1] + d[1];
-                if (r < 0 || r >= m || c < 0 || c >= n || 
-                    matrix[r][c] <= matrix[cell[0]][cell[1]] + 1) continue;
-                queue.add(new int[] {r, c});
-                matrix[r][c] = matrix[cell[0]][cell[1]] + 1;
-            }
-        }
-        return matrix;
-    }
+        return ans;
+    }   
 }
