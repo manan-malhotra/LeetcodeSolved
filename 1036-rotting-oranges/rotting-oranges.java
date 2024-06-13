59988a07f08dea8cdd2331accf862a5ed8848265
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-13 05:35:04 +0530

    Time: 2 ms (73.25%) | Memory: 42.4 MB (32.67%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -4,36 +4,33 @@ class Solution {
         int m = grid[0].length;
         if(n==0) return -1;
         int ones = 0;
-        Queue<List<Integer>> q = new LinkedList<>();
+        Queue<Pair<Integer,Integer>> q = new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1) ones++;
                 else if(grid[i][j]==2) {
-                    List<Integer> arr = new ArrayList<>();
-                    arr.add(i);
-                    arr.add(j);
-                    arr.add(0);
-                    q.offer(arr);
+                    q.offer(new Pair(i , j));
                 }
             }
         }
+        if(ones==0) return 0;
         int max = 0;
         int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
-        while(!q.isEmpty()){
-            List<Integer> arr = q.poll();
+        while(!q.isEmpty() && ones!=0){
+            ++max;
+            int size = q.size();
+            for(int i=0;i<size;i++){
+            Pair<Integer,Integer> arr = q.poll();
+            int row = arr.getKey();
+            int col = arr.getValue();
             for(int k=0;k<dir.length;k++){
-                int x = arr.get(0)+ dir[k][0];
-                int y = arr.get(1)+ dir[k][1];
+                int x = row+ dir[k][0];
+                int y = col+ dir[k][1];
                 if(x<0 || y<0 || x>=n || y>=m || grid[x][y]!=1) continue;
                 grid[x][y] = 2;
-                int time = arr.get(2)+1;
-                max = Math.max(max,time);
                 ones--;
-                List<Integer> arr2 = new ArrayList<>();
-                arr2.add(x);
-                arr2.add(y);
-                arr2.add(time);
-                q.offer(arr2);
+                q.offer(new Pair(x,y));
+            }
             }
         }    
         if(ones==0) return max;
