4aeb8f50e29b3e38dbf16ccf8b9f5f524bc28e8d
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-14 19:22:41 +0530

    Time: 4 ms (99.19%) | Memory: 55.4 MB (47.10%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,35 +1,32 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
+        List<Integer> ans = new ArrayList<>();
         int n = graph.length;
-        int[] vis = new int[n];
-        int[] pathVis = new int[n];
-        List<Integer> arr = new ArrayList<>();
+        int[] visited = new int[n];
+        int[] pathVisited = new int[n];
         for(int i=0;i<n;i++){
-            if(vis[i]==0){
-                dfs(graph,vis,pathVis,i,arr);
+            if(visited[i]==0){
+                dfsCheck(i,-1,graph,visited,pathVisited);
             }
         }
-        Collections.sort(arr);
-        return arr;
+        for(int i=0;i<n;i++){
+            if(visited[i]==-1) ans.add(i);
+        }
+        return ans;
     }
-    public boolean dfs(int[][] graph,int[] vis,int[] pathVis,int i, List<Integer> arr){
-        int n = graph.length;
-        if(vis[i]==1){
-            if(pathVis[i]==1){
-                //cycle
-                return false;
-            }else{
-                return true;
-                //dont go ahead
-            }
+    public boolean dfsCheck(int startNode, int parentNode, int[][] graph, int[] visited, int[] pathVisited) {
+        visited[startNode] = 1;
+        pathVisited[startNode] = 1;
+        for(int childNode : graph[startNode]){
+            // if(childNode!=parentNode){
+                if(visited[childNode]==0){
+                    if(dfsCheck(childNode,startNode,graph,visited,pathVisited)) return true;
+                }
+                if(pathVisited[childNode] == 1) return true;
+            // }
         }
-        vis[i]=1;
-        pathVis[i]=1;
-        for(int j:graph[i]){
-            if(!dfs(graph,vis,pathVis,j,arr)) return false;
-        }
-        arr.add(i);
-        pathVis[i]=0;
-        return true;
+        pathVisited[startNode] = 0;
+        visited[startNode] = -1;
+        return false;
     }
 }
