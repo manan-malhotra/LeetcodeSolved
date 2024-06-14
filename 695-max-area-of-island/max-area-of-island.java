52b6074482febc4cdaaa58ecddbee8ec79fc7204
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-14 10:42:55 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (61.17%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,27 +1,26 @@
 class Solution {
     int area = 0;
-    int currentCount = 0;
     public int maxAreaOfIsland(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
-                    currentCount=0;
-                    dfs(grid,i,j,n,m);
+                    dfs(grid,i,j,0,n,m);
                 }
             }
         }
         return area;
     }
-    public void dfs(int[][] grid,int i,int j,int n,int m){
-        if(i<0 || i>=n || j<0 || j>=m || grid[i][j]==0) return;
+    public int dfs(int[][] grid,int i,int j, int currentCount,int n,int m){
+        if(i<0 || i>=n || j<0 || j>=m || grid[i][j]==0) return currentCount;
         grid[i][j]=0;
         currentCount++;
         area=Math.max(area,currentCount);
-        dfs(grid,i+1,j,n,m);
-        dfs(grid,i,j+1,n,m);
-        dfs(grid,i,j-1,n,m);
-        dfs(grid,i-1,j,n,m);
+        currentCount=dfs(grid,i+1,j,currentCount,n,m);
+        currentCount=dfs(grid,i,j+1,currentCount,n,m);
+        currentCount=dfs(grid,i,j-1,currentCount,n,m);
+        currentCount=dfs(grid,i-1,j,currentCount,n,m);
+        return currentCount;
     }
 }
