11da1213c8546ef621af49a450d504781072f743
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-15 08:45:03 +0530

    Time: 2 ms (99.92%) | Memory: 43.8 MB (97.49%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,40 +1,51 @@
 class Solution {
-    public boolean canFinish(int n, int[][] prerequisites) {
-        List<Integer>[] adj = new List[n];
-        int[] indegree = new int[n];
-        List<Integer> ans = new ArrayList<>();
-
-        for (int[] pair : prerequisites) {
-            int course = pair[0];
-            int prerequisite = pair[1];
-            if (adj[prerequisite] == null) {
-                adj[prerequisite] = new ArrayList<>();
-            }
-            adj[prerequisite].add(course);
-            indegree[course]++;
+    public boolean canFinish(int n, int[][] pre) {
+      
+        boolean visited[] = new boolean[n]; 
+        boolean helper[] = new boolean[n];     
+        ArrayList<ArrayList<Integer>> al = new ArrayList<>();
+        
+        for(int i = 0; i < n; i++) 
+        {
+            al.add(new ArrayList());
         }
-
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
-                queue.offer(i);
+        
+        for(int[] t: pre)
+        {
+            al.get( t[1] ).add( t[0] );
+        }
+        
+        for(int i = 0; i < n; i++)
+        {   
+            if(visited[i] == false)
+            {
+                boolean ans = dfs_cyclicDetection(al, i , visited , helper);
+                if(ans)
+                return false;
+            }     
+        }
+        return true;
+    }
+    
+    public static boolean dfs_cyclicDetection( ArrayList<ArrayList<Integer>> al, int i,  boolean visited[],  boolean helper[]) {
+        
+        visited[i] = true;
+        helper[i] = true; 
+        ArrayList< Integer > neighbour = al.get(i);
+        
+        for(int k = 0; k < neighbour.size(); k++)
+        {
+            int curr = neighbour.get(k);
+            if(helper[curr] == true) return true;
+            
+            if(visited[curr] == false)
+            {
+                boolean ans = dfs_cyclicDetection(al,curr,visited,helper);
+                if(ans == true) return true;
             }
         }
-
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
-            ans.add(current);
-
-            if (adj[current] != null) {
-                for (int next : adj[current]) {
-                    indegree[next]--;
-                    if (indegree[next] == 0) {
-                        queue.offer(next);
-                    }
-                }
-            }
-        }
-
-        return ans.size() == n;
+		
+        helper[i] = false;  
+        return false;
     }
 }
