20281b3745ed4f5f0f219a3da2b630a4da946b6e
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-16 17:06:14 +0530

    Time: 11 ms (98.52%) | Memory: 45.6 MB (37.16%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,52 +1,42 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n = grid.length;
-        if (n == 1) {
-            if (grid[0][0] == 0)
-                return 1;
+        if(n==1){
+            if(grid[0][0]==0) return 1;
             return -1;
         }
         Queue<Node> queue = new LinkedList<>();
-        if (grid[0][0] == 0 && grid[n - 1][n - 1] == 0)
-            queue.offer(new Node(0, 0));
-        int[] row = { 1, 1, 1, 0, 0, -1, -1, -1 };
-        int[] col = { -1, 0, 1, -1, 1, -1, 0, 1 };
-        int result = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            result++;
-            for (int k = 0; k < size; k++) {
-
-                Node node = queue.poll();
-                int x = node.x;
-                int y = node.y;
-                if (x == y && x == n - 1)
-                    return result;
-                for (int i = 0; i < 8; i++) {
-                    int newRow = x + row[i];
-                    int newCol = y + col[i];
-                    if (isValid(newRow, newCol, n, grid)) {
-                        queue.offer(new Node(newRow, newCol));
-                        grid[newRow][newCol] = 1;
-                    }
-                }
+        if(grid[0][0]==0 && grid[n-1][n-1]==0)queue.offer(new Node(0,0,1));
+        int[] row = {1,1,1,0,0,-1,-1,-1};
+        int[] col = {-1,0,1,-1,1,-1,0,1};
+        while(!queue.isEmpty()){
+            Node node = queue.poll();
+            int x = node.x;
+            int y = node.y;
+            int wt = node.wt;
+            if(x==y && x==n-1) return wt;
+            for(int i=0;i<8;i++){
+                int newRow = x + row[i];
+                int newCol = y + col[i];
+                if(isValid(newRow,newCol,n,grid)) {
+                    queue.offer(new Node(newRow,newCol,wt+1));
+                    grid[newRow][newCol]=1;
+                }    
             }
-
         }
         return -1;
     }
-
-    public boolean isValid(int i, int j, int n, int[][] grid) {
-        return (i >= 0 && j >= 0 && i < n && j < n && grid[i][j] == 0);
+    public boolean isValid(int i,int j, int n,int[][] grid){
+        return (i>=0 && j>=0 && i<n && j<n && grid[i][j]==0 );
     }
 }
-
-class Node {
+class Node{
     int x;
     int y;
-
-    public Node(int x, int y) {
-        this.x = x;
-        this.y = y;
+    int wt;
+    public Node(int x,int y,int wt){
+        this.x=x;
+        this.y=y;
+        this.wt=wt;
     }
 }
