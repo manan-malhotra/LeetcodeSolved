c4248ba31c985fe7bf693ef54aacb558aa51ed7c
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-19 06:57:11 +0530

    Time: 28 ms (85.88%) | Memory: 47.5 MB (67.96%) - LeetSync

--- 721-accounts-merge/accounts-merge.java
+++ 721-accounts-merge/accounts-merge.java
@@ -1,125 +1,90 @@
-/**
- * Using union-find (Union by rank and path compression) to group all emails
- * belonging to same owner. And using TreeSet to sort each group of emails.
- *
- * Time Complexity: O(N + N * log(N) + N) = O(N * log(N))
- *
- * Space Complexity: O(N)
- *
- * N = Total number of email addresses in the input. Here assuming the length of
- * each email and owner string is a fixed constant.
- */
+class DisjointSet {
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+    public DisjointSet(int n) {
+        for (int i = 0; i <= n; i++) {
+            rank.add(0);
+            parent.add(i);
+            size.add(1);
+        }
+    }
 
+    public int findUPar(int node) {
+        if (node == parent.get(node)) {
+            return node;
+        }
+        int ulp = findUPar(parent.get(node));
+        parent.set(node, ulp);
+        return parent.get(node);
+    }
+
+    public void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank.get(ulp_u) < rank.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+        } else if (rank.get(ulp_v) < rank.get(ulp_u)) {
+            parent.set(ulp_v, ulp_u);
+        } else {
+            parent.set(ulp_v, ulp_u);
+            int rankU = rank.get(ulp_u);
+            rank.set(ulp_u, rankU + 1);
+        }
+    }
+
+    public void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size.get(ulp_u) < size.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u));
+        } else {
+            parent.set(ulp_v, ulp_u);
+            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+        }
+    }
+}
 class Solution {
-    private class Node {
-        String parent;
-        String owner;
-        int rank;
+    static List<List<String>> accountsMerge(List<List<String>> details) {
+        int n = details.size();
+        DisjointSet ds = new DisjointSet(n);
+        HashMap<String, Integer> mapMailNode = new HashMap<String, Integer>();
 
-        public Node(String owner, String parent, int rank) {
-            this.owner = owner;
-            this.parent = parent;
-            this.rank = rank;
-        }
-    }
-
-    // Union by Rank and Path Compression
-    private class AccountMergeUnionFind {
-        private Map<String, Node> emailToNode;
-
-        public AccountMergeUnionFind() {
-            this.emailToNode = new HashMap<>();
-        }
-
-        public void addEmail(String owner, String email) {
-            if (!emailToNode.containsKey(email)) {
-                emailToNode.put(email, new Node(owner, email, 0));
-            }
-        }
-
-        public String findParent(String email) {
-            Node curNode = emailToNode.get(email);
-            if (!email.equals(curNode.parent)) {
-                curNode.parent = findParent(curNode.parent);
-            }
-            return curNode.parent;
-        }
-
-        public void union(String email1, String email2) {
-            String parent1 = findParent(email1);
-            String parent2 = findParent(email2);
-
-            if (parent1.equals(parent2)) {
-                return;
-            }
-
-            Node node1 = emailToNode.get(parent1);
-            Node node2 = emailToNode.get(parent2);
-
-            if (node1.rank >= node2.rank) {
-                node2.parent = parent1;
-                if (node1.rank == node2.rank) {
-                    node1.rank++;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < details.get(i).size(); j++) {
+                String mail = details.get(i).get(j);
+                if (mapMailNode.containsKey(mail) == false) {
+                    mapMailNode.put(mail, i);
+                } else {
+                    ds.unionBySize(i, mapMailNode.get(mail));
                 }
-            } else {
-                node1.parent = parent2;
             }
         }
 
-        public String getOwner(String email) {
-            return emailToNode.get(email).owner;
-        }
-    }
-
-    public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        if (accounts == null) {
-            throw new IllegalArgumentException("Input is null");
-        }
-        if (accounts.size() == 0) {
-            return new ArrayList<>();
+        ArrayList<String>[] mergedMail = new ArrayList[n];
+        for (int i = 0; i < n; i++) mergedMail[i] = new ArrayList<String>();
+        for (Map.Entry<String, Integer> it : mapMailNode.entrySet()) {
+            String mail = it.getKey();
+            int node = ds.findUPar(it.getValue());
+            mergedMail[node].add(mail);
         }
 
-        AccountMergeUnionFind uf = new AccountMergeUnionFind();
+        List<List<String>> ans = new ArrayList<>();
 
-        // Populating UnionFind with all emails.
-        for (List<String> account : accounts) {
-            if (account.size() <= 1) {
-                continue;
+        for (int i = 0; i < n; i++) {
+            if (mergedMail[i].size() == 0) continue;
+            Collections.sort(mergedMail[i]);
+            List<String> temp = new ArrayList<>();
+            temp.add(details.get(i).get(0));
+            for (String it : mergedMail[i]) {
+                temp.add(it);
             }
-            String owner = account.get(0);
-            uf.addEmail(owner, account.get(1));
-
-            for (int i = 2; i < account.size(); i++) {
-                uf.addEmail(owner, account.get(i));
-                uf.union(account.get(1), account.get(i));
-            }
+            ans.add(temp);
         }
+        return ans;
 
-        // Creating merged Groups with sorted emails
-        Map<String, TreeSet<String>> groups = new HashMap<>();
-        for (List<String> account : accounts) {
-            if (account.size() <= 1) {
-                continue;
-            }
-            String parent = uf.findParent(account.get(1));
-            if (!groups.containsKey(parent)) {
-                groups.put(parent, new TreeSet<>());
-            }
-            TreeSet<String> curGroup = groups.get(parent);
-            for (int i = 1; i < account.size(); i++) {
-                curGroup.add(account.get(i));
-            }
-        }
-
-        // Creating final result list
-        List<List<String>> result = new ArrayList<>();
-        for (Map.Entry<String, TreeSet<String>> group : groups.entrySet()) {
-            List<String> account = new ArrayList<>();
-            account.add(uf.getOwner(group.getKey()));
-            account.addAll(group.getValue());
-            result.add(account);
-        }
-
-        return result;
     }
 }
