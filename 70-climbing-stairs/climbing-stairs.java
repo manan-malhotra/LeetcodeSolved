65248df73925a0e9ef7d7ed6dee94815f66ccfa5
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-21 06:24:32 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (69.32%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2)return n;
+        int one = 1;
+        int two = 2;
+        for(int i=3;i<=n;i++){
+            int temp = one+two;
+            one = two ;
+            two=temp;
+        }
+        return two;
+    }
+}
