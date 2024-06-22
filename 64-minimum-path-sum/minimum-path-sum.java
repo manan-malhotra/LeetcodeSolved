631a341a8f9aabdc0dbfbe7458c36fc6fbe07135
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-22 13:56:02 +0530

    Time: 3 ms (66.21%) | Memory: 45.4 MB (37.08%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -3,18 +3,19 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int[][] dp = new int[m][n];
-        for(int[] arr:dp){
-            Arrays.fill(arr,-1);
+        dp[0][0]=grid[0][0];
+        for(int i=1;i<n;i++){
+            dp[0][i] = dp[0][i-1] + grid[0][i];
         }
-        return findMin(grid,m-1,n-1,dp);
-    }
-    public int findMin(int[][] grid, int m, int n,int[][] dp){
-        if(dp[m][n]!=-1) return dp[m][n];
-        else if(m==0 && n==0) dp[m][n] =  grid[m][n];
-        else if(m==0) dp[m][n] =  grid[m][n]+findMin(grid,m,n-1,dp);
-        else if(n==0) dp[m][n] =  grid[m][n]+findMin(grid,m-1,n,dp);
-        else dp[m][n] = Math.min(findMin(grid,m-1,n,dp),findMin(grid,m,n-1,dp)) + grid[m][n];
-        return dp[m][n];
+        for(int j=1;j<m;j++){
+            dp[j][0] = dp[j-1][0] + grid[j][0];
+        }
+        for(int j=1;j<m;j++){
+            for(int i=1;i<n;i++){
+                dp[j][i] = Math.min(dp[j-1][i],dp[j][i-1]) + grid[j][i];
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
 
