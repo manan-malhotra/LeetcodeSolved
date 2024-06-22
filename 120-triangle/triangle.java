b7a7c8573c059d5bcbeb29515d4867c876c723e3
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-22 21:35:43 +0530

    Time: 3 ms (65.46%) | Memory: 44.1 MB (47.75%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -2,22 +2,14 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
         int m = triangle.get(n-1).size();
-        int dp[][] = new int[2][m];
-        dp[0][0] = triangle.get(0).get(0);
-        for(int i=1;i<n;i++){
+        int dp[][] = new int[n][m];
+        for(int j=0;j<m;j++) dp[n-1][j] = triangle.get(n-1).get(j);
+        for(int i=n-2;i>=0;i--){
             m = triangle.get(i).size();
             for(int j=0;j<m;j++){
-                if(j==0) dp[i%2][j] = dp[(i-1)%2][j];
-                else if(j!=0)dp[i%2][j] = dp[(i-1)%2][j-1];
-                if(j!=m-1) dp[i%2][j]=Math.min(dp[i%2][j],dp[(i-1)%2][j]);
-                dp[i%2][j] += triangle.get(i).get(j);
+                dp[i][j] = Math.min(dp[i+1][j],dp[i+1][j+1]) + triangle.get(i).get(j);
             }
         }
-        int ans = Integer.MAX_VALUE;
-        m = dp[0].length;
-        for(int i=0;i<m;i++){
-            ans=Math.min(ans,dp[(n-1)%2][i]);
-        }
-        return ans;
+        return dp[0][0];
     }
 }
