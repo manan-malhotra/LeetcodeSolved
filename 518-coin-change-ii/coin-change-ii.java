b89b8dc7a31b4077f7a6b0f2a5fc23febd9f3d88
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-24 08:13:39 +0530

    Time: 7 ms (49.11%) | Memory: 47.7 MB (42.00%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -2,19 +2,19 @@ class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
         int[][] dp = new int[n][amount+1];
-        for(int[] arr : dp) Arrays.fill(arr,-1);
-        return countChange(n-1,amount,coins,dp);
-    }
-    public int countChange(int i, int amount, int[] coins,int[][] dp){
-        if(dp[i][amount] !=-1) return dp[i][amount] ;
-        if(i==0){
-            if(amount%coins[i]==0) return dp[i][amount] = 1;
-            return dp[i][amount] = 0;
+        for(int j=0;j<=amount;j++){
+            if(j%coins[0]==0) dp[0][j] = 1;
         }
-        int notTake = countChange(i-1,amount,coins,dp);
-        int take = 0;
-        if(amount>=coins[i]) take = countChange(i,amount-coins[i],coins,dp);
-        return dp[i][amount] = take + notTake;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                int notTake = dp[i-1][j];
+                int take = 0;
+                if(j>=coins[i]) take = dp[i][j-coins[i]];
+                dp[i][j] = take + notTake;
+            }
+        }
+        return dp[n-1][amount];
     }
+    
 }
 
