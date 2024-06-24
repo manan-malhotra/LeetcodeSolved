1425aa18fea5381c697e383652053700545b700d
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-24 06:39:39 +0530

    Time: 131 ms (7.15%) | Memory: 46.1 MB (5.95%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,13 +1,27 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] dp=new int[amount+1];
-        Arrays.fill(dp,amount+1);
-        dp[0]=0;
-        for(int i=1;i<=amount;i++)
-            for(int c:coins)
-                if(i>=c) dp[i]=Math.min(dp[i],dp[i-c]+1);
-        return dp[amount]>amount?-1:dp[amount];
-
-
+        if(amount==0) return 0;
+        int n = coins.length - 1;
+        int min = coins[0];
+        for(int coin:coins){
+            min=Math.min(coin,min);
+        }
+        if(min>amount) return -1;
+        int[][] dp = new int[n+1][amount+1];
+        for(int[] arr:dp) Arrays.fill(arr,-1);
+        int ans = coinChange(n,amount,coins,dp);
+        if(ans>=(int) Math.pow(10, 9)) return -1;
+        return ans;
+    }
+    public int coinChange(int i, int amount, int[] coins,int[][] dp){
+        if(amount == 0) return dp[i][amount] = 0;
+        if(dp[i][amount]!=-1) return dp[i][amount];
+        int notTake = (int) Math.pow(10, 9);
+        int take = (int) Math.pow(10, 9);
+        if(i>0) notTake = coinChange(i-1,amount,coins,dp);
+        if(coins[i]<=amount) take = coinChange(i,amount-coins[i],coins,dp) + 1;
+        return dp[i][amount] = Math.min(notTake,take);
     }
 }
+
+
