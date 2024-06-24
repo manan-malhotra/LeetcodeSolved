22cba90402a46c8410ded4ca22491e2e2e30fb37
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-24 19:19:23 +0530

    Time: 26 ms (26.64%) | Memory: 41 MB (99.72%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -2,15 +2,18 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int n = text1.length();
         int m = text2.length();
-        int[][] dp = new int[2][m];
+        int[][] dp = new int[2][m+1];
         for(int i=n-1;i>=0;i--){
             for(int j=m-1;j>=0;j--){
                 int diagonal = text1.charAt(i)==text2.charAt(j)?1:0;
                 int right = 0;
                 int down = 0;
-                if(i+1 < n && j+1<m) diagonal+=dp[(i+1)%2][j+1];
-                if(i+1<n) down = dp[(i+1)%2][j];
-                if(j+1<m) right = dp[i%2][j+1];
+                // if(i+1 < n && j+1<m) 
+                diagonal+=dp[(i+1)%2][j+1];
+                // if(i+1<n) 
+                down = dp[(i+1)%2][j];
+                // if(j+1<m) 
+                right = dp[i%2][j+1];
                 dp[i%2][j] = Math.max(diagonal, Math.max(right,down));
             }
         }
