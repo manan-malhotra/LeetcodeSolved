64a9c1714207925bbb7a72c894518100868d7293
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-26 15:40:47 +0530

    Time: 5 ms (65.32%) | Memory: 42.7 MB (99.08%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -2,25 +2,25 @@ class Solution {
     public int minDistance(String s1, String s2) {
         int n = s1.length();
         int m = s2.length();
-        int[][] dp = new int[n+1][m+1];
+        int[][] dp = new int[2][m+1];
         for(int i=0;i<=n;i++){
             for(int j=0;j<=m;j++){
-                if(i==0) dp[i][j] = j;
-                else if(j==0) dp[i][j] = i;
+                if(i==0) dp[0][j] = j;
+                else if(j==0) dp[i%2][0] = i;
                 else{
                     if(s1.charAt(i-1) == s2.charAt(j-1)){
-                        dp[i][j] = dp[i-1][j-1];
+                        dp[i%2][j] = dp[(i-1)%2][j-1];
                     }
                     else{
-                        int replace = dp[i-1][j-1];
-                        int delete = dp[i-1][j];
-                        int add = dp[i][j-1];
-                        dp[i][j] =  Math.min(replace,Math.min(delete,add)) + 1;
+                        int replace = dp[(i-1)%2][j-1];
+                        int delete = dp[(i-1)%2][j];
+                        int add = dp[i%2][j-1];
+                        dp[i%2][j] =  Math.min(replace,Math.min(delete,add)) + 1;
                     }
                 }
             }
         }
-        return dp[n][m];
+        return dp[n%2][m];
     }
    
 }
