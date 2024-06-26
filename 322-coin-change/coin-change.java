a3e2dfbc3320c77d0c75a5b40f4e41d1dc710965
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-26 20:13:35 +0530

    Time: 14 ms (88.03%) | Memory: 43.8 MB (90.95%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,26 +1,13 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(amount==0) return 0;
-        int n = coins.length;
-        int[][] dp = new int[2][amount+1];
-        for(int i=0;i<=amount;i++){
-            if (i % coins[0] == 0)
-                dp[0][i] = i / coins[0];
-            else
-                dp[0][i] = (int) Math.pow(10, 9);
-        }
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=amount;j++){
-                int notTake = dp[(i-1)%2][j];
-                int take = (int) Math.pow(10, 9);
-                if(coins[i]<=j) take = dp[i%2][j-coins[i]] + 1;
-                dp[i%2][j] = Math.min(notTake,take);
-            }
-        }
-        int ans = dp[(n-1)%2][amount];
-        if(ans>=(int)Math.pow(10,9)) return -1;
-        return ans;
+        int[] dp=new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++)
+            for(int c:coins)
+                if(i>=c) dp[i]=Math.min(dp[i],dp[i-c]+1);
+        return dp[amount]>amount?-1:dp[amount];
+
+
     }
 }
-
-
