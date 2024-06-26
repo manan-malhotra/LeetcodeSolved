49c9044f93449d8700b735ed8c6025b34ac6ee2a
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-26 15:38:34 +0530

    Time: 5 ms (65.32%) | Memory: 44.8 MB (67.34%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -2,21 +2,26 @@ class Solution {
     public int minDistance(String s1, String s2) {
         int n = s1.length();
         int m = s2.length();
-        int[][] dp = new int[n][m];
-        for(int[] arr : dp) Arrays.fill(arr,-1);
-        return count(n-1,m-1,n,m,s1,s2,dp);
-    }
-    public int count(int i,int j,int n,int m, String s1, String s2,int[][] dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1.charAt(i) == s2.charAt(j)){
-            return dp[i][j] = count(i-1,j-1,n,m,s1,s2,dp);
+        int[][] dp = new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0) dp[i][j] = j;
+                else if(j==0) dp[i][j] = i;
+                else{
+                    if(s1.charAt(i-1) == s2.charAt(j-1)){
+                        dp[i][j] = dp[i-1][j-1];
+                    }
+                    else{
+                        int replace = dp[i-1][j-1];
+                        int delete = dp[i-1][j];
+                        int add = dp[i][j-1];
+                        dp[i][j] =  Math.min(replace,Math.min(delete,add)) + 1;
+                    }
+                }
+            }
         }
-        int replace = count(i-1,j-1,n,m,s1,s2,dp);
-        int delete = count(i-1,j,n,m,s1,s2,dp);
-        int add = count(i,j-1,n,m,s1,s2,dp);
-        return dp[i][j] =  Math.min(replace,Math.min(delete,add)) + 1;
+        return dp[n][m];
     }
+   
 }
 
