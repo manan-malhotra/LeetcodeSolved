5d869619f4132df52d8cee522e8aa57d1fa79d93
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-26 16:17:15 +0530

    Time: 28 ms (17.18%) | Memory: 44.6 MB (60.09%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -2,14 +2,14 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int n = s.length();
         int m = p.length();
-        boolean[][] dp = new boolean[n+1][m+1];
-        for(int i=0;i<=n;i++){
+        boolean[][] dp = new boolean[2][m+1];
+        dp[0][0] = true;
+        for(int j=1;j<=m;j++){
+            if(p.charAt(j-1)=='*') dp[0][j] = dp[0][j-1];
+        }
+        for(int i=1;i<=n;i++){
             for(int j=0;j<=m;j++){
-                if(i==0 && j==0) dp[0][0] = true;
-                else if(i==0){
-                    if(p.charAt(j-1)=='*') dp[0][j] = dp[0][j-1];
-                }
-                else if(j==0) dp[i%2][0] = false;
+                if(j==0) dp[i%2][j] = false;
                 else if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1)=='?'){
                     dp[i%2][j] = dp[(i-1)%2][j-1];
                 }
