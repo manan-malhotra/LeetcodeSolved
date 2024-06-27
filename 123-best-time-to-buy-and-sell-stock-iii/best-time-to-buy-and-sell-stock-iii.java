e9de62dec3bb9c0bac56a3f22ebb9387a1415b86
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-27 08:32:02 +0530

    Time: 66 ms (41.09%) | Memory: 63.9 MB (32.84%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,15 +1,29 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n][2][2];
-        for(int [][] arr:dp){
-            for(int[] arr2:arr) Arrays.fill(arr2,-1);
+        int[][][] dp = new int[n][2][3];
+        dp[n-1][1][0] = prices[n-1];
+        dp[n-1][1][1] = prices[n-1];
+        for(int i=n-2;i>=0;i--){
+            for(int isBought = 1;isBought>=0;isBought--){
+                for(int timesSold = 1; timesSold>=0;timesSold--){
+                    if(isBought==1){
+                        int hold = dp[i+1][isBought][timesSold];
+                        int sell = prices[i] + dp[i+1][0][timesSold+1];
+                        dp[i][isBought][timesSold] = Math.max(sell,hold);
+                    }else{
+                        int buy =  dp[i+1][1][timesSold] - prices[i];
+                        int ignore = dp[i+1][isBought][timesSold];
+                        dp[i][isBought][timesSold] = Math.max(buy,ignore);
+                    }
+                }
+            }
         }
-        return maxProfit(0,0,0,prices,n-1,dp);
+        return dp[0][0][0];
     }
     public int maxProfit(int i,int isBought, int timesSold,int[] prices, int n,int[][][]dp){
-        if(timesSold == 2) return 0;
         if(dp[i][isBought][timesSold]!=-1) return dp[i][isBought][timesSold];
+        if(timesSold == 2) return dp[i][isBought][timesSold] = 0;
         if(i==n){
             if(isBought==1) return dp[i][isBought][timesSold] = prices[i];
             return dp[i][isBought][timesSold] = 0;
