d7f0f100aaa0d15bc7889b963915e4fc85229ef0
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-27 15:00:09 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (70.97%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,19 +1,19 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][] dp = new int[n][2];
-        dp[n-1][1] = prices[n-1];
+        int[][] dp = new int[2][2];
+        dp[(n-1)%2][1] = prices[n-1];
         for(int i=n-2;i>=0;i--){
             for(int isBought=1; isBought>=0;isBought--){
                 if(isBought==1){
                     int sell = prices[i] ;
-                    if(i+2<n)sell += dp[i+2][0];
-                    int hold = dp[i+1][isBought];
-                     dp[i][isBought] = Math.max(sell,hold);
+                    if(i+2<n)sell += dp[(i+2)%2][0];
+                    int hold = dp[(i+1)%2][isBought];
+                     dp[i%2][isBought] = Math.max(sell,hold);
                 }else{
-                    int buy =  dp[i+1][1] - prices[i];
-                    int ignore = dp[i+1][isBought];
-                     dp[i][isBought] = Math.max(buy,ignore);
+                    int buy =  dp[(i+1)%2][1] - prices[i];
+                    int ignore = dp[(i+1)%2][isBought];
+                     dp[i%2][isBought] = Math.max(buy,ignore);
                 }
             }
         }
