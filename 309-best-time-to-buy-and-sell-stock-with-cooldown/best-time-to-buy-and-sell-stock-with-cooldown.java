00a7f61ff3ae29f9e817bdb74b040ecd35c8318f
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-27 14:57:49 +0530

    Time: 1 ms (81.94%) | Memory: 41.5 MB (70.97%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,26 +1,24 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][] dp = new int[2][n];
-        for(int[] arr:dp) Arrays.fill(arr,-1);
-        return maxProfit(0,0,prices,n-1,dp);
-    }
-    public int maxProfit(int i, int isBought, int[] prices, int n,int[][] dp){
-        if(i>n) return 0;
-        if(dp[isBought][i]!=-1) return dp[isBought][i];
-        if(i==n){
-            if(isBought==1) return dp[isBought][i] = prices[i];
-            else return dp[isBought][i] = 0;
+        int[][] dp = new int[n][2];
+        dp[n-1][1] = prices[n-1];
+        for(int i=n-2;i>=0;i--){
+            for(int isBought=1; isBought>=0;isBought--){
+                if(isBought==1){
+                    int sell = prices[i] ;
+                    if(i+2<n)sell += dp[i+2][0];
+                    int hold = dp[i+1][isBought];
+                     dp[i][isBought] = Math.max(sell,hold);
+                }else{
+                    int buy =  dp[i+1][1] - prices[i];
+                    int ignore = dp[i+1][isBought];
+                     dp[i][isBought] = Math.max(buy,ignore);
+                }
+            }
         }
-        if(isBought==1){
-            int sell = prices[i] ;
-            if(i+2<=n)sell += maxProfit(i+2,0,prices,n,dp);
-            int hold = maxProfit(i+1,isBought,prices,n,dp);
-            return dp[isBought][i] = Math.max(sell,hold);
-        }else{
-            int buy =  maxProfit(i+1,1,prices,n,dp) - prices[i];
-            int ignore = maxProfit(i+1,isBought,prices,n,dp);
-            return dp[isBought][i] = Math.max(buy,ignore);
-        }
+        return dp[0][0];
     }
 }
+
+
