185dcb0565155ecee25abe9294d31ebb1a9914e2
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-27 15:06:28 +0530

    Time: 7 ms (60.90%) | Memory: 54.7 MB (91.44%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,18 +1,18 @@
 class Solution {
     public int maxProfit(int[] prices,int fee) {
         int n = prices.length;
-        int[][] dp = new int[2][n];
-        dp[1][n-1] = prices[n-1];
+        int[][] dp = new int[2][2];
+        dp[1][(n-1)%2] = prices[n-1];
         for(int i=n-2;i>=0;i--){
             for(int isBought=1;isBought>=0;isBought--){
                 if(isBought==1){
-                    int sell = prices[i] + dp[0][i+1];
-                    int hold = dp[1][i+1];
-                    dp[1][i] = Math.max(sell,hold);
+                    int sell = prices[i] + dp[0][(i+1)%2];
+                    int hold = dp[1][(i+1)%2];
+                    dp[1][i%2] = Math.max(sell,hold);
                 }else{
-                    int buy =  dp[1][i+1] - prices[i]-fee;
-                    int ignore = dp[0][i+1];
-                    dp[0][i] = Math.max(buy,ignore);
+                    int buy =  dp[1][(i+1)%2] - prices[i]-fee;
+                    int ignore = dp[0][(i+1)%2];
+                    dp[0][i%2] = Math.max(buy,ignore);
                 }
             }
         }
