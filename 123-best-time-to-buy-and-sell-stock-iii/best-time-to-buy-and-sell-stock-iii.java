15f7a5ac6f403168051cc6cf42c0c61540dcab96
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-27 08:35:05 +0530

    Time: 12 ms (51.31%) | Memory: 60.9 MB (59.62%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,20 +1,20 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n][2][3];
-        dp[n-1][1][0] = prices[n-1];
-        dp[n-1][1][1] = prices[n-1];
+        int[][][] dp = new int[2][2][3];
+        dp[(n-1)%2][1][0] = prices[n-1];
+        dp[(n-1)%2][1][1] = prices[n-1];
         for(int i=n-2;i>=0;i--){
             for(int isBought = 1;isBought>=0;isBought--){
                 for(int timesSold = 1; timesSold>=0;timesSold--){
                     if(isBought==1){
-                        int hold = dp[i+1][isBought][timesSold];
-                        int sell = prices[i] + dp[i+1][0][timesSold+1];
-                        dp[i][isBought][timesSold] = Math.max(sell,hold);
+                        int hold = dp[(i+1)%2][isBought][timesSold];
+                        int sell = prices[i] + dp[(i+1)%2][0][timesSold+1];
+                        dp[i%2][isBought][timesSold] = Math.max(sell,hold);
                     }else{
-                        int buy =  dp[i+1][1][timesSold] - prices[i];
-                        int ignore = dp[i+1][isBought][timesSold];
-                        dp[i][isBought][timesSold] = Math.max(buy,ignore);
+                        int buy =  dp[(i+1)%2][1][timesSold] - prices[i];
+                        int ignore = dp[(i+1)%2][isBought][timesSold];
+                        dp[i%2][isBought][timesSold] = Math.max(buy,ignore);
                     }
                 }
             }
