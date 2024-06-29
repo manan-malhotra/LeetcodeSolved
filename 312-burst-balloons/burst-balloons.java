231966f3a85c7cf10136e0ea4ab86516d7d167f6
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-29 16:27:15 +0530

    Time: 630 ms (5.25%) | Memory: 46.1 MB (5.56%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -6,8 +6,27 @@ class Solution {
         for(int num:nums) arr.add(num);
         arr.add(1);
         int[][] dp = new int[n+2][n+2];
-        for(int row[] : dp) Arrays.fill(row,-1);
-        return f(1,n,arr,dp);
+        for(int i=1;i<=n;i++){
+            dp[i][i] = arr.get(i-1)*arr.get(i)*arr.get(i+1);
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n;j++){
+                if(i>=j) continue;
+                int maxi = Integer.MIN_VALUE;
+                for(int k=i;k<=j;k++){
+                    int total = dp[i][k-1] + dp[k+1][j] + (arr.get(i-1) * arr.get(k) * arr.get(j+1));
+                    maxi = Math.max(maxi,total);
+                }
+                dp[i][j] = maxi;
+            }
+        }
+        for(int[] row:dp){
+            for(int nn:row){
+                System.out.print(nn+" ");
+            }
+            System.out.println();
+        }
+        return dp[1][n];
     }
     public int f(int i, int j, List<Integer> arr,int[][] dp){
         if(dp[i][j]!=-1) return dp[i][j];
@@ -23,3 +42,6 @@ class Solution {
         return dp[i][j] = maxi;
     }
 }
+
+
+
