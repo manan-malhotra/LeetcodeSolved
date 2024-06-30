a8dad00b26564ce7b42fadf2fe9abe6517301c71
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-30 12:04:14 +0530

    Time: 95 ms (5.26%) | Memory: 45 MB (5.61%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -2,23 +2,29 @@ class Solution {
     public int maxSumAfterPartitioning(int[] arr, int k) {
         int n = arr.length;
         int[] dp = new int[n+1];
+        int[][] max = new int[n][n+1];
+        for(int[] row:max) Arrays.fill(row,-1);
+        for(int i=0;i<n;i++){
+            max[i][i] = arr[i];
+        }
         dp[n] = 0;
         for(int i=n-1;i>=0;i--){
             int maxCost = Integer.MIN_VALUE;
             for(int j=i;j<i+k;j++){
                 if(j>=n) continue;
-                int cost = (findMax(i,j+1,arr)*(j+1-i)) + dp[j+1];
+                int cost = (findMax(i,j+1,arr,max)*(j+1-i)) + dp[j+1];
                 maxCost = Math.max(cost,maxCost);
             }
             dp[i] = maxCost;
         }
         return dp[0];
     }
-    public int findMax(int i,int j,int[] arr){
-        int max = 0;
+    public int findMax(int i,int j,int[] arr,int[][] max){
+        if(max[i][j]!=-1) return max[i][j];
+        int currMax = 0;
         for(int num=i;num<j;num++){
-            max = Math.max(max,arr[num]);
+            currMax = Math.max(currMax,arr[num]);
         }
-        return max;
+        return max[i][j] = currMax;
     }
 }
