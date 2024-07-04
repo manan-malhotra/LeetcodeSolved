65124217015e71abcf5098a67d2eb75fa02e8982
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-07-04 15:44:36 +0530

    Time: 392 ms (82.04%) | Memory: 106.3 MB (77.40%) - LeetSync

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.java
@@ -0,0 +1,74 @@
+class Node{
+    char data;
+    Node left;
+    Node right;
+
+    Node(char data){
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+class Solution {
+     static void insert(Node root, int num){
+        Node curr = root;
+        
+        for(int i=31; i>=0; i--){
+            int bit = (num>>i) & 1;
+            if(bit==0){
+                if(curr.left==null){
+                    Node x = new Node('0');
+                    curr.left = x;
+                }
+                curr = curr.left;
+            }else{
+                if(curr.right==null){
+                    Node x = new Node('1');
+                    curr.right = x;   
+                }
+                curr = curr.right;
+            }
+        }
+    }
+
+    
+    static int helper(Node root, int num){
+        int ans = 0;
+        Node curr = root;
+
+        for(int i=31; i>=0; i--){
+            int bit = (num>>i) & 1;
+            if(bit==0){
+                if(curr.right!=null){
+                    curr = curr.right;
+                    ans += (1 << i);
+                }else{
+                    curr = curr.left;
+                }
+            }else{
+                if(curr.left!=null){
+                    curr = curr.left;
+                    ans += (1 << i);
+                }else{
+                    curr = curr.right;
+                }
+            }
+        }
+        return ans;
+    }
+
+
+    public int findMaximumXOR(int[] arr) {
+        int n = arr.length;
+        int ans = 0;
+
+
+        Node root = new Node('.');
+        insert(root,arr[0]);
+        for(int i=1; i<n; i++){
+            ans = Math.max(ans,helper(root,arr[i]));
+            insert(root,arr[i]);
+        }
+        return ans;
+    }
+}
