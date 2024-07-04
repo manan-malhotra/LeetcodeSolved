f3fe4583a8f33cf30108e170de41f49e31be5f1e
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-07-04 15:50:04 +0530

    Time: 126 ms (5.08%) | Memory: 42.5 MB (72.31%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -60,4 +60,10 @@ public class Solution {
         return canSegmentString(root, s, 0, memo);
     }
 
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        List<String> wordDict = Arrays.asList("leet", "code");
+        String s = "leetcode";
+        System.out.println(solution.wordBreak(s, wordDict)); // true
+    }
 }
