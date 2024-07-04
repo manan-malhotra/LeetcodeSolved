549cbd8ccc7e51c1a60db2d5aede15c749a36dd5
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-07-04 16:35:10 +0530

    Time: 50 ms (94.75%) | Memory: 44.6 MB (77.94%) - LeetSync

--- 212-word-search-ii/word-search-ii.java
+++ 212-word-search-ii/word-search-ii.java
@@ -1,54 +1,83 @@
 class Solution {
+
+    private TrieNode root = new TrieNode();
+    private HashSet<String> result = new HashSet<>();
+
     public List<String> findWords(char[][] board, String[] words) {
-        List<String> res = new ArrayList<>();
-        TrieNode root = buildTrie(words);
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                dfs(board, i, j, root, res);
+        addWordToTrie(words);
+
+        int row = board.length;
+        int col = board[0].length;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                TrieNode node = root;
+
+                recursiveSearch(board, node, i, j);
             }
         }
-        return res;
+        return new ArrayList<>(result);
     }
 
-    public void dfs(char[][] board, int i, int j, TrieNode p, List<String> res) {
-        char c = board[i][j];
-        if (c == '#' || p.next[c - 'a'] == null)
+    private void recursiveSearch(char[][] board, TrieNode node, int row, int col) {
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) {
             return;
-        p = p.next[c - 'a'];
-        if (p.word != null) { // found one
-            res.add(p.word);
-            p.word = null; // de-duplicate
         }
 
-        board[i][j] = '#';
-        if (i > 0)
-            dfs(board, i - 1, j, p, res);
-        if (j > 0)
-            dfs(board, i, j - 1, p, res);
-        if (i < board.length - 1)
-            dfs(board, i + 1, j, p, res);
-        if (j < board[0].length - 1)
-            dfs(board, i, j + 1, p, res);
-        board[i][j] = c;
-    }
 
-    public TrieNode buildTrie(String[] words) {
-        TrieNode root = new TrieNode();
-        for (String w : words) {
-            TrieNode p = root;
-            for (char c : w.toCharArray()) {
-                int i = c - 'a';
-                if (p.next[i] == null)
-                    p.next[i] = new TrieNode();
-                p = p.next[i];
+        
+        if (node.children.containsKey(board[row][col])) {
+            char currChar = board[row][col];
+            TrieNode currNode = node.children.get(currChar);
+
+            if (currNode.isWord()) {
+                result.add(currNode.getWord());
             }
-            p.word = w;
-        }
-        return root;
-    }
-}
+            board[row][col] = '#';
+            recursiveSearch(board, currNode, row - 1, col);
+            recursiveSearch(board, currNode, row + 1, col);
+            recursiveSearch(board, currNode, row, col - 1);
+            recursiveSearch(board, currNode, row, col + 1);
+            board[row][col] = currChar;
 
-class TrieNode {
-    TrieNode[] next = new TrieNode[26];
-    String word;
+            if (currNode.children.isEmpty()) {
+                node.children.remove(board[row][col]);
+            }
+        }
+        
+
+        
+
+        return;
+    }
+
+    private void addWordToTrie(String[] words) {
+        for (String word : words) {
+            TrieNode node = root;
+            for (int i = 0; i < word.length(); i++) {
+                if (!node.children.containsKey(word.charAt(i))) {
+                    node.children.put(word.charAt(i), new TrieNode());
+                } 
+                node = node.children.get(word.charAt(i));
+            }
+            node.setWord(word);
+        }
+    }
+
+    private class TrieNode {
+        HashMap<Character, TrieNode> children = new HashMap<>();
+        String word;
+
+        public void setWord(String word) {
+            this.word = word;
+        }
+
+        public String getWord() {
+            return this.word;
+        }
+
+        public boolean isWord() {
+            return this.word != null;
+        }
+    }
 }
