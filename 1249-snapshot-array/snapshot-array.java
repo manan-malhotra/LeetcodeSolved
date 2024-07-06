1bf509a9d13a283f1839bd91333ef10ca9013d70
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-07-06 22:58:46 +0530

    Time: 50 ms (90.37%) | Memory: 84.5 MB (14.49%) - LeetSync

--- 1249-snapshot-array/snapshot-array.java
+++ 1249-snapshot-array/snapshot-array.java
@@ -1,11 +1,8 @@
 class SnapshotArray {
-
     int snapId=0;
     List<SnapObj>[] arr;
     public SnapshotArray(int length) {
-        
         this.arr = new ArrayList[length];
-
         for(int i=0; i<length; i++) {
             this.arr[i] = new ArrayList<>();
             this.arr[i].add(new SnapObj(0,0));
@@ -13,10 +10,8 @@ class SnapshotArray {
     }
     
     public void set(int index, int val) {
-
             SnapObj snapObj = new SnapObj(snapId, val);
             arr[index].add(snapObj);
-
     }
     
     public int snap() {
@@ -24,17 +19,11 @@ class SnapshotArray {
     }
     
     public int get(int index, int snap_id) {
-
         List<SnapObj> iterList = arr[index];
-
-        int left=0, right = iterList.size()-1;
-
-        int res=0;
+        int left=0, res=0, right = iterList.size()-1;
         while(left <= right) {
             int mid = left + (right-left) / 2;
-
             SnapObj tempObj = iterList.get(mid);
-
             if(tempObj.id <= snap_id) {
                 res = mid;
                 left = mid+1;
@@ -42,14 +31,12 @@ class SnapshotArray {
                 right = mid-1;
             }
         }
-
         return iterList.get(res).val;
     }
 
     public class SnapObj {
         int id;
         int val;
-
         public SnapObj(int id, int val) {
             this.id = id;
             this.val = val;
