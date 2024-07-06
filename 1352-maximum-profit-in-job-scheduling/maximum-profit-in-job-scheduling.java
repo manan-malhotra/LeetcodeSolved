6ecf430c905c2f3d492a2e356a03347a1ca78f85
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-07-06 12:15:21 +0530

    Time: 18 ms (100.00%) | Memory: 56.2 MB (62.19%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
@@ -1,44 +1,55 @@
 class Solution {
-    /**
-     * Calculates the maximum profit that can be obtained by scheduling jobs without overlapping time ranges.
-     *
-     * @param startTime An array representing the start times of jobs.
-     * @param endTime   An array representing the end times of jobs.
-     * @param profit    An array representing the profit associated with each job.
-     * @return The maximum profit achievable without overlapping time ranges.
-     */
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        // Get the number of jobs
-        int n = profit.length;
-
-        // Create a 2D array 'jobs' to store information about each job
-        int[][] jobs = new int[n][3];
-
-        // Populate 'jobs' with start times, end times, and profits
-        for (int i = 0; i < n; i++) {
-            jobs[i] = new int[]{startTime[i], endTime[i], profit[i]};
+        int len = profit.length;
+        
+        Job[] jobs = new Job[len];
+        for (int i = 0; i < len; i++) {
+            jobs[i] = new Job(startTime[i], endTime[i], profit[i]);
         }
-
-        // Sort 'jobs' based on end times in ascending order
-        Arrays.sort(jobs, (a, b) -> a[1] - b[1]);
-
-        // Use a TreeMap to store the dynamic programming states (end time -> maximum profit)
-        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
-        treeMap.put(0, 0);  // Initialize the TreeMap with a key-value pair representing no jobs scheduled
-
-        // Iterate through the sorted jobs to find the optimal schedule
-        for (int[] job : jobs) {
-            // Calculate the current profit by adding the current job's profit to the maximum profit until the end time of the previous job
-            int currentProfit = treeMap.floorEntry(job[0]).getValue() + job[2];
-
-            // Check if the current profit is greater than the maximum profit obtained so far
-            if (currentProfit > treeMap.lastEntry().getValue()) {
-                // Update the TreeMap with a new key-value pair representing the end time of the current job and the maximum profit achievable
-                treeMap.put(job[1], currentProfit);
+        
+        Arrays.sort(jobs, (a, b) -> Integer.compare(a.end, b.end));
+        
+        int[] dp = new int[len];
+        dp[0] = jobs[0].profit;
+        
+        for (int i = 1; i < len; i++) {
+            int start = jobs[i].start;
+            int left = 0;
+            int right = i - 1;
+            int res = -1;
+            
+            while (left <= right) {
+                int mid = left + ((right - left) >> 1);
+                if (jobs[mid].end <= start) {
+                    res = mid;
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
+            
+            int doCurJob = jobs[i].profit;
+            if (res != -1) {
+                doCurJob += dp[res];
+            }
+            
+            int notDoCurJob = dp[i - 1];
+            
+            dp[i] = Math.max(doCurJob, notDoCurJob);
         }
+        
+        return dp[len - 1];
+    }
+}
 
-        // Return the maximum profit achievable by considering all jobs
-        return treeMap.lastEntry().getValue();
+class Job {
+    int start;
+    int end;
+    int profit;
+    
+    public Job(int start, int end, int profit) {
+        this.start = start;
+        this.end = end;
+        this.profit = profit;
     }
 }
