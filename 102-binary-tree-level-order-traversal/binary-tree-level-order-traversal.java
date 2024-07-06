16c599cf047685107e9ecc4fb0982e4b2013a88e
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-07-06 19:41:24 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (42.86%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -1,35 +1,16 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
-        if(root==null) return ans;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            List<Integer> temp = new ArrayList<>();
-            for(int i=0;i<size;i++){
-                TreeNode node = queue.poll();
-                temp.add(node.val);
-                if(node.left!=null) queue.offer(node.left);
-                if(node.right!=null) queue.offer(node.right);
-            }
-            ans.add(new ArrayList<>(temp));
-        }
+        helper(root, 0, ans);
         return ans;
+    }
+    private void helper(TreeNode root, int level, List<List<Integer>> ans) {
+        if (root == null)
+            return;
+        if (ans.size() <= level)
+            ans.add(new ArrayList<>());
+        ans.get(level).add(root.val);
+        helper(root.left, level + 1, ans);
+        helper(root.right, level + 1, ans);
     }
 }
