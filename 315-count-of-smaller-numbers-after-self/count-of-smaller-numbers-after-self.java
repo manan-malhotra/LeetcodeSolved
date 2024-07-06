e62174010728f2c58d1497ed56a2ce977ae17d26
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-07-06 11:57:14 +0530

    Time: 14 ms (97.85%) | Memory: 58.4 MB (75.17%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.java
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.java
@@ -1,42 +1,31 @@
-class Solution {
+class Solution {    
     public List<Integer> countSmaller(int[] nums) {
-        int min = nums[0];
-        int max = nums[0];
-        for (int i = 0; i < nums.length; i++) {
-            final int v = nums[i];
-            if (v < min) {
-                min = v;
-            } else if (v > max) {
-                max = v;
+        int min = 20001;
+        int max = -1;
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+        
+        min--;
+        int[] count = new int[max-min+1];
+        Integer[] result = new Integer[nums.length];
+        for (int i = nums.length-1; i >=0; i--) {
+            int k = nums[i]-min-1;
+            int c = 0;
+            do {
+                c += count[k];
+                k -= (-k&k);
+            } while (k > 0);
+            result[i] = c;
+            
+            k = nums[i]-min;
+            while (k < count.length) {
+                count[k]++;
+                k += (-k&k);
             }
         }
-        final int delta = -min + 1;
-        final int[] arr = new int[max + delta + 1];
-        final int[] res = new int[nums.length];
-        for (int i = nums.length - 1; i >= 0; i--) {
-            final int v = nums[i] + delta;
-            res[i] = get(arr, v - 1);
-            add(arr, v);
-        }
-        return new java.util.AbstractList<Integer>() {
-            @Override
-            public Integer get(int index) {
-                return res[index];
-            }
-            @Override
-            public int size() {
-                return res.length;
-            }
-        };
-    }
-    static int get(final int[] arr, int v) {
-        int sum = 0;
-        for (; v > 0; v -= v & -v)
-            sum += arr[v];
-        return sum;
-    }
-    static void add(final int[] arr, int v) {
-        for (; v < arr.length; v += v & -v)
-            arr[v]++;
+        
+        return Arrays.asList(result);
     }
 }
