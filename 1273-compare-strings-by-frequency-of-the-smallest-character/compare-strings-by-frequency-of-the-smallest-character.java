f8920580fe85fe5e11db39f63e785be72c21a90e
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-07-09 23:43:57 +0530

    Time: 3 ms (88.52%) | Memory: 44.8 MB (60.25%) - LeetSync

--- 1273-compare-strings-by-frequency-of-the-smallest-character/compare-strings-by-frequency-of-the-smallest-character.java
+++ 1273-compare-strings-by-frequency-of-the-smallest-character/compare-strings-by-frequency-of-the-smallest-character.java
@@ -1,36 +1,43 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 class Solution {
-    public int[] numSmallerByFrequency(String[] queries, String[] words) {
-        int[] res = new int[queries.length];
-        int j =0;
-        for(String query : queries){
-            int val = 0;
-            for(String word : words){
-                if(frequency(query) < frequency(word)){
-                    val++;
-                }
-            }
-            res[j] = val;
-            j++;
-        }
-        return Arrays.stream(res).toArray();
+  public int[] numSmallerByFrequency(String[] queries, String[] words) {
+    int[] ans = new int[queries.length];
+    int[] wordsFreq = new int[words.length];
+
+    for (int i = 0; i < words.length; ++i)
+      wordsFreq[i] = f(words[i]);
+    Arrays.sort(wordsFreq);
+
+    for (int i = 0; i < queries.length; ++i) {
+      final int freq = f(queries[i]);
+      ans[i] = words.length - firstGreater(wordsFreq, 0, wordsFreq.length, freq);
     }
-    public int frequency(String word){
-        List<Character> list = new ArrayList<>();
-        int count = 0;
-        char[] arr = word.toCharArray();
-        Arrays.sort(arr);
-        for(int i = 0; i<arr.length;i++){
-            if(arr[0] == arr[i]){
-                count++;
-            }
-            else{
-                break;
-            }
-        }
-        return count;
+
+    return ans;
+  }
+
+  private int f(final String word) {
+    int count = 0;
+    char currentChar = 'z' + 1;
+
+    for (final char c : word.toCharArray())
+      if (c < currentChar) {
+        currentChar = c;
+        count = 1;
+      } else if (c == currentChar) {
+        ++count;
+      }
+
+    return count;
+  }
+
+  private int firstGreater(int[] nums, int l, int r, int value) {
+    while (l < r) {
+      final int m = (l + r) / 2;
+      if (nums[m] > value)
+        r = m;
+      else
+        l = m + 1;
     }
+    return l;
+  }
 }
