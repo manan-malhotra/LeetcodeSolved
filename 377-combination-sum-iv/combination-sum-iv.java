a849b540604c46bd6afcfbb2308002f258a0c889
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-07-09 11:48:47 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (55.47%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.java
+++ 377-combination-sum-iv/combination-sum-iv.java
@@ -1,11 +1,11 @@
 class Solution {
-    int[] t;
+    
     public int combinationSum4(int[] nums, int target) {
-        t = new int[target+1];
-        Arrays.fill(t,-1);
-        return solve(nums,target);
+        int[] dp = new int[target+1];
+        Arrays.fill(dp,-1);
+        return solve(nums,target,dp);
     }
-    public int solve(int[] nums, int target)
+    public int solve(int[] nums, int target,int[] dp)
     {
         if(target == 0) {
             return 1;
@@ -14,14 +14,14 @@ class Solution {
             return 0;
         }
         
-        if(t[target] != -1) {
-            return t[target];
+        if(dp[target] != -1) {
+            return dp[target];
         }
         
         int result = 0;
         for(int i = 0; i<nums.length;i++) {
-            result += solve(nums, target-nums[i]);
+            result += solve(nums, target-nums[i],dp);
         }
-        return t[target] = result;
+        return dp[target] = result;
     }
 }
