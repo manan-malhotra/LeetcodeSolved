5ea434a20fa2e02e2722149f9c0c1bab6b0d5364
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-07-10 18:09:25 +0530

    Time: 48 ms (79.24%) | Memory: 45.2 MB (90.79%) - LeetSync

--- 808-number-of-matching-subsequences/number-of-matching-subsequences.java
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.java
@@ -1,22 +1,23 @@
 class Solution {
     public int numMatchingSubseq(String s, String[] words) {
         int count = 0;
+        char[] str = s.toCharArray();
         Map<String,Integer> map = new HashMap<>();
         for(String word:words){
             map.put(word,map.getOrDefault(word,0)+1);
         }
         for(String word:map.keySet()){
-            if(isSubsequence(s,word)) count+= map.get(word);
+            if(isSubsequence(str,word.toCharArray())) count+= map.get(word);
         }
         return count;
     }
-    public boolean isSubsequence(String s, String word){
-        int n = s.length();
-        int m = word.length();
+    public boolean isSubsequence(char[] s, char[] word){
+        int n = s.length;
+        int m = word.length;
         if(m>n) return false;
         int i=0, j=0;
         while(j!=m && i!=n){
-            if(s.charAt(i)==word.charAt(j)){
+            if(s[i]==word[j]){
                 j++;
             }
             i++;
