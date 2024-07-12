2bbfee5d8fe213eb9d33eeb2a7a99e22815bb9ba
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-07-12 20:52:16 +0530

    Time: 20 ms (100.00%) | Memory: 45.2 MB (93.94%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
@@ -1,43 +1,36 @@
 class Solution {
     public int maximumGain(String s, int x, int y) {
-        int res = 0;
-        String top, bot;
-        int top_score, bot_score;
-
-        if (y > x) {
-            top = "ba";
-            top_score = y;
-            bot = "ab";
-            bot_score = x;
-        } else {
-            top = "ab";
-            top_score = x;
-            bot = "ba";
-            bot_score = y;
-        }
-
-        // Removing first top substrings cause they give more points
-        StringBuilder stack = new StringBuilder();
-        for (char ch : s.toCharArray()) { // Changed 'char' to 'ch'
-            if (ch == top.charAt(1) && stack.length() > 0 && stack.charAt(stack.length() - 1) == top.charAt(0)) {
-                res += top_score;
-                stack.setLength(stack.length() - 1);
+        
+        int aCount = 0;
+        int bCount = 0;
+        int lesser = Math.min(x, y);
+        int result = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c > 'b') {
+                result += Math.min(aCount, bCount) * lesser;
+                aCount = 0;
+                bCount = 0;
+            } else if (c == 'a') {
+                if (x < y && bCount > 0) {
+                    bCount--;
+                    result += y;
+                } else {
+                    aCount++;
+                }
             } else {
-                stack.append(ch);
+                if (x > y && aCount > 0) {
+                    aCount--;
+                    result += x;
+                } else {
+                    bCount++;
+                };
             }
         }
-
-        // Removing bot substrings cause they give less or equal amount of scores
-        StringBuilder new_stack = new StringBuilder();
-        for (char ch : stack.toString().toCharArray()) { // Changed 'char' to 'ch'
-            if (ch == bot.charAt(1) && new_stack.length() > 0 && new_stack.charAt(new_stack.length() - 1) == bot.charAt(0)) {
-                res += bot_score;
-                new_stack.setLength(new_stack.length() - 1);
-            } else {
-                new_stack.append(ch);
-            }
-        }
-
-        return res;
+        
+        result += Math.min(aCount, bCount) * lesser;
+        
+        return result;
     }
 }
