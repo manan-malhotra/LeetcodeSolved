34782030c848893cb1b740141c184c64897c4ed1
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-07-20 08:51:29 +0530

    Time: 35 ms (15.05%) | Memory: 44.5 MB (16.45%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -8,32 +8,20 @@ class Solution {
         HashMap<Character, Integer> map = new HashMap<>();
 
         while (right < n) {
-            // Perform operation on right
             char rightChar = s.charAt(right);
             map.put(rightChar, map.getOrDefault(rightChar, 0) + 1);
             maxCount = Math.max(maxCount, map.get(rightChar));
 
-            // If the window size minus the count of the most frequent character is greater than k
-            // Reduce the window from the left
-            // maxCount is the maximum of same character, the condition dicdated we can change anything to the required character 
-            //so the remaining characters == length-maxCount. Here "current_length" is right-left+1. hope it is clear now
             if (right - left + 1 - maxCount > k) {
                 char leftChar = s.charAt(left);
                 map.put(leftChar, map.get(leftChar) - 1);
 
-                /* this part is not required because we dont have to do 
-                anything with size() of map reference fruits into basket
-                
                 if(map.get(leftChar)==0) map.remove(leftChar);
 
-                */
-                
                 left++;
             }
 
-            // Compute the max length
             max = Math.max(max, right - left + 1);
-            // Increment right to calculate for the next value
             right++;
         }
         return max;
