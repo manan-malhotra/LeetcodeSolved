30355a6b7b6873cd5da48b0aae4ca4024fbec4f0
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-07-23 19:53:05 +0530

    Time: 2 ms (71.07%) | Memory: 44.4 MB (30.42%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,23 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        for(int i=0;i<preorder.length;i++){
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i=0;i<inorder.length;i++){
             hm.put(inorder[i],i);
         }
-        int[] index = {0};
-        int l = 0;
-        int r = inorder.length-1;
-        return helper(preorder,inorder,l,r,hm,index);
+        return build(preorder,0,preorder.length-1,inorder,0,inorder.length-1,hm);
     }
-    public TreeNode helper(int[] preorder, int[] inorder, int l, int r, HashMap<Integer,Integer> hm,int[] index){
-        if(l>r) return null;
-        int current = preorder[index[0]];
-        index[0]++;
-        TreeNode node = new TreeNode(current);
-        if(l==r) return node;
-        int inOrderIndex = hm.get(current);
-        node.left=helper(preorder,inorder,l,inOrderIndex-1,hm,index);
-        node.right=helper(preorder,inorder,inOrderIndex+1,r,hm,index);
-        return node;
+
+    public TreeNode build(int[] preorder, int ps, int pe,
+    int[] inorder, int is,int ie,HashMap<Integer,Integer> hm){
+        if(ps>pe || is>ie) return null;
+        TreeNode root = new TreeNode(preorder[ps]);
+        int index = hm.get(preorder[ps]);
+        root.left = build(preorder,ps+1,ps+(index-is),inorder,is,index-1,hm);
+        root.right = build(preorder,ps+(index-is)+1,pe,inorder,index+1,ie,hm);
+        return root;
     }
+
+
+
 }
