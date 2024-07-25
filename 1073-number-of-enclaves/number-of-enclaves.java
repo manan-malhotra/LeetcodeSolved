21ecc5e69f10a6d29bc9fad3f8d3395607378a25
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-07-25 22:39:04 +0530

    Time: 7 ms (88.94%) | Memory: 56.7 MB (52.48%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,38 +1,30 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-        int count = 0;
         int n = grid.length;
         int m = grid[0].length;
         for(int i=0;i<n;i++){
-            if(grid[i][0]==1){
-                dfs(grid,i,0,n,m);
-            }
-            if(grid[i][m-1]==1){
-                dfs(grid,i,m-1,n,m);
-            }
+            if(grid[i][0]==1)dfs(i,0,grid);
+            if(grid[i][m-1]==1)dfs(i,m-1,grid);
         }
-        for(int j=0;j<m;j++){
-            if(grid[0][j]==1){
-                dfs(grid,0,j,n,m);
-            }
-            if(grid[n-1][j]==1){
-                dfs(grid,n-1,j,n,m);
-            }
+        for(int i=0;i<m;i++){
+            if(grid[0][i]==1)dfs(0,i,grid);
+            if(grid[n-1][i]==1)dfs(n-1,i,grid);
         }
+        int ans = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1) count++;
+                if(grid[i][j]==2) grid[i][j] = 1;
+                else if(grid[i][j]==1) ans++;;
             }
         }
-        return count;
+        return ans;
     }
-    public void dfs(int[][] grid, int i,int j, int n, int m){
-        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]!=1) return;
-        grid[i][j]=0;
-        dfs(grid,i+1,j,n,m);
-        dfs(grid,i-1,j,n,m);
-        dfs(grid,i,j+1,n,m);
-        dfs(grid,i,j-1,n,m);
-
+    public void dfs(int i, int j, int[][] board){
+        if(i<0 || j<0 || i>= board.length || j>= board[0].length || board[i][j]!=1) return;
+        board[i][j]=2;
+        dfs(i+1,j,board);
+        dfs(i-1,j,board);
+        dfs(i,j+1,board);
+        dfs(i,j-1,board);
     }
 }
