974db8998d817969e936bfaaea923593187bfeb1
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-07-25 18:02:13 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (27.04%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -4,12 +4,12 @@ class Solution {
         int m = grid[0].length;
         if(n==0) return -1;
         int ones = 0;
-        Queue<Pair<Integer,Integer>> q = new LinkedList<>();
+        Queue<int[]> q = new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1) ones++;
                 else if(grid[i][j]==2) {
-                    q.offer(new Pair(i , j));
+                    q.offer(new int[]{i , j});
                 }
             }
         }
@@ -20,16 +20,14 @@ class Solution {
             ++max;
             int size = q.size();
             for(int i=0;i<size;i++){
-            Pair<Integer,Integer> arr = q.poll();
-            int row = arr.getKey();
-            int col = arr.getValue();
+            int[] arr = q.poll();
             for(int k=0;k<dir.length;k++){
-                int x = row+ dir[k][0];
-                int y = col+ dir[k][1];
+                int x = arr[0]+ dir[k][0];
+                int y = arr[1]+ dir[k][1];
                 if(x<0 || y<0 || x>=n || y>=m || grid[x][y]!=1) continue;
                 grid[x][y] = 2;
                 ones--;
-                q.offer(new Pair(x,y));
+                q.offer(new int[]{x , y});
             }
             }
         }    
