af172d689e3a1c00457bb156468b17ee90575ebb
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-07-27 21:36:41 +0530

    Time: 6 ms (81.85%) | Memory: 44 MB (79.06%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,47 +1,50 @@
 class Solution {
+    int min = Integer.MAX_VALUE;
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<List<Stop>> adj = new ArrayList<>();
-        int[] visited = new int[n];
-        Arrays.fill(visited,Integer.MAX_VALUE);
-        for(int i=0;i<n;i++){
-            adj.add(new ArrayList<Stop>());
+        int[] vis = new int[n];
+        Arrays.fill(vis,Integer.MAX_VALUE);
+        vis[src] = 0;
+        List<List<Edge>> adj = new ArrayList<>();
+        for(int i=0; i<n;i++){
+            adj.add(new ArrayList<>());
         }
-        for(int[] flight:flights){
-            int from = flight[0];
+        for(int[] flight : flights){
             int to = flight[1];
-            int price = flight[2];
-            adj.get(from).add(new Stop(to,price));
+            int from = flight[0];
+            int weight = flight[2];
+            adj.get(from).add(new Edge(to,weight));
         }
-        int ans = Integer.MAX_VALUE;
-        Queue<Stop> queue = new LinkedList<>();
-        queue.offer(new Stop(src,0));
+        Queue<Edge> queue = new LinkedList<>();
+        queue.offer(new Edge(src,0));
         k+=2;
-        while(k!=0 && !queue.isEmpty()){
+        while(!queue.isEmpty() && k!=0){
             k--;
             int size = queue.size();
             for(int i=0;i<size;i++){
-                Stop currentStop = queue.poll();
-                int stop = currentStop.stop;
-                int price = currentStop.price;
-                if(stop==dst) ans = Math.min(price,ans);
-                for(Stop stops : adj.get(stop)){
-                    int newStop = stops.stop;
-                    int newprice = stops.price+price;
-                    if(visited[newStop]>newprice){
-                        queue.offer(new Stop(newStop,newprice));
-                        visited[newStop] = newprice;
+                Edge oldEdge = queue.poll();
+                int to = oldEdge.to;
+                int wt = oldEdge.weight;
+                if(to==dst) min = Math.min(min,wt);
+                for(Edge edge : adj.get(to)){
+                    int newStop = edge.to;
+                    int newWt = edge.weight + wt;
+                    if(vis[newStop]>newWt){
+                        queue.offer(new Edge(newStop,newWt));
+                        vis[newStop] = newWt;
                     }
                 }
             }
         }
-        return ans==Integer.MAX_VALUE?-1:ans;
+        return min == Integer.MAX_VALUE ? -1 : min;
     }
+
+
 }
-class Stop{
-    int stop;
-    int price;
-    public Stop(int stop,int price){
-        this.stop=stop;
-        this.price=price;
+class Edge{
+    int to;
+    int weight;
+    public Edge(int to, int weight){
+        this.to = to;
+        this.weight = weight;
     }
 }
