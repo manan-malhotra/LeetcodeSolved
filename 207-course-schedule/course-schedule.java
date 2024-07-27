a74fc53a2b4eba72eb1936e3693d05964cf8020a
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-07-27 14:04:19 +0530

    Time: 3 ms (94.20%) | Memory: 44.5 MB (65.75%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,50 +1,33 @@
 class Solution {
-    public boolean canFinish(int n, int[][] pre) {
-      
-        boolean visited[] = new boolean[n]; 
-        boolean helper[] = new boolean[n];     
-        ArrayList<ArrayList<Integer>> al = new ArrayList<>();
-        
-        for(int i = 0; i < n; i++) 
-        {
-            al.add(new ArrayList());
+    public boolean canFinish(int n, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
         }
-        
-        for(int[] t: pre)
-        {
-            al.get( t[1] ).add( t[0] );
+        for(int [] edge : prerequisites){
+            int u = edge[1];
+            int v = edge[0];
+            adj.get(u).add(v);
         }
-        
-        for(int i = 0; i < n; i++)
-        {   
-            if(visited[i] == false)
-            {
-                boolean ans = dfs_cyclicDetection(al, i , visited , helper);
-                if(ans)
-                return false;
-            }     
+        int[] vis = new int[n];
+        int[] pathVis = new int[n];
+        for(int i=0;i<n;i++){
+            if(vis[i]==0){
+                if(dfs(i,adj,vis,pathVis))
+                    return false;
+            } 
         }
         return true;
     }
-    
-    public static boolean dfs_cyclicDetection( ArrayList<ArrayList<Integer>> al, int i,  boolean visited[],  boolean helper[]) {
-        
-        visited[i] = true;
-        helper[i] = true; 
-        ArrayList< Integer > neighbour = al.get(i);
-        
-        for(int k = 0; k < neighbour.size(); k++)
-        {
-            int curr = neighbour.get(k);
-            if(helper[curr] == true) return true;
-            
-            if(visited[curr] == false)
-            {
-                boolean ans = dfs_cyclicDetection(al,curr,visited,helper);
-                if(ans == true) return true;
-            }
+    public boolean dfs(int node, List<List<Integer>> adj, int[] vis, int[] pathVis){
+        vis[node] = 1;
+        pathVis[node] = 1;
+        for(int child : adj.get(node)){
+            if(vis[child]==0){
+                if(dfs(child,adj,vis,pathVis)) return true;
+            }else if(pathVis[child]==1) return true;
         }
-		
-        return helper[i] = false;  
+        pathVis[node] = 0;
+        return false;
     }
 }
