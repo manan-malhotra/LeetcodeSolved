4a994c4db3fa8aef49fe9dc8ec638d2eeacd0824
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-07-27 22:20:39 +0530

    Time: 19 ms (40.84%) | Memory: 45.5 MB (50.08%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,42 +1,47 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n = grid.length;
-        if(n==1){
-            if(grid[0][0]==0) return 1;
-            return -1;
-        }
-        Queue<Node> queue = new LinkedList<>();
-        if(grid[0][0]==0 && grid[n-1][n-1]==0)queue.offer(new Node(0,0,1));
-        int[] row = {0,-1,-1,-1,1,1,1,0};
-        int[] col = {1,-1,0,1,-1,0,1,-1};
-        while(!queue.isEmpty()){
-            Node node = queue.poll();
-            int x = node.x;
-            int y = node.y;
-            int wt = node.wt;
-            if(x==y && x==n-1) return wt;
-            for(int i=0;i<8;i++){
-                int newRow = x + row[i];
-                int newCol = y + col[i];
-                if(isValid(newRow,newCol,n,grid)) {
-                    queue.offer(new Node(newRow,newCol,wt+1));
-                    grid[newRow][newCol]=1;
-                }    
+        if(grid[0][0] == 1 || grid[n-1][n-1]==1) return -1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1) grid[i][j] = Integer.MAX_VALUE;
             }
         }
-        return -1;
-    }
-    public boolean isValid(int i,int j, int n,int[][] grid){
-        return (i>=0 && j>=0 && i<n && j<n && grid[i][j]==0 );
-    }
-}
-class Node{
-    int x;
-    int y;
-    int wt;
-    public Node(int x,int y,int wt){
-        this.x=x;
-        this.y=y;
-        this.wt=wt;
+        grid[0][0] = 1;
+        Queue<List<Integer>> q = new LinkedList<>();
+        List<Integer> start = new ArrayList<>();
+        start.add(0);
+        start.add(0);
+        q.offer(start);
+        int[] dRow = {-1,-1,-1,0,0,1,1,1};
+        int[] dCol = {-1,0,1,1,-1,-1,0,1};
+        while(!q.isEmpty()){
+            List<Integer> node = q.remove();
+            int i = node.get(0);
+            int j = node.get(1);
+            if(i==n-1 && j==n-1) return grid[n-1][n-1];
+            // System.out.println(i+" "+j+" start");
+            int weight = grid[i][j];
+            for(int ii=0;ii<8;ii++){
+                int newI = i+dRow[ii];
+                int newJ = j+dCol[ii];
+                // System.out.println(newI+" "+newJ);
+                if(newI<0 || newJ<0 || newI>=n || newJ>=n || grid[newI][newJ]!=0) continue;
+                List<Integer> newNode = new ArrayList<>();
+                newNode.add(newI);
+                newNode.add(newJ);
+                q.offer(newNode);
+                grid[newI][newJ] = weight+1;
+            }
+        }
+        int ans = grid[n-1][n-1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==Integer.MAX_VALUE) grid[i][j] = 1;
+                else grid[i][j] = 0;
+            }
+        }
+        if(ans==0) return -1;
+        return ans;
     }
 }
