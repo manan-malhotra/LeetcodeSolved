bd7061d6d5fdc0c8541dbc935810cc741d3ed2bc
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-08-02 12:45:37 +0530

    Time: 39 ms (70.20%) | Memory: 43.7 MB (90.71%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,28 +1,17 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[][] dp = new int[2][n+1];
-        for(int i=n;i>0;i--){
-            for(int prev = i-1; prev>=0; prev--){
-                int take = 0;
-                if(prev == 0 || nums[i-1]>nums[prev-1]){
-                    take = 1 + dp[(i+1)%2][i];
-                }
-                int notTake = dp[(i+1)%2][prev];
-                dp[i%2][prev] =  Math.max(take,notTake);
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int lis = 1;
+        for(int i=1;i < n; i++){
+            int currMax = 0;
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]) currMax = Math.max(currMax,dp[j]);
             }
+            dp[i] = currMax+1;
+            lis = Math.max(dp[i],lis);
         }
-        return dp[1][0];
-        // return f(1,0,n,nums,dp);
-    }
-    public int f(int i, int prev, int n, int[] nums,int[][] dp){
-        if(i==n+1) return 0;
-        if(dp[i][prev]!=-1) return dp[i][prev];
-        int take = 0;
-        if(prev == 0 || nums[i-1]>nums[prev-1]){
-            take = 1 + f(i+1,i,n,nums,dp);
-        }
-        int notTake = f(i+1,prev,n,nums,dp);
-        return dp[i][prev] =  Math.max(take,notTake);
+        return lis;
     }
 }
