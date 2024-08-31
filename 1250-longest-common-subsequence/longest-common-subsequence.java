f7e07e5cca470cb66232fa738188ae0bf661fe67
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-08-31 10:37:53 +0530

    Time: 23 ms (40.16%) | Memory: 50.6 MB (80.41%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -3,14 +3,15 @@ class Solution {
         int n = text1.length();
         int m = text2.length();
         int[][] dp = new int[n][m];
-        for(int[] row : dp) Arrays.fill(row,-1);
-        return f(0,0,n,m,text1,text2,dp);
+        for(int[] row:dp)
+        Arrays.fill(row,-1);
+        return f(n-1,m-1,text1,text2,dp);
     }
-    public int f(int i, int j, int n, int m, String text1, String text2,int[][] dp){
-        if(i==n || j==m) return 0;
+    public int f(int i, int j, String text1, String text2,int[][] dp){
+        if(i==-1 || j==-1) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        int match = 0;
-        if(text1.charAt(i)==text2.charAt(j)) return dp[i][j] =  1 + f(i+1,j+1,n,m,text1,text2,dp);
-        return  dp[i][j] = Math.max(f(i,j+1,n,m,text1,text2,dp),f(i+1,j,n,m,text1,text2,dp));
+        int match = text1.charAt(i) == text2.charAt(j)?1:0;
+        if(match == 1) return dp[i][j] = 1 + f(i-1,j-1,text1,text2,dp);
+        return dp[i][j] = Math.max(f(i-1,j,text1,text2,dp), f(i,j-1,text1,text2,dp));
     }
 }
