6369fba3dd7fe0cf3511fa170bb5ae98c39421e4
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-08-31 10:16:31 +0530

    Time: 12 ms (19.45%) | Memory: 41.7 MB (73.73%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,16 +1,16 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int[][] dp = new int[n][amount+1];
+        int[][] dp = new int[2][amount+1];
         for(int j=0;j<=amount;j++){
             if(j%coins[0]==0) dp[0][j] = 1;
         }
         for(int i=1;i<n;i++){
             for(int j=0;j<=amount;j++){
-                dp[i][j] = dp[i-1][j];
-                if(j>=coins[i]) dp[i][j] += dp[i][j-coins[i]];
+                dp[i%2][j] = dp[(i-1)%2][j];
+                if(j>=coins[i]) dp[i%2][j] += dp[i%2][j-coins[i]];
             }
         }
-        return dp[n-1][amount];
+        return dp[(n-1)%2][amount];
     }
 }
