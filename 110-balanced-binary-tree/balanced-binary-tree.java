25fca334db5e815c32092cf5a9a56d284abfc292
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-09-06 07:31:55 +0530

    Time: 1 ms (29.08%) | Memory: 44.2 MB (60.39%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -1,29 +1,25 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if (root == null)
-            return true;
-        return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+        if(root==null) return true;
+        return Math.abs(height(root.left)-height(root.right))<=1 && isBalanced(root.left) && isBalanced(root.right);
     }
-
-    private int height(TreeNode root) {
-        if (root == null)
-            return 0;
-        return Math.max(height(root.left), height(root.right)) + 1;
+    public int height(TreeNode root){
+        if(root==null) return 0;
+        return 1 + Math.max(height(root.left),height(root.right));
     }
-
 }
