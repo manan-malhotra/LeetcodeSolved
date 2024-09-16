ab0256391889902c0ed9fd10fee0735c7b12a2e0
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-09-16 13:08:09 +0530

    Time: 10 ms (95.37%) | Memory: 41 MB (95.02%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.java
+++ 149-max-points-on-a-line/max-points-on-a-line.java
@@ -2,9 +2,9 @@ class Solution {
     public int maxPoints(int[][] points) {
         int n = points.length;
         int ans = 1;
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i <= n-2; ++i) {
             int x1 = points[i][0], y1 = points[i][1];
-            for (int j = i + 1; j < n; ++j) {
+            for (int j = i + 1; j <= n-1; ++j) {
                 int x2 = points[j][0], y2 = points[j][1];
                 int cnt = 2;
                 for (int k = j + 1; k < n; ++k) {
