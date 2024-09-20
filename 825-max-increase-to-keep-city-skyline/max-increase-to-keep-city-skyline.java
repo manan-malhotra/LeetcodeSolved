549e06b22ccbb0634f7b6704eefc677b679a1cb3
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-09-20 16:21:25 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (84.50%) - LeetSync

--- 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
+++ 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
@@ -1,28 +1,26 @@
 class Solution {
     public int maxIncreaseKeepingSkyline(int[][] grid) {
-        int n = grid.length;
-        int[] maxR = new int[n];
-        int[] maxC = new int[n];
-        for(int i=0;i<n;i++){
-            int max = 0;    
-            for(int j=0;j<n;j++) max=Math.max(max,grid[i][j]);
-            maxR[i] = max;
-        }
-        for(int j=0;j<n;j++){
-            int max = 0;    
-            for(int i=0;i<n;i++) max=Math.max(max,grid[i][j]);
-            maxC[j] = max;
-        }
-        for(int m:maxR) System.out.println(m);
-        for(int m:maxC) System.out.println(m);
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                int maxVal = Math.min(maxC[i],maxR[j]);
-                ans += (maxVal-grid[i][j]);
-                grid[i][j] = maxVal;
+        int n=grid.length;
+        int maxrow[]=new int[n];
+        int maxcol[]=new int[n];
+        int add=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                maxrow[i]=Math.max(maxrow[i],grid[i][j]);
+                maxcol[j]=Math.max(maxcol[j],grid[i][j]);
             }
         }
-        return ans;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                int newheight=Math.min(maxrow[i],maxcol[j]);
+                add+=newheight-grid[i][j];
+            }
+        }
+        return add;
     }
 }
