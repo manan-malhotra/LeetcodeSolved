7fae7871c0c338ba3ac633e3b7ef3f3388ef7ee6
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-10-12 11:21:03 +0530

    Time: 17 ms (32.70%) | Memory: 41.5 MB (53.08%) - LeetSync

--- 1743-count-substrings-that-differ-by-one-character/count-substrings-that-differ-by-one-character.java
+++ 1743-count-substrings-that-differ-by-one-character/count-substrings-that-differ-by-one-character.java
@@ -1,38 +1,23 @@
-// version 4:  O(1)- space
 class Solution {
-    private String s;
-    private String t;
-    private int res = 0;
     public int countSubstrings(String s, String t) {
-        this.s = s;
-        this.t = t;
-        
-        // starting from left boundary dp[0][0], dp[1][0], ..., dp[m-1][0]
-        for (int i = 0; i < s.length(); i++) {
-            traverseRightBottom(i, 0);
-        }
-        
-        // starting from top boundary dp[0][1], dp[0][2], ..., dp[0][n-1]
-        for (int j = 1; j < t.length(); j++) {
-            traverseRightBottom(0, j);
-        }
-        
-        return res;
-    }
-    
-    private void traverseRightBottom(int i, int j) {
-        int prev0 = 0;      // dp[i][j][0]
-        int prev1 = 0;      // dp[i][j][1]
-        
-        while (i < s.length() && j < t.length()) {
-            boolean match = (s.charAt(i++) == t.charAt(j++));
-            int curr0 = match ? prev0 + 1 : 0;            // dp[i+1][j+1][0]
-            int curr1 = match ? prev1     : prev0 + 1;    // dp[i+1][j+1][1]
+        int ans =0;
+        for(int i=0;i<s.length();i++)
+        {
+            for(int j=0;j<t.length();j++)
+            {
+                int count =0;
+                for(int k=0;i+k<s.length()&&j+k<t.length();k++)
+                {
+                    if(s.charAt(i+k)!=t.charAt(j+k))
+                    {
+                        count++;
+                    }
+                    if(count==1)
+                     ans++;
+                }
+            }
             
-            res += curr1; 
-            
-            prev0 = curr0;
-            prev1 = curr1;
         }
+        return ans;
     }
 }
