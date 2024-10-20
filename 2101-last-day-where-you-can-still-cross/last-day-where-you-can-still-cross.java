3ba4e6ebe7e7becf90adc7c795bbe9922c59933e
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-10-20 16:42:03 +0530

    Time: 13 ms (95.51%) | Memory: 56.1 MB (73.72%) - LeetSync

--- 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.java
+++ 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.java
@@ -1,61 +1,66 @@
+class DSU {
+    int[] root, size;
+    
+    public DSU(int n) {
+        root = new int[n];
+        for (int i = 0; i < n; i++) {
+            root[i] = i;
+        }
+        size = new int[n];
+        Arrays.fill(size, 1);
+    }
+    
+    public int find(int x) {
+        if (root[x] != x) {
+            root[x] = find(root[x]);
+        }
+        return root[x];
+    }
+    
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX == rootY) {
+            return;
+        }
+        
+        if (size[rootX] > size[rootY]) {
+            int tmp = rootX;
+            rootX = rootY;
+            rootY = tmp;
+        }
+        root[rootX] = rootY;
+        if (size[rootX] > size[rootY]) size[rootY]++;
+    }
+}
+
 class Solution {
-
-    private int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
-    private boolean cancross(int row, int col, int[][] cells, int index){
-        int[][] grid = new int[row][col];
-
-        for (int i = 0; i < index; ++i) {
-            grid[cells[i][0] - 1][cells[i][1] - 1] = 1;
-        }
-
-        for(int i = 0;i < col;i++){
-            if(dfs(grid,0,i)){
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private boolean dfs(int[][] grid, int i, int j){
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 1){
-            return false;
-        }
-
-        if(i == grid.length-1){
-            return true;
-        }
-
-        grid[i][j] = 1;
-
-        for(int[] dir : dirs){
-            int x = i + dir[0];
-            int y = j + dir[1];
-
-            if(dfs(grid,x,y)){
-                return true;
-            }
-        }
-
-        return false;
-    }
-
     public int latestDayToCross(int row, int col, int[][] cells) {
-        int lo = 1, hi = cells.length;
-
-        while(lo < hi){
-
-            int mid = hi - (hi-lo) / 2;
-
-            if(cancross(row,col,cells,mid)){
-                lo = mid;
+        DSU dsu = new DSU(row * col + 2);
+        int[][] grid = new int[row][col];
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+        
+        for (int i = 0; i < row * col; ++i) {
+            int r = cells[i][0] - 1, c = cells[i][1] - 1;
+            grid[r][c] = 1;
+            int index1 = r * col + c + 1;
+            for (int[] d : directions) {
+                int newR = r + d[0], newC = c + d[1];
+                int index2 = newR * col + newC + 1;
+                if (newR >= 0 && newR < row && newC >= 0 && newC < col && grid[newR][newC] == 1) {
+                    dsu.union(index1, index2);
+                }
             }
-            else{
-                hi = mid-1;
+            if (c == 0) {
+                dsu.union(0, index1);
+            }
+            if (c == col - 1) {
+                dsu.union(row * col + 1, index1);
+            }
+            if (dsu.find(0) == dsu.find(row * col + 1)) {
+                return i;
             }
         }
-
-        return lo;
+        return -1;
     }
 }
