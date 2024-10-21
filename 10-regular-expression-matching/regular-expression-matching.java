cb0b6836e6527badff4909b26da11bb5ecdefc85
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-10-21 22:41:04 +0530

    Time: 1 ms (99.97%) | Memory: 41.9 MB (67.14%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -0,0 +1,35 @@
+enum Result {
+    TRUE, FALSE
+}
+
+class Solution {
+    Result[][] dp;
+
+    public boolean isMatch(String text, String pattern) {
+        dp = new Result[text.length() + 1][pattern.length() + 1];
+        return dp(0, 0, text, pattern);
+    }
+
+    public boolean dp(int i, int j, String text, String pattern) {
+        if (dp[i][j] != null) {
+            return dp[i][j] == Result.TRUE;
+        }
+        boolean ans;
+        if (j == pattern.length()){
+            ans = i == text.length();
+        } else{
+            boolean first_match = (i < text.length() &&
+                                   (pattern.charAt(j) == text.charAt(i) ||
+                                    pattern.charAt(j) == '.'));
+
+            if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
+                ans = (dp(i, j+2, text, pattern) ||
+                       first_match && dp(i+1, j, text, pattern));
+            } else {
+                ans = first_match && dp(i+1, j+1, text, pattern);
+            }
+        }
+        dp[i][j] = ans ? Result.TRUE : Result.FALSE;
+        return ans;
+    }
+}
