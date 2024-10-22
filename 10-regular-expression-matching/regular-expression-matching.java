a337505465c5f5529ae11eea5712ac760f290244
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-10-22 10:43:47 +0530

    Time: 319 ms (25.64%) | Memory: 42.2 MB (31.41%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,31 +1,28 @@
 class Solution {
-    	Boolean[][] mem;
+    Boolean[][] dp;
     public boolean isMatch(String s, String p) {
-        mem = new Boolean[s.length()+1][p.length()];
-        return isMatch(0,s,0,p);
+        int n = s.length(), m = p.length();
+        dp = new Boolean[n+1][m];
+        return isMatch(0,s,0,p,n,m);
     }
-    private boolean isMatch(int i, String s, int j, String p) { 
-        int sn = s.length(), pn = p.length();
-        if(j==pn) { // since * in p can match 0 of previous char, so empty string(i==sn) may match p
-            return i==sn;    
+    private boolean isMatch(int i, String s, int j, String p,int n, int m) { 
+        if(j==m) { // since * in p can match 0 of previous char, so empty string(i==n) may match p
+            return i==n;    
         }
-        if(mem[i][j]!=null) {
-            return mem[i][j];
-        }
-        char pj = p.charAt(j);
-        if(j+1<pn && p.charAt(j+1)=='*') { //match *, needs to look at the next char to repeate current char
-            if(isMatch(i,s,j+2,p)) {
-                return mem[i][j]=true;
+        if(dp[i][j]!=null) return dp[i][j];
+        if(j+1<m && p.charAt(j+1)=='*') { //match *, needs to look at the next char to repeate current char
+            if(isMatch(i,s,j+2,p,n,m)) {
+                return dp[i][j] = true;
             }
-            if(i<sn && (pj == '.'||pj==s.charAt(i))) {
-                if(isMatch(i+1,s,j,p)) {
-                    return mem[i][j]=true;
+            while(i<n && (p.charAt(j) == '.'||p.charAt(j)==s.charAt(i))) {
+                if(isMatch(++i,s,j+2,p,n,m)) {
+                    return dp[i][j] = true;
                 }
             }
-        } else if(i<sn && (s.charAt(i) == pj ||    //match char
-                   pj=='.')) {              //match dot
-            return mem[i][j]=isMatch(i+1, s, j+1, p);
+        } else if(i<n && (s.charAt(i) == p.charAt(j) ||    //match char
+                   p.charAt(j)=='.')) {              //match dot
+            return dp[i][j] = isMatch(i+1, s, j+1, p,n,m);
         }
-        return mem[i][j]=false;
+        return dp[i][j] = false;
     }
 }
