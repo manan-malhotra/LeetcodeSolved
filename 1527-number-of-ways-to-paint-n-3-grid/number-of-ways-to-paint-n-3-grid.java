2afdda4453b933883c22bf143c80f814aa8323a1
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-10-22 11:06:09 +0530

    Time: 726 ms (5.60%) | Memory: 56.2 MB (5.60%) - LeetSync

--- 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.java
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.java
@@ -1,17 +1,24 @@
 class Solution {
+    int [][] colors = {{0,1,0}, {1,0,1}, {2,0,1}, {0,1,2}, {1,0,2}, {2,0,2}, {0,2,0}, {1,2,0}, {2,1,0}, {0,2,1},{1,2,1}, {2,1,2}}; // States for n = 1
     public int numOfWays(int n) {
-        long MOD = 1_000_000_007;
-        long dpPreviousRed = 6;
-        long dpPreviousBlue = 6;
-
-        for (int i = 2; i <= n; i++) {
-            long newDpRed = (2 * dpPreviousRed + 2 * dpPreviousBlue) % MOD;
-            long newDpBlue = (2 * dpPreviousRed + 3 * dpPreviousBlue) % MOD;
-            
-            dpPreviousRed = newDpRed;
-            dpPreviousBlue = newDpBlue;
+        int[] init = {-1, -1, -1};
+        return dfs(n, init, 0);
+    }
+    int mod = 1000000007;
+    HashMap<String, Integer> memo = new HashMap<>();
+    public int dfs(int n, int[] curr, int idx) {
+        if (idx == n) {
+            return 1;
         }
-
-        return (int)((dpPreviousRed + dpPreviousBlue) % MOD);
+        String key = curr.toString() + "," + idx;
+        if (memo.containsKey(key)) return memo.get(key);
+        int result = 0;
+        for (int i = 0; i < 12; i++) {
+            int []next = colors[i];
+            if (next[0] == curr[0] || next[1] == curr[1] || next[2] == curr[2]) continue;
+            result = (result%mod + dfs(n, next, idx+1)%mod)%mod;
+        }
+        memo.put(key, result%mod);
+        return memo.get(key);
     }
 }
