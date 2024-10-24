61fc4558de8c5e59be797f5f3ff0e7c52545a17b
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-10-25 00:36:37 +0530

    Time: 4 ms (93.84%) | Memory: 42.8 MB (56.70%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,44 +1,25 @@
 class Solution {
-    private int countPartitionsUtil(int ind, int target, int[] arr, int[][] dp) {
-        if (ind == 0) {
-            if (target == 0 && arr[0] == 0)
-                return 2;
-            if (target == 0 || target == arr[0])
-                return 1;
-            return 0;
-        }
-
-        if (dp[ind][target] != -1)
-            return dp[ind][target];
-
-        int notTaken = countPartitionsUtil(ind - 1, target, arr, dp);
-        int taken = 0;
-
-        if (arr[ind] <= target)
-            taken = countPartitionsUtil(ind - 1, target - arr[ind], arr, dp);
-
-        return dp[ind][target] = (notTaken + taken);
-    }
-
     public int findTargetSumWays(int[] nums, int target) {
-        int totSum = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            totSum += nums[i];
+        int total = 0;
+        for(int num:nums) total += num;
+        if(total-target<0) return 0;
+        if((total-target)%2==1) return 0;
+        int newTarget = (total-target)/2;
+        int n = nums.length;
+        int[][] dp = new int[n][newTarget+1];
+        for(int[] row : dp) Arrays.fill(row,-1);
+        return findTarget(n-1,newTarget,nums,dp);
+    }
+    public int findTarget(int i, int target, int[] arr, int[][] dp){
+        if(dp[i][target]!=-1) return dp[i][target];
+        if(i==0){
+            if(target==0 && arr[i]==0) return dp[0][0]=2;
+            if(target==0 || arr[i]==target) return dp[0][target]=1;
+            return 0;
         }
-
-        if (totSum - target < 0)
-            return 0;
-        if ((totSum - target) % 2 == 1)
-            return 0;
-
-        int s2 = (totSum - target) / 2;
-
-        int dp[][] = new int[nums.length][s2 + 1];
-
-        for (int row[] : dp)
-            Arrays.fill(row, -1);
-
-        return countPartitionsUtil(nums.length - 1, s2, nums, dp);
+        int notTake = findTarget(i-1,target,arr,dp);
+        int take = 0;
+        if(arr[i]<=target) take = findTarget(i-1,target-arr[i],arr,dp);
+        return dp[i][target] = take + notTake;
     }
 }
