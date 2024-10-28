f1807f56ead423fd3d07fe9ef5d86f9e3f8095d8
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-10-28 15:09:25 +0530

    Time: 107 ms (20.55%) | Memory: 78.6 MB (22.99%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -11,15 +11,16 @@ class Solution {
         if(dp[i][bought][left]!=-1) return dp[i][bought][left];
         if(left==0) return dp[i][bought][left] = 0;
         if(i==n-1){
-            if(bought==1) return dp[i][bought][left] = prices[i];
-            else return dp[i][bought][left] = 0;
-        }
-        int ignore = f(i+1,bought,left,n,prices,dp);
-        if(bought==1){
+            if(bought==0) return dp[i][bought][left] = 0;
+            return dp[i][bought][left] = prices[i];
+        }else{
+            int ignore = f(i+1,bought,left,n,prices,dp);
+            if(bought==0){
+                int buy = f(i+1,1,left,n,prices,dp) - prices[i];
+                return dp[i][bought][left] = Math.max(ignore,buy);
+            }
             int sell = f(i+1,0,left-1,n,prices,dp) + prices[i];
-            return dp[i][bought][left] = Math.max(sell,ignore);
+            return dp[i][bought][left] = Math.max(ignore,sell);
         }
-        int buy = f(i+1,1,left,n,prices,dp) - prices[i];
-        return dp[i][bought][left] = Math.max(buy,ignore);
     }
 }
