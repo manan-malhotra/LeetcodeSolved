417fe4f52649155c8f34c40371395ece0bec2c19
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-11-02 15:47:03 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (94.64%) - LeetSync

--- 1409-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix/minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix.java
+++ 1409-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix/minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix.java
@@ -1,75 +1,35 @@
 class Solution {
-    int count =0;
-    public int minFlips(int[][] mat) {
-        int m = mat.length;
-        int n = mat[0].length;
-        for(int i=0; i< m ; i++){
-            for(int j =0; j< n; j++){
-                if(mat[i][j] == 1){
-                    count++;
-                }
-            }
-        }
-        int res = recursion(mat, 0);
-        if(res == Integer.MAX_VALUE){
-            return -1;
-        }
-        return res;
+    private int ans;
+    private int[][] dir = {{-1,0}, {0,-1}, {0,0}, {0,1}, {1,0}};
+    public int minFlips(int[][] a) {
+        ans = 123456789;
+        dfs(a, 0, 0, 0);
+        return ans == 123456789 ? -1 : ans;
     }
 
-    private int recursion(int [][]mat, int ci){
-        if(count ==0){
-            return 0;
+    private void dfs(int[][] a, int x, int y, int step) {
+        if (x == a.length -1 && y == a[0].length) {
+            for (int i = 0; i < a.length; i++)
+                for (int j = 0; j < a[0].length; j++)
+                    if (a[i][j] != 0)
+                        return;
+            ans = Math.min(ans, step);
         }
-        int m = mat.length;
-        int n = mat[0].length;
-        if(ci >= m*n){
-            return Integer.MAX_VALUE;
+        if (step >= ans)
+            return;
+        if (y == a[0].length) {
+            x++;
+            y = 0;
         }
-         int res = Integer.MAX_VALUE;
-        int flip = Integer.MAX_VALUE;
-        int nonflip = recursion(mat,ci+1);
-        res = Math.min(res, nonflip);
-        flipMatrix(mat, ci);
-        int curr = recursion(mat, ci+1);
-        if(curr != Integer.MAX_VALUE){
-            flip = 1+ curr;
+        dfs(a, x, y + 1, step);
+        for (int[] d : dir) {
+            if (x + d[0] >= 0 && x + d[0] < a.length && y +d[1] >= 0 && y + d[1] < a[0].length)
+                a[x + d[0]][y +d[1]] = 1 - a[x + d[0]][y +d[1]];
         }
-        flipMatrix(mat, ci);
-        res = Math.min(flip, res);
-        return res;
-
-    }
-
-    private void flipMatrix(int[][]mat, int ci){
-        int m = mat.length;
-        int n = mat[0].length;
-        int r = ci/n;
-        int c = ci%n;
-
-        if(mat[r][c] == 0){
-            mat[r][c] = 1;
-            count++;
-        }else {
-            mat[r][c] = 0;
-            count--;
-        }
-
-        int [][]dirs = {{0,1}, {0,-1}, {1,0}, {-1,0}};
-        for(int []d : dirs){
-            int nr = r + d[0];
-            int nc = c + d[1];
-
-            if(nr <0 || nr>= m || nc< 0 || nc>=n){
-                continue;
-            }
-            if(mat[nr][nc] == 1){
-                mat[nr][nc] = 0;
-                count--;
-            }else {
-                mat[nr][nc] = 1;
-                count++;
-            }
+        dfs(a, x, y + 1, step + 1);
+        for (int[] d : dir) {
+            if (x + d[0] >= 0 && x + d[0] < a.length && y +d[1] >= 0 && y + d[1] < a[0].length)
+                a[x + d[0]][y +d[1]] = 1 - a[x + d[0]][y +d[1]];
         }
     }
 }
