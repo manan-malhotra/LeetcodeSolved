a7e6fbe85d4cd74a17c7568553d99cdcba72d8fa
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-11-04 17:09:26 +0530

    Time: 419 ms (38.40%) | Memory: 50.1 MB (37.30%) - LeetSync

---
+++ 1906-maximize-score-after-n-operations/maximize-score-after-n-operations.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int maxScore(int[] nums) {
+        Map<String, Integer> memo = new HashMap<>();
+        return helper(nums, 1, 0, memo);
+    }
+    private int helper(int[] nums, int i, int mask, Map<String, Integer> memo) {
+        if (i > nums.length / 2) {
+            return 0;
+        }
+
+        String key = i + "," + mask;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+
+        int maxScore = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if ((mask & (1 << j)) == 0) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if ((mask & (1 << k)) == 0) {
+                        int newMask = mask | (1 << j) | (1 << k);
+                        int score = i * gcd(nums[j], nums[k]) + helper(nums, i + 1, newMask, memo);
+                        maxScore = Math.max(maxScore, score);
+                    }
+                }
+            }
+        }
+
+        memo.put(key, maxScore);
+        return maxScore;
+    }
+
+    private int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
