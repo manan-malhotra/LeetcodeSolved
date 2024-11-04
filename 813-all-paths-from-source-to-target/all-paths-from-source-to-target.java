b1d9be2011f7446f74e3dcc56371ea81e6ac201c
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-11-04 08:13:51 +0530

    Time: 2 ms (73.36%) | Memory: 46 MB (81.59%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
@@ -1,11 +1,9 @@
 class Solution {
     List<List<Integer>> ans;
     List<Integer> temp;
-    int[] pathVis;
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         ans = new ArrayList<>();    
         temp = new ArrayList<>();    
-        pathVis = new int[graph.length];
         temp.add(0);
         dfs(0,graph);
         return ans;
@@ -16,13 +14,9 @@ class Solution {
             return;
         }
         for(int child:graph[i]){
-            if(pathVis[child]==0){
-                temp.add(child);
-                pathVis[child]=1;
-                dfs(child,graph);
-                pathVis[child]=0;
-                temp.remove(temp.size()-1);
-            }
+            temp.add(child);
+            dfs(child,graph);
+            temp.remove(temp.size()-1);
         }
     }
 }
